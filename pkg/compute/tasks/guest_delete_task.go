a77e8f3b67f0f3e21b25663e2883970300c302b9
Zhang Dongliang <zhangdongliang@yunionyun.com>
2018-08-25 07:54:50 +0800

    阿里云虚机删除日志

--- pkg/compute/tasks/guest_delete_task.go
+++ pkg/compute/tasks/guest_delete_task.go
@@ -9,6 +9,7 @@ import (
 	"yunion.io/x/onecloud/pkg/cloudcommon/notifyclient"
 	"yunion.io/x/onecloud/pkg/compute/models"
 	"yunion.io/x/onecloud/pkg/compute/options"
+	"yunion.io/x/onecloud/pkg/util/logclient"
 	"yunion.io/x/pkg/utils"
 )
 
@@ -88,6 +89,7 @@ func (self *GuestDeleteTask) OnGuestDeleteCompleteFailed(ctx context.Context, ob
 	guest := obj.(*models.SGuest)
 	guest.SetStatus(self.UserCred, models.VM_DELETE_FAIL, err.String())
 	db.OpsLog.LogEvent(guest, db.ACT_DELOCATE_FAIL, err, self.UserCred)
+	logclient.AddActionLog(self.UserCred, logclient.ACT_DELETE, "", guest, err.String())
 }
 
 func (self *GuestDeleteTask) OnGuestDeleteComplete(ctx context.Context, obj db.IStandaloneModel, data jsonutils.JSONObject) {
@@ -103,6 +105,7 @@ func (self *GuestDeleteTask) DeleteGuest(ctx context.Context, guest *models.SGue
 	guest.RealDelete(ctx, self.UserCred)
 	guest.RemoveAllMetadata(ctx, self.UserCred)
 	db.OpsLog.LogEvent(guest, db.ACT_DELOCATE, nil, self.UserCred)
+	logclient.AddActionLog(self.UserCred, logclient.ACT_DELETE, "", guest, "")
 	if !guest.IsSystem && !guest.PendingDeleted {
 		self.NotifyServerDeleted(ctx, guest)
 	}
