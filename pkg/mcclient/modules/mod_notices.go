8f5f8c06244662800d5596a9806b5e4aacd4e0ca
TangBin <tangbin@yunionyun.com>
2018-08-27 19:28:53 +0800

    add notice mod

---
+++ pkg/mcclient/modules/mod_notices.go
@@ -0,0 +1,26 @@
+package modules
+
+type NoticeManager struct {
+	ResourceManager
+}
+
+type NoticeReadMarkManager struct {
+	ResourceManager
+}
+
+var (
+	Notice         NoticeManager
+	NoticeReadMark NoticeReadMarkManager
+)
+
+func init() {
+	Version = VersionManager{NewYunionAgentManager("notice", "notices",
+		[]string{},
+		[]string{})}
+	register(&Notice)
+
+	NoticeReadMark = NoticeReadMarkManager{NewYunionAgentManager("readmark", "readmarks",
+		[]string{},
+		[]string{})}
+	register(&Notice)
+}
