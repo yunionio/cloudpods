29caf7d6fa5fc33eed0862a715052c99f170f882
屈轩 <quxuan@yunionyun.com>
2018-08-28 16:09:59 +0800

    添加GetDetachDiskStatus内容

--- pkg/compute/guestdrivers/aliyun.go
+++ pkg/compute/guestdrivers/aliyun.go
@@ -49,6 +49,10 @@ func (self *SAliyunGuestDriver) ChooseHostStorage(host *models.SHost, backend st
 	return nil
 }
 
+func (self *SAliyunGuestDriver) GetDetachDiskStatus() ([]string, error) {
+	return []string{models.VM_READY, models.VM_RUNNING}, nil
+}
+
 func (self *SAliyunGuestDriver) ValidateCreateData(ctx context.Context, userCred mcclient.TokenCredential, data *jsonutils.JSONDict) (*jsonutils.JSONDict, error) {
 	data, err := self.SManagedVirtualizedGuestDriver.ValidateCreateData(ctx, userCred, data)
 	if err != nil {
