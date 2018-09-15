bff2fd4649fccb5778d63fa2ab7d2254e60e3c36
zhengjiajie <zhengjiajie@yunionyun.com>
2018-09-15 17:24:59 +0800

    zhengjiajie modify code style

--- pkg/mcclient/modules/mod_res_results.go
+++ pkg/mcclient/modules/mod_res_results.go
@@ -6,7 +6,8 @@ var (
 
 func init() {
 	ResResults = NewMeterManager("res_result", "res_results",
-		[]string{"res_id", "res_name", "cpu", "mem", "sys_disk","data_disk", "ips", "res_type", "band_width", "os_distribution", "os_version", "platform", "region_id", "project_name", "user_name","start_time", "end_time", "time_length", "cpu_amount", "mem_amount", "disk_amount", "baremetal_amount", "gpu_amount", "res_fee"},
+		[]string{"res_id", "res_name", "cpu", "mem", "sys_disk", "data_disk", "ips", "res_type", "band_width", "os_distribution", "os_version", "platform", "region_id",
+			 "project_name", "user_name", "start_time", "end_time", "time_length", "cpu_amount", "mem_amount", "disk_amount", "baremetal_amount", "gpu_amount", "res_fee"},
 		[]string{},
 	)
 	register(&ResResults)
