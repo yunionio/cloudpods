f653f2b4993b1e00263457e23baa0c31c109b3ad
zhengjiajie <zhengjiajie@yunionyun.com>
2018-09-18 12:29:27 +0800

    zhengjiajie delete old param

--- cmd/climc/shell/res_results.go
+++ cmd/climc/shell/res_results.go
@@ -16,9 +16,6 @@ func init() {
 		EndDate   string `help:"end_date of the query"`
 		ResType   string `help:"res_type of the query"`
 		Platform  string `help:"platform of the query"`
-		Export    string `help:"export of the query"`
-		ExportKeys     []string `help:"export_keys of the query"`
-		ExportTexts    []string `help:"export_texts of the query"`
 		ProjectId string `help:"project_id of the query"`
 	}
 	R(&ResResultsListOptions{}, "resresult-list", "List all res results ", func(s *mcclient.ClientSession, args *ResResultsListOptions) error {
@@ -46,15 +43,6 @@ func init() {
 		}
 		if len(args.Platform) > 0 {
 			params.Add(jsonutils.NewString(args.Platform), "platform")
-		}
-		if len(args.Export) > 0 {
-			 params.Add(jsonutils.NewString(args.Export), "export")
-		}
-		if len(args.ExportKeys) > 0 {
-			params.Add(jsonutils.NewStringArray(args.ExportKeys), "export_keys")
-		}
-		if len(args.ExportTexts) > 0 {
-			params.Add(jsonutils.NewStringArray(args.ExportTexts), "export_texts")
 		}
 		if len(args.ProjectId) > 0 {
 			params.Add(jsonutils.NewString(args.ProjectId), "project_id")
