45e9ecd36264c45704812e35506b3d9626b3c455
Zexi Li <lizexi@yunionyun.com>
2018-10-18 17:01:46 +0800

    scheduler: filter host when cloudprovider status not connected

---
+++ pkg/scheduler/db/models/zone.go
@@ -0,0 +1,41 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Zone struct {
+	StandaloneModel
+	Location      string `gorm:"column:location"`
+	ManagerUri    string `gorm:"column:manager_uri"`
+	CloudregionId string `gorm:"column:cloudregion_id"`
+}
+
+func (z Zone) TableName() string {
+	return zonesTable
+}
+
+func (z Zone) String() string {
+	s, _ := JsonString(z)
+	return s
+}
+
+func NewZoneResource(db *gorm.DB) (Resourcer, error) {
+	return newResource(db, zonesTable,
+		func() interface{} {
+			return &Zone{}
+		},
+		func() interface{} {
+			zones := []Zone{}
+			return &zones
+		},
+	)
+}
+
+func FetchZoneByID(id string) (*Zone, error) {
+	zone, err := FetchByID(Zones, id)
+	if err != nil {
+		return nil, err
+	}
+	return zone.(*Zone), nil
+}
