ec7e149ffb029a4d19780bbc0d8d3492c0af33fa
TangBin <tangbin@yunionyun.com>
2018-10-29 20:32:11 +0800

    update guest driver

--- pkg/compute/guestdrivers/aws.go
+++ pkg/compute/guestdrivers/aws.go
@@ -2,8 +2,10 @@ package guestdrivers
 
 import (
 	"context"
+	"fmt"
 	"yunion.io/x/jsonutils"
 	"yunion.io/x/log"
+	"yunion.io/x/onecloud/pkg/cloudcommon/db"
 	"yunion.io/x/onecloud/pkg/cloudcommon/db/taskman"
 	"yunion.io/x/onecloud/pkg/compute/models"
 	"yunion.io/x/onecloud/pkg/mcclient"
@@ -50,14 +52,105 @@ func (self *SAwsGuestDriver) ValidateCreateData(ctx context.Context, userCred mc
 func (self *SAwsGuestDriver) RequestDeployGuestOnHost(ctx context.Context, guest *models.SGuest, host *models.SHost, task taskman.ITask) error {
 	config := guest.GetDeployConfigOnHost(ctx, host, task.GetParams())
 	log.Debugf("RequestDeployGuestOnHost: %s", config)
+
+	action, err := config.GetString("action")
+	if err != nil {
+		return err
+	}
+
+	switch action {
+	case "deploy":
+		return nil
+	case "create":
+		return nil
+	case "rebuild":
+		return nil
+	default:
+		return nil
+	}
 	return nil
 }
 
 func (self *SAwsGuestDriver) OnGuestDeployTaskDataReceived(ctx context.Context, guest *models.SGuest, task taskman.ITask, data jsonutils.JSONObject) error {
+	if data.Contains("disks") {
+		diskInfo := make([]SDiskInfo, 0)
+		err := data.Unmarshal(&diskInfo, "disks")
+		if err != nil {
+			return err
+		}
+		disks := guest.GetDisks()
+		if len(disks) != len(diskInfo) {
+			msg := fmt.Sprintf("inconsistent disk number: have %d want %d", len(disks), len(diskInfo))
+			log.Errorf(msg)
+			return fmt.Errorf(msg)
+		}
+		for i := 0; i < len(diskInfo); i += 1 {
+			disk := disks[i].GetDisk()
+			_, err = disk.GetModelManager().TableSpec().Update(disk, func() error {
+				disk.DiskSize = diskInfo[i].Size
+				disk.ExternalId = diskInfo[i].Uuid
+				disk.DiskType = diskInfo[i].DiskType
+				disk.Status = models.DISK_READY
+				if len(diskInfo[i].Metadata) > 0 {
+					for key, value := range diskInfo[i].Metadata {
+						if err := disk.SetMetadata(ctx, key, value, task.GetUserCred()); err != nil {
+							log.Errorf("set disk %s mata %s => %s error: %v", disk.Name, key, value, err)
+						}
+					}
+				}
+				return nil
+			})
+			if err != nil {
+				msg := fmt.Sprintf("save disk info failed %s", err)
+				log.Errorf(msg)
+				break
+			} else {
+				db.OpsLog.LogEvent(disk, db.ACT_ALLOCATE, disk.GetShortDesc(), task.GetUserCred())
+			}
+		}
+	}
+	uuid, _ := data.GetString("uuid")
+	if len(uuid) > 0 {
+		guest.SetExternalId(uuid)
+	}
+
+	if metaData, _ := data.Get("metadata"); metaData != nil {
+		meta := make(map[string]string, 0)
+		if err := metaData.Unmarshal(meta); err != nil {
+			log.Errorf("Get guest %s metadata error: %v", guest.Name, err)
+		} else {
+			for key, value := range meta {
+				if err := guest.SetMetadata(ctx, key, value, task.GetUserCred()); err != nil {
+					log.Errorf("set guest %s mata %s => %s error: %v", guest.Name, key, value, err)
+				}
+			}
+		}
+	}
+
+	guest.SaveDeployInfo(ctx, task.GetUserCred(), data)
 	return nil
 }
 
 func (self *SAwsGuestDriver) RequestDiskSnapshot(ctx context.Context, guest *models.SGuest, task taskman.ITask, snapshotId, diskId string) error {
+	iDisk, _ := models.DiskManager.FetchById(diskId)
+	disk := iDisk.(*models.SDisk)
+	providerDisk, err := disk.GetIDisk()
+	if err != nil {
+		return err
+	}
+	iSnapshot, _ := models.SnapshotManager.FetchById(snapshotId)
+	snapshot := iSnapshot.(*models.SSnapshot)
+	taskman.LocalTaskRun(task, func() (jsonutils.JSONObject, error) {
+		cloudSnapshot, err := providerDisk.CreateISnapshot(snapshot.Name, "")
+		if err != nil {
+			return nil, err
+		}
+		res := jsonutils.NewDict()
+		res.Set("snapshot_id", jsonutils.NewString(cloudSnapshot.GetId()))
+		res.Set("manager_id", jsonutils.NewString(cloudSnapshot.GetManagerId()))
+		res.Set("cloudregion_id", jsonutils.NewString(cloudSnapshot.GetRegionId()))
+		return res, nil
+	})
 	return nil
 }
 
