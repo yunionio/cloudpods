5eca3c4c97a8a7b2ee4e2a1254a695a77e81661b
TangBin <tangbin@yunionyun.com>
2018-10-29 14:42:29 +0800

    add driver

--- pkg/compute/guestdrivers/aws.go
+++ pkg/compute/guestdrivers/aws.go
@@ -1,1 +1,64 @@
 package guestdrivers
+
+import (
+	"context"
+	"yunion.io/x/jsonutils"
+	"yunion.io/x/onecloud/pkg/cloudcommon/db/taskman"
+	"yunion.io/x/onecloud/pkg/compute/models"
+	"yunion.io/x/onecloud/pkg/mcclient"
+)
+
+type SAwsGuestDriver struct {
+	SManagedVirtualizedGuestDriver
+}
+
+func (self *SAwsGuestDriver) GetHypervisor() string {
+	return models.HYPERVISOR_AWS
+}
+
+func (self *SAwsGuestDriver) ChooseHostStorage(host *models.SHost, backend string) *models.SStorage {
+	storages := host.GetAttachedStorages("")
+	for i := 0; i < len(storages); i += 1 {
+		if storages[i].StorageType == backend {
+			return &storages[i]
+		}
+	}
+
+	for _, stype := range []string{"gp2", "io1", "st1", "sc1", "standard"} {
+		for i := 0; i < len(storages); i += 1 {
+			if storages[i].StorageType == stype {
+				return &storages[i]
+			}
+		}
+	}
+	return nil
+}
+
+func (self *SAwsGuestDriver) GetDetachDiskStatus() ([]string, error) {
+	return []string{models.VM_READY, models.VM_RUNNING}, nil
+}
+
+func (self *SAwsGuestDriver) RequestDetachDisk(ctx context.Context, guest *models.SGuest, task taskman.ITask) error {
+	return guest.StartSyncTask(ctx, task.GetUserCred(), false, task.GetTaskId())
+}
+
+func (self *SAwsGuestDriver) ValidateCreateData(ctx context.Context, userCred mcclient.TokenCredential, data *jsonutils.JSONDict) (*jsonutils.JSONDict, error) {
+	return self.SManagedVirtualizedGuestDriver.ValidateCreateData(ctx, userCred, data)
+}
+
+func (self *SAwsGuestDriver) RequestDeployGuestOnHost(ctx context.Context, guest *models.SGuest, host *models.SHost, task taskman.ITask) error {
+	return nil
+}
+
+func (self *SAwsGuestDriver) OnGuestDeployTaskDataReceived(ctx context.Context, guest *models.SGuest, task taskman.ITask, data jsonutils.JSONObject) error {
+	return nil
+}
+
+func (self *SAwsGuestDriver) RequestDiskSnapshot(ctx context.Context, guest *models.SGuest, task taskman.ITask, snapshotId, diskId string) error {
+	return nil
+}
+
+func init() {
+	driver := SAwsGuestDriver{}
+	models.RegisterGuestDriver(&driver)
+}
