1fbb35ae672b1e459d0df33920a20b754dce36c0
屈轩 <quxuan@yunionyun.com>
2018-10-29 15:47:59 +0800

    避免host name使用中文

--- pkg/util/azure/classic_host.go
+++ pkg/util/azure/classic_host.go
@@ -26,7 +26,7 @@ func (self *SClassicHost) GetId() string {
 }
 
 func (self *SClassicHost) GetName() string {
-	return fmt.Sprintf("%s(%s)-classic", self.zone.region.client.subscriptionId, self.zone.region.client.subscriptionName)
+	return fmt.Sprintf("%s-classic", self.zone.region.client.subscriptionId)
 }
 
 func (self *SClassicHost) GetGlobalId() string {
