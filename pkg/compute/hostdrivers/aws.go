fe7d54e6cb0a1fa8791416b499981494953f6907
Qiu Jian <qiujian@yunionyun.com>
2018-12-03 07:36:30 +0800

    fix aws missing init

--- pkg/compute/hostdrivers/aws.go
+++ pkg/compute/hostdrivers/aws.go
@@ -12,6 +12,11 @@ type SAwsHostDriver struct {
 	SManagedVirtualizationHostDriver
 }
 
+func init() {
+	driver := SAwsHostDriver{}
+	models.RegisterHostDriver(&driver)
+}
+
 func (self *SAwsHostDriver) GetHostType() string {
 	return models.HOST_TYPE_AWS
 }
