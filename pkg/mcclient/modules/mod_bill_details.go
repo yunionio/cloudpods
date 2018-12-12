31cb423a2f8442a1385a6f0c87e4a0bec4062e74
zhengjiajie <zhengjiajie@yunionyun.com>
2018-12-12 11:53:45 +0800

    zhengjj add files

--- pkg/mcclient/modules/mod_bill_details.go
+++ pkg/mcclient/modules/mod_bill_details.go
@@ -6,7 +6,7 @@ var (
 
 func init() {
 	BillDetails = NewMeterManager("bill_detail", "bill_details",
-		[]string{"bill_id", "account", "platform", "region", "sub_account_project", "res_id",
+		[]string{"bill_id", "account", "platform", "region", "manager_project", "res_id",
 			"res_type", "res_name", "start_time", "end_time", "charge_type", "item_rate", "item_fee"},
 		[]string{},
 	)
