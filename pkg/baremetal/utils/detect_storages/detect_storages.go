c7d0a215f0ca5f63eaf8bacad37a4d20e54ab30d
Zexi Li <lizexi@yunionyun.com>
2018-12-19 21:38:25 +0800

    baremetal: megactl

--- pkg/baremetal/utils/detect_storages/detect_storages.go
+++ pkg/baremetal/utils/detect_storages/detect_storages.go
@@ -1,9 +1,20 @@
 package detect_storages
 
 import (
+	"fmt"
+
+	"yunion.io/x/onecloud/pkg/baremetal/sysutils"
 	"yunion.io/x/onecloud/pkg/util/ssh"
 )
 
 func DetectStorageInfo(cli *ssh.Client, wait bool) (interface{}, error) {
+	raidDiskInfo := nil
+
+	pcieRet, err := cli.Run("/lib/mos/lsdisk --pcie")
+	if err != nil {
+		return nil, fmt.Errorf("Fail to retrieve PCIE DISK info")
+	}
+	pcieDiskInfo := sysutils.ParsePCIEDiskInfo(pcieRet)
+
 	return nil, nil
 }
