fdf47a1528cddd8d4611cde5b6e7fc598cf7a235
ningyu <ningyu@yunionyun.com>
2018-12-25 17:43:36 +0800

    refactor: 修改cloudwatcher的服务名称

--- pkg/mcclient/modules/mod_underutilizedinstances.go
+++ pkg/mcclient/modules/mod_underutilizedinstances.go
@@ -5,7 +5,7 @@ var (
 )
 
 func init() {
-	UnderutilizedInstances = NewCloudmonManager("underutilizedinstance", "underutilizedinstances",
+	UnderutilizedInstances = NewCloudwatcherManager("underutilizedinstance", "underutilizedinstances",
 		[]string{"id", "vm_id", "vm_name", "datetime_str", "vm_cpu", "vm_disk", "vm_memory", "vm_provider", "cpu_usage_threshold", "netio_rx_bps_threshold", "netio_tx_bps_threshold", "stastics_details"},
 		[]string{})
 
