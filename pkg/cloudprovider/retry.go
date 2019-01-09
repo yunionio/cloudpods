4fb3974367f5435985fe86d2dbae4b0aed4c129b
Qiu Jian <qiujian@yunionyun.com>
2019-01-09 16:42:59 +0800

    修正对包年包月主机续费后没有效果的问题

--- pkg/cloudprovider/retry.go
+++ pkg/cloudprovider/retry.go
@@ -3,6 +3,8 @@ package cloudprovider
 import (
 	"strings"
 	"time"
+
+	"yunion.io/x/log"
 )
 
 func IsError(err error, errs []string) bool {
@@ -25,6 +27,20 @@ func RetryOnError(tryFunc func() error, errs []string, maxTries int) error {
 			return err
 		}
 		tried += 1
+		time.Sleep(10 * time.Duration(tried) * time.Second)
+	}
+	return ErrTimeout
+}
+
+func Retry(tryFunc func() error, maxTries int) error {
+	tried := 0
+	for tried < maxTries {
+		err := tryFunc()
+		if err == nil {
+			return nil
+		}
+		tried += 1
+		log.Errorf("Tried %d fail %s", tried, err)
 		time.Sleep(10 * time.Duration(tried) * time.Second)
 	}
 	return ErrTimeout
