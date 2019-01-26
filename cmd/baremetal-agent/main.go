a57f8e46be7342d9ffe5d4885eb7dc4e7b467823
Zexi Li <lizexi@yunionyun.com>
2019-01-26 15:39:31 +0800

    baremetal: quit signal handle

--- cmd/baremetal-agent/main.go
+++ cmd/baremetal-agent/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	service.StartService()
+	service.New().StartService()
 }
--- pkg/baremetal/service/service.go
+++ pkg/baremetal/service/service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"context"
 	"os"
 
 	"yunion.io/x/log"
@@ -8,25 +9,45 @@ import (
 	"yunion.io/x/onecloud/pkg/baremetal"
 	"yunion.io/x/onecloud/pkg/baremetal/handler"
 	o "yunion.io/x/onecloud/pkg/baremetal/options"
+	"yunion.io/x/onecloud/pkg/baremetal/tasks"
 	"yunion.io/x/onecloud/pkg/cloudcommon"
 	"yunion.io/x/onecloud/pkg/cloudcommon/service"
 )
 
 type BaremetalService struct {
 	service.SServiceBase
+	isExiting bool
+}
+
+func New() *BaremetalService {
+	return &BaremetalService{}
 }
 
 func (s *BaremetalService) StartService() {
 	cloudcommon.ParseOptions(&o.Options, os.Args, "baremetal.conf", "baremetal")
 	cloudcommon.InitAuth(&o.Options.CommonOptions, s.startAgent)
 
-	s.RegisterSIGUSR1()
-
-	s.RegisterQuitSignals(func() {
-		log.Infof("Baremetal agent quit !!!")
-	})
 	app := cloudcommon.InitApp(&o.Options.CommonOptions, false)
 	handler.InitHandlers(app)
+
+	s.RegisterSIGUSR1()
+	s.RegisterQuitSignals(func() {
+		log.Infof("Baremetal agent quit !!!")
+		if s.isExiting {
+			return
+		} else {
+			s.isExiting = true
+		}
+
+		if app.IsInServe() {
+			if err := app.ShowDown(context.Background()); err != nil {
+				log.Errorf("App shutdown err: %v", err)
+			}
+		}
+		tasks.OnStop()
+		os.Exit(0)
+	})
+
 	cloudcommon.ServeForever(app, &o.Options.CommonOptions)
 }
 
