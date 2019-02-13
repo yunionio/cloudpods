e1b7854993bcf42d8fc86d36f0cae3f9ff44295e
Qiu Jian <qiujian@yunionyun.com>
2019-02-14 01:40:26 +0800

    feature: get list of cities where cloud region resides and filter cloud regions by city

--- cmd/climc/shell/cloudregions.go
+++ cmd/climc/shell/cloudregions.go
@@ -15,6 +15,8 @@ func init() {
 		Public    *bool `help:"show public cloud regions only" json:"is_public"`
 		Usable    *bool `help:"List regions where networks are usable"`
 		UsableVpc *bool `help:"List regions where VPC are usable"`
+
+		City string `help:"List regions in the specified city"`
 	}
 	R(&CloudregionListOptions{}, "cloud-region-list", "List cloud regions", func(s *mcclient.ClientSession, opts *CloudregionListOptions) error {
 		params, err := options.ListStructToParams(opts)
@@ -29,20 +31,48 @@ func init() {
 		return nil
 	})
 
+	type CloudregionCityListOptions struct {
+		Manager  string `help:"List objects belonging to the cloud provider"`
+		Account  string `help:"List objects belonging to the cloud account"`
+		Provider string `help:"List objects from the provider" choices:"VMware|Aliyun|Qcloud|Azure|Aws|Huawei|Openstack"`
+
+		Private   *bool `help:"show private cloud regions only" json:"is_private"`
+		Public    *bool `help:"show public cloud regions only" json:"is_public"`
+		Usable    *bool `help:"List regions where networks are usable"`
+		UsableVpc *bool `help:"List regions where VPC are usable"`
+	}
+	R(&CloudregionCityListOptions{}, "cloud-region-cities", "List cities where cloud region resides", func(s *mcclient.ClientSession, args *CloudregionCityListOptions) error {
+		params, err := options.StructToParams(args)
+		if err != nil {
+			return err
+		}
+		results, err := modules.Cloudregions.GetRegionCities(s, params)
+		if err != nil {
+			return err
+		}
+		listResult := modules.ListResult{}
+		listResult.Data, err = results.GetArray()
+		if err != nil {
+			return err
+		}
+		printList(&listResult, nil)
+		return nil
+	})
+
 	type CloudregionCreateOptions struct {
-		Id       string `help:"ID of the region"`
-		NAME     string `help:"Name of the region"`
-		Provider string `help:"Cloud provider"`
-		Desc     string `help:"Description"`
+		Id          string  `help:"ID of the region"`
+		NAME        string  `help:"Name of the region"`
+		Provider    string  `help:"Cloud provider"`
+		Desc        string  `help:"Description" json:"description" token:"desc"`
+		Latitude    float32 `help:"region geographical location - latitude"`
+		Longitude   float32 `help:"region geographical location - longitude"`
+		City        string  `help:"region geograpical location - city, e.g. Beijing, Frankfurt"`
+		CountryCode string  `help:"region geographical location - ISO country code, e.g. CN"`
 	}
 	R(&CloudregionCreateOptions{}, "cloud-region-create", "Create a cloud region", func(s *mcclient.ClientSession, args *CloudregionCreateOptions) error {
-		params := jsonutils.NewDict()
-		params.Add(jsonutils.NewString(args.NAME), "name")
-		if len(args.Id) > 0 {
-			params.Add(jsonutils.NewString(args.Id), "id")
-		}
-		if len(args.Desc) > 0 {
-			params.Add(jsonutils.NewString(args.Desc), "description")
+		params, err := options.StructToParams(args)
+		if err != nil {
+			return err
 		}
 		results, err := modules.Cloudregions.Create(s, params)
 		if err != nil {
@@ -74,17 +104,18 @@ func init() {
 	})
 
 	type CloudregionUpdateOptions struct {
-		ID   string `help:"ID or name of the region"`
-		Name string `help:"New name of the region"`
-		Desc string `help:"Description of the region"`
+		ID          string  `help:"ID or name of the region to update" json:"-"`
+		Name        string  `help:"New name of the region"`
+		Desc        string  `help:"Description of the region" json:"description" token:"desc"`
+		Latitude    float32 `help:"region geographical location - latitude"`
+		Longitude   float32 `help:"region geographical location - longitude"`
+		City        string  `help:"region geograpical location - city, e.g. Beijing, Frankfurt"`
+		CountryCode string  `help:"region geographical location - ISO country code, e.g. CN"`
 	}
 	R(&CloudregionUpdateOptions{}, "cloud-region-update", "Update a cloud region", func(s *mcclient.ClientSession, args *CloudregionUpdateOptions) error {
-		params := jsonutils.NewDict()
-		if len(args.Name) > 0 {
-			params.Add(jsonutils.NewString(args.Name), "name")
-		}
-		if len(args.Desc) > 0 {
-			params.Add(jsonutils.NewString(args.Desc), "description")
+		params, err := options.StructToParams(args)
+		if err != nil {
+			return err
 		}
 		results, err := modules.Cloudregions.Update(s, args.ID, params)
 		if err != nil {
--- pkg/mcclient/modules/mod_cloudregions.go
+++ pkg/mcclient/modules/mod_cloudregions.go
@@ -1,16 +1,78 @@
 package modules
 
-var (
-	Cloudregions ResourceManager
+import (
+	"yunion.io/x/jsonutils"
+
+	"sort"
+	"yunion.io/x/onecloud/pkg/mcclient"
 )
 
+type SCloudregionManager struct {
+	ResourceManager
+}
+
+var (
+	Cloudregions SCloudregionManager
+)
+
+type sCityCounter struct {
+	City    string
+	Regions int
+}
+
+type cityCounters []sCityCounter
+
+func (cc cityCounters) Len() int      { return len(cc) }
+func (cc cityCounters) Swap(i, j int) { cc[i], cc[j] = cc[j], cc[i] }
+func (cc cityCounters) Less(i, j int) bool {
+	if cc[i].Regions != cc[j].Regions {
+		return cc[i].Regions > cc[j].Regions
+	}
+	return cc[i].City < cc[j].City
+}
+
+func (this *SCloudregionManager) GetRegionCities(session *mcclient.ClientSession, params jsonutils.JSONObject) (jsonutils.JSONObject, error) {
+	paramsDict := params.(*jsonutils.JSONDict)
+	paramsDict.Set("limit", jsonutils.NewInt(0))
+
+	listResult, err := this.List(session, params)
+	if err != nil {
+		return nil, err
+	}
+
+	cities := make(map[string]int)
+	for i := range listResult.Data {
+		cityStr, _ := listResult.Data[i].GetString("city")
+		if len(cityStr) > 0 {
+			if _, ok := cities[cityStr]; ok {
+				cities[cityStr] += 1
+			} else {
+				cities[cityStr] = 1
+			}
+		}
+	}
+
+	cityList := make([]sCityCounter, len(cities))
+	i := 0
+	for k, v := range cities {
+		cityList[i] = sCityCounter{City: k, Regions: v}
+		i += 1
+	}
+
+	sort.Sort(cityCounters(cityList))
+
+	return jsonutils.Marshal(cityList), nil
+}
+
 func init() {
-	Cloudregions = NewComputeManager("cloudregion", "cloudregions",
-		[]string{"ID", "Name", "Enabled", "Status", "Provider",
-			"Latitude", "Longitude", "City", "Country_Code",
-			"vpc_count", "zone_count", "guest_count", "guest_increment_count",
-			"External_Id"},
-		[]string{})
+	Cloudregions = SCloudregionManager{
+		NewComputeManager("cloudregion", "cloudregions",
+			[]string{"ID", "Name", "Enabled", "Status", "Provider",
+				"Latitude", "Longitude", "City", "Country_Code",
+				"vpc_count", "zone_count", "guest_count", "guest_increment_count",
+				"External_Id"},
+			[]string{}),
+	}
 
 	registerCompute(&Cloudregions)
 }
