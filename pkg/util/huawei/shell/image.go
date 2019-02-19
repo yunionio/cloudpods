766ad18d1a7704fddcd259ca6bea11181ac04975
Qiu Jian <qiujian@yunionyun.com>
2019-02-19 18:07:15 +0800

    fix: fail to create huawei server with native image

--- pkg/util/huawei/shell/image.go
+++ pkg/util/huawei/shell/image.go
@@ -30,4 +30,13 @@ func init() {
 	shellutils.R(&ImageDeleteOptions{}, "image-delete", "Delete image", func(cli *huawei.SRegion, args *ImageDeleteOptions) error {
 		return cli.DeleteImage(args.ID)
 	})
+
+	shellutils.R(&ImageDeleteOptions{}, "image-show", "Show image", func(cli *huawei.SRegion, args *ImageDeleteOptions) error {
+		img, err := cli.GetImage(args.ID)
+		if err != nil {
+			return err
+		}
+		printObject(img)
+		return nil
+	})
 }
