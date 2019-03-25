f4fe2ba45f17d44d0155119c1928f969c9185dde
Qiu Jian <qiujian@yunionyun.com>
2019-03-25 22:39:53 +0800

    fix: adjust cloudaccount list columns

--- pkg/mcclient/modules/mod_cloudaccounts.go
+++ pkg/mcclient/modules/mod_cloudaccounts.go
@@ -6,9 +6,10 @@ var (
 
 func init() {
 	Cloudaccounts = NewComputeManager("cloudaccount", "cloudaccounts",
-		[]string{"ID", "Name", "Enabled", "Status", "Access_url", "Account",
-			"version", "balance", "error_count",
-			"Sync_Status", "Last_sync", "Last_sync_end_at",
+		[]string{"ID", "Name", "Enabled", "Status", "Access_url",
+			"balance", "error_count",
+			"Sync_Status", "Last_sync",
+			"guest_count",
 			"Provider", "Enable_Auto_Sync", "Sync_Interval_Seconds"},
 		[]string{})
 
