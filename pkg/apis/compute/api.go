e4737c5a72ef4761fba3839cb9047941c1c5620a
wanyaoqi <wanyaoqi@yunion.cn>
2019-04-04 22:12:32 +0800

    fix climc server-deploy (#244)

--- pkg/apis/compute/api.go
+++ pkg/apis/compute/api.go
@@ -175,7 +175,7 @@ type ServerDeployInput struct {
 	Id string
 
 	Keypair       string          `json:"keypair"`
-	DeleteKeypair bool            `json:"__delete_keypair__"`
+	DeleteKeypair *bool           `json:"__delete_keypair__"`
 	DeployConfigs []*DeployConfig `json:"deploy_configs"`
 	ResetPassword *bool           `json:"reset_password"`
 	Password      string          `json:"password"`
