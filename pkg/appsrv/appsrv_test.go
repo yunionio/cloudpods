5acea07fe64a071defbe29646954cce61fa6dbb0
Zexi <zexi.li@qq.com>
2019-04-12 17:29:36 +0800

    add https://github.com/stretchr/testify to vendor for unit tests

--- pkg/appsrv/appsrv_test.go
+++ pkg/appsrv/appsrv_test.go
@@ -15,7 +15,13 @@
 package appsrv
 
 import (
+	"context"
+	"net/http"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 func TestSplitPath(t *testing.T) {
@@ -34,4 +40,43 @@ func TestSplitPath(t *testing.T) {
 			t.Error("Split error for ", p.in, " out ", ret)
 		}
 	}
+}
+
+type ApplicationTestSuit struct {
+	suite.Suite
+	app  *Application
+	done chan<- struct{}
+}
+
+func (suite *ApplicationTestSuit) SetupTest() {
+	suite.app = NewApplication("test", 4, false)
+	suite.app.AddHandler("GET", "/delay", func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Second * 1)
+		Send(w, "delay pong")
+	})
+	suite.app.AddHandler("GET", "/panic", func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+		panic("the handler is panic")
+	})
+	suite.app.AddHandler("GET", "/delaypanic", func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Second * 1)
+		panic("the handler is delay panic")
+	})
+	//suite.goServe()
+}
+
+func (suite *ApplicationTestSuit) goServe() {
+	go func() {
+		suite.app.ListenAndServe("0.0.0.0:44444")
+	}()
+}
+
+func (suite *ApplicationTestSuit) TestHandler() {
+	app := suite.app
+	assert.True(suite.T(), assert.HTTPBodyContains(suite.T(), app.ServeHTTP, "GET", "/delay", nil, "delay pong"))
+	assert.True(suite.T(), assert.HTTPBodyContains(suite.T(), app.ServeHTTP, "GET", "/panic", nil, "the handler is panic"))
+	assert.True(suite.T(), assert.HTTPBodyContains(suite.T(), app.ServeHTTP, "GET", "/delaypanic", nil, "the handler is delay panic"))
+}
+
+func TestApplicationTestSuite(t *testing.T) {
+	suite.Run(t, new(ApplicationTestSuit))
 }
