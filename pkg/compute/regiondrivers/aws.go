bf9841caa73bd7cf37eb24ad8d4770e935b4c21f
屈轩 <quxuan@yunionyun.com>
2019-04-15 12:23:01 +0800

    避免创建不受支持的loadbalancer

--- pkg/compute/regiondrivers/aws.go
+++ pkg/compute/regiondrivers/aws.go
@@ -1,7 +1,12 @@
 package regiondrivers
 
 import (
+	"context"
+
+	"yunion.io/x/jsonutils"
 	"yunion.io/x/onecloud/pkg/compute/models"
+	"yunion.io/x/onecloud/pkg/httperrors"
+	"yunion.io/x/onecloud/pkg/mcclient"
 )
 
 type SAwsRegionDriver struct {
@@ -15,4 +20,16 @@ func init() {
 
 func (self *SAwsRegionDriver) GetProvider() string {
 	return models.CLOUD_PROVIDER_AWS
+}
+
+func (self *SAwsRegionDriver) ValidateCreateLoadbalancerData(ctx context.Context, userCred mcclient.TokenCredential, data *jsonutils.JSONDict) (*jsonutils.JSONDict, error) {
+	return nil, httperrors.NewNotImplementedError("%s does not currently support creating loadbalancer", self.GetProvider())
+}
+
+func (self *SAwsRegionDriver) ValidateCreateLoadbalancerAclData(ctx context.Context, userCred mcclient.TokenCredential, data *jsonutils.JSONDict) (*jsonutils.JSONDict, error) {
+	return nil, httperrors.NewNotImplementedError("%s does not currently support creating loadbalancer acl", self.GetProvider())
+}
+
+func (self *SAwsRegionDriver) ValidateCreateLoadbalancerCertificateData(ctx context.Context, userCred mcclient.TokenCredential, data *jsonutils.JSONDict) (*jsonutils.JSONDict, error) {
+	return nil, httperrors.NewNotImplementedError("%s does not currently support creating loadbalancer certificate", self.GetProvider())
 }
