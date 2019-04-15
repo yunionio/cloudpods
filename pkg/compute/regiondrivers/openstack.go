e0a7263b69ad6bc93fbbf08c4bc915a55948ddab
屈轩 <qu_xuan@icloud.com>
2019-04-15 16:25:10 +0800

    避免创建不受支持的loadbalancer (#410)

--- pkg/compute/regiondrivers/openstack.go
+++ pkg/compute/regiondrivers/openstack.go
@@ -1,7 +1,12 @@
 package regiondrivers
 
 import (
+	"context"
+
+	"yunion.io/x/jsonutils"
 	"yunion.io/x/onecloud/pkg/compute/models"
+	"yunion.io/x/onecloud/pkg/httperrors"
+	"yunion.io/x/onecloud/pkg/mcclient"
 )
 
 type SOpenStackRegionDriver struct {
@@ -15,4 +20,16 @@ func init() {
 
 func (self *SOpenStackRegionDriver) GetProvider() string {
 	return models.CLOUD_PROVIDER_OPENSTACK
+}
+
+func (self *SOpenStackRegionDriver) ValidateCreateLoadbalancerData(ctx context.Context, userCred mcclient.TokenCredential, data *jsonutils.JSONDict) (*jsonutils.JSONDict, error) {
+	return nil, httperrors.NewNotImplementedError("%s does not currently support creating loadbalancer", self.GetProvider())
+}
+
+func (self *SOpenStackRegionDriver) ValidateCreateLoadbalancerAclData(ctx context.Context, userCred mcclient.TokenCredential, data *jsonutils.JSONDict) (*jsonutils.JSONDict, error) {
+	return nil, httperrors.NewNotImplementedError("%s does not currently support creating loadbalancer acl", self.GetProvider())
+}
+
+func (self *SOpenStackRegionDriver) ValidateCreateLoadbalancerCertificateData(ctx context.Context, userCred mcclient.TokenCredential, data *jsonutils.JSONDict) (*jsonutils.JSONDict, error) {
+	return nil, httperrors.NewNotImplementedError("%s does not currently support creating loadbalancer certificate", self.GetProvider())
 }
