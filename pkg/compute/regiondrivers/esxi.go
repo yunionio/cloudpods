0323db57b0dd80d79d445034e7b6997dfd8c7d21
屈轩 <qu_xuan@icloud.com>
2019-04-15 16:25:10 +0800

    避免创建不受支持的loadbalancer (#411)

--- pkg/compute/regiondrivers/esxi.go
+++ pkg/compute/regiondrivers/esxi.go
@@ -1,7 +1,12 @@
 package regiondrivers
 
 import (
+	"context"
+
+	"yunion.io/x/jsonutils"
 	"yunion.io/x/onecloud/pkg/compute/models"
+	"yunion.io/x/onecloud/pkg/httperrors"
+	"yunion.io/x/onecloud/pkg/mcclient"
 )
 
 type SEsxiRegionDriver struct {
@@ -15,4 +20,16 @@ func init() {
 
 func (self *SEsxiRegionDriver) GetProvider() string {
 	return models.CLOUD_PROVIDER_VMWARE
+}
+
+func (self *SEsxiRegionDriver) ValidateCreateLoadbalancerData(ctx context.Context, userCred mcclient.TokenCredential, data *jsonutils.JSONDict) (*jsonutils.JSONDict, error) {
+	return nil, httperrors.NewUnsupportOperationError("%s does not support creating loadbalancer", self.GetProvider())
+}
+
+func (self *SEsxiRegionDriver) ValidateCreateLoadbalancerAclData(ctx context.Context, userCred mcclient.TokenCredential, data *jsonutils.JSONDict) (*jsonutils.JSONDict, error) {
+	return nil, httperrors.NewNotImplementedError("%s does not support creating loadbalancer acl", self.GetProvider())
+}
+
+func (self *SEsxiRegionDriver) ValidateCreateLoadbalancerCertificateData(ctx context.Context, userCred mcclient.TokenCredential, data *jsonutils.JSONDict) (*jsonutils.JSONDict, error) {
+	return nil, httperrors.NewNotImplementedError("%s does not support creating loadbalancer certificate", self.GetProvider())
 }
