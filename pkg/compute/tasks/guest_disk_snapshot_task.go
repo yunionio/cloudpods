356439744def50d82b9302d60362ee5f670cb9de
wanyaoqi <wanyaoqi@yunionyun.com>
2019-04-23 16:19:45 +0800

    fix aliyun snapshot delete failed, delete linked images first

--- pkg/compute/tasks/guest_disk_snapshot_task.go
+++ pkg/compute/tasks/guest_disk_snapshot_task.go
@@ -302,6 +302,8 @@ func (self *SnapshotDeleteTask) TaskFailed(ctx context.Context, snapshot *models
 	if snapshot.Status == models.SNAPSHOT_DELETING {
 		snapshot.SetStatus(self.UserCred, models.SNAPSHOT_READY, "On SnapshotDeleteTask TaskFailed")
 	}
+	db.OpsLog.LogEvent(snapshot, db.ACT_SNAPSHOT_DELETE_FAIL, reason, self.UserCred)
+	logclient.AddActionLogWithStartable(self, snapshot, logclient.ACT_DELOCATE, reason, self.UserCred, false)
 	self.SetStageFailed(ctx, reason)
 	guest, err := snapshot.GetGuest()
 	if err != nil {
