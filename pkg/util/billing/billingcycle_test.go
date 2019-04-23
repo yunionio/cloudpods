c875905f2670d51808cd736e074bf70d1cd9d858
Qiu Jian <qiujian@yunionyun.com>
2019-04-23 16:06:23 +0800

    fix: convert billingcycle to aliyun renew period error

--- pkg/util/billing/billingcycle_test.go
+++ pkg/util/billing/billingcycle_test.go
@@ -21,12 +21,12 @@ import (
 
 func TestParseBillingCycle(t *testing.T) {
 	now := time.Now().UTC()
-	for _, cycleStr := range []string{"1H", "1D", "1W", "1M", "1Y"} {
+	for _, cycleStr := range []string{"1H", "2H", "1D", "1W", "2W", "3W", "4W", "1M", "2M", "1Y", "2Y"} {
 		bc, err := ParseBillingCycle(cycleStr)
 		if err != nil {
 			t.Errorf("error parse %s: %s", cycleStr, err)
 		} else {
-			t.Logf("%s + %s = %s", now, bc.String(), bc.EndAt(now))
+			t.Logf("%s: %s + %s = %s Weeks: %d Months: %d", bc.String(), now, bc.String(), bc.EndAt(now), bc.GetWeeks(), bc.GetMonths())
 		}
 	}
 }
