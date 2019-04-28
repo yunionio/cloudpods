5a87cbfe7f72ce7f409156c55f721b1249995bcf
ioito <qu_xuan@icloud.com>
2019-04-28 15:23:44 +0800

    统一使用const,避免city命名不一致

--- pkg/util/aliyun/latitud_and_longitude.go
+++ pkg/util/aliyun/latitud_and_longitude.go
@@ -1,25 +1,28 @@
 package aliyun
 
-import "yunion.io/x/onecloud/pkg/cloudprovider"
+import (
+	api "yunion.io/x/onecloud/pkg/apis/compute"
+	"yunion.io/x/onecloud/pkg/cloudprovider"
+)
 
 var LatitudeAndLongitude = map[string]cloudprovider.SGeographicInfo{
-	"cn-qingdao":     {Latitude: 36.067108, Longitude: 120.382607, City: "Qingdao", CountryCode: "CN"},
-	"cn-beijing":     {Latitude: 39.904202, Longitude: 116.407394, City: "Beijing", CountryCode: "CN"},
-	"cn-zhangjiakou": {Latitude: 40.767544, Longitude: 114.886337, City: "Zhangjiakou", CountryCode: "CN"},
-	"cn-huhehaote":   {Latitude: 40.842358, Longitude: 111.749992, City: "Huhehaote", CountryCode: "CN"},
-	"cn-hangzhou":    {Latitude: 30.274084, Longitude: 120.155067, City: "Hangzhou", CountryCode: "CN"},
-	"cn-shanghai":    {Latitude: 31.230391, Longitude: 121.473701, City: "Shanghai", CountryCode: "CN"},
-	"cn-shenzhen":    {Latitude: 22.543097, Longitude: 114.057861, City: "Shenzhen", CountryCode: "CN"},
-	"cn-hongkong":    {Latitude: 22.396427, Longitude: 114.109497, City: "Hongkong", CountryCode: "CN"},
-	"ap-northeast-1": {Latitude: 35.709026, Longitude: 139.731995, City: "Tokyo", CountryCode: "JP"},
-	"ap-southeast-1": {Latitude: 1.352083, Longitude: 103.819839, City: "Singapore", CountryCode: "SG"},
-	"ap-southeast-2": {Latitude: -33.868820, Longitude: 151.209290, City: "Sydney", CountryCode: "AU"},
-	"ap-southeast-3": {Latitude: 3.139003, Longitude: 101.686852, City: "Kuala Lumpur", CountryCode: "MY"},
-	"ap-southeast-5": {Latitude: -6.175110, Longitude: 106.865036, City: "Jakarta", CountryCode: "ID"},
-	"ap-south-1":     {Latitude: 19.075983, Longitude: 72.877655, City: "Mumbai", CountryCode: "IN"},
-	"us-east-1":      {Latitude: 37.431572, Longitude: -78.656891, City: "Virgina", CountryCode: "US"},
-	"us-west-1":      {Latitude: 37.387474, Longitude: -122.057541, City: "Siliconvalley", CountryCode: "US"},
-	"eu-west-1":      {Latitude: 51.507351, Longitude: -0.127758, City: "London", CountryCode: "GB"},
-	"me-east-1":      {Latitude: 25.204849, Longitude: 55.270782, City: "Dubai", CountryCode: "AE"},
-	"eu-central-1":   {Latitude: 50.110924, Longitude: 8.682127, City: "Frankfurt", CountryCode: "DE"},
+	"cn-qingdao":     {Latitude: 36.067108, Longitude: 120.382607, City: api.CITY_QING_DAO, CountryCode: api.COUNTRY_CODE_CN},
+	"cn-beijing":     {Latitude: 39.904202, Longitude: 116.407394, City: api.CITY_BEI_JING, CountryCode: api.COUNTRY_CODE_CN},
+	"cn-zhangjiakou": {Latitude: 40.767544, Longitude: 114.886337, City: api.CITY_ZHANG_JIA_KOU, CountryCode: api.COUNTRY_CODE_CN},
+	"cn-huhehaote":   {Latitude: 40.842358, Longitude: 111.749992, City: api.CITY_HU_HE_HAO_TE, CountryCode: api.COUNTRY_CODE_CN},
+	"cn-hangzhou":    {Latitude: 30.274084, Longitude: 120.155067, City: api.CITY_HANG_ZHOU, CountryCode: api.COUNTRY_CODE_CN},
+	"cn-shanghai":    {Latitude: 31.230391, Longitude: 121.473701, City: api.CITY_SHANG_HAI, CountryCode: api.COUNTRY_CODE_CN},
+	"cn-shenzhen":    {Latitude: 22.543097, Longitude: 114.057861, City: api.CITY_SHEN_ZHEN, CountryCode: api.COUNTRY_CODE_CN},
+	"cn-hongkong":    {Latitude: 22.396427, Longitude: 114.109497, City: api.CITY_HONG_KONG, CountryCode: api.COUNTRY_CODE_CN},
+	"ap-northeast-1": {Latitude: 35.709026, Longitude: 139.731995, City: api.CITY_TOKYO, CountryCode: api.COUNTRY_CODE_JP},
+	"ap-southeast-1": {Latitude: 1.352083, Longitude: 103.819839, City: api.CITY_SINGAPORE, CountryCode: api.COUNTRY_CODE_SG},
+	"ap-southeast-2": {Latitude: -33.868820, Longitude: 151.209290, City: api.CITY_SYDNEY, CountryCode: api.COUNTRY_CODE_AU},
+	"ap-southeast-3": {Latitude: 3.139003, Longitude: 101.686852, City: api.CITY_KUALA_LUMPUR, CountryCode: api.COUNTRY_CODE_MY},
+	"ap-southeast-5": {Latitude: -6.175110, Longitude: 106.865036, City: api.CITY_JAKARTA, CountryCode: api.COUNTRY_CODE_ID},
+	"ap-south-1":     {Latitude: 19.075983, Longitude: 72.877655, City: api.CITY_MUMBAI, CountryCode: api.COUNTRY_CODE_IN},
+	"us-east-1":      {Latitude: 37.431572, Longitude: -78.656891, City: api.CITY_VIRGINIA, CountryCode: api.COUNTRY_CODE_US},
+	"us-west-1":      {Latitude: 37.387474, Longitude: -122.057541, City: api.CITY_SILICONVALLEY, CountryCode: api.COUNTRY_CODE_US},
+	"eu-west-1":      {Latitude: 51.507351, Longitude: -0.127758, City: api.CITY_LONDON, CountryCode: api.COUNTRY_CODE_GB},
+	"me-east-1":      {Latitude: 25.204849, Longitude: 55.270782, City: api.CITY_DUBAI, CountryCode: api.COUNTRY_CODE_AE},
+	"eu-central-1":   {Latitude: 50.110924, Longitude: 8.682127, City: api.CITY_FRANKFURT, CountryCode: api.COUNTRY_CODE_DE},
 }
--- pkg/util/huawei/latitud_and_longitude.go
+++ pkg/util/huawei/latitud_and_longitude.go
@@ -1,20 +1,23 @@
 package huawei
 
-import "yunion.io/x/onecloud/pkg/cloudprovider"
+import (
+	api "yunion.io/x/onecloud/pkg/apis/compute"
+	"yunion.io/x/onecloud/pkg/cloudprovider"
+)
 
 // China: https://developer.huaweicloud.com/endpoint
 // International: https://developer-intl.huaweicloud.com/endpoint
 // ref: https://countrycode.org
 var LatitudeAndLongitude = map[string]cloudprovider.SGeographicInfo{
-	"cn-east-2":      {Latitude: 31.210344, Longitude: 121.455364, City: "Shanghai", CountryCode: "CN"},
-	"cn-north-1":     {Latitude: 39.997743, Longitude: 116.304542, City: "Beijing", CountryCode: "CN"},
-	"cn-north-4":     {Latitude: 39.997743, Longitude: 116.304542, City: "Beijing", CountryCode: "CN"},
-	"cn-south-1":     {Latitude: 23.12911, Longitude: 113.264385, City: "Guangzhou", CountryCode: "CN"},
-	"cn-south-2":     {Latitude: 23.12911, Longitude: 113.264385, City: "Guangzhou", CountryCode: "CN"},
-	"ap-southeast-1": {Latitude: 22.396428, Longitude: 114.109497, City: "Hongkong", CountryCode: "CN"},
-	"ap-southeast-2": {Latitude: 13.7563309, Longitude: 100.5017651, City: "Bangkok", CountryCode: "TH"},
-	"ap-southeast-3": {Latitude: 1.360386, Longitude: 103.821195, City: "Singapore", CountryCode: "SG"},
-	"eu-west-0":      {Latitude: 48.856614, Longitude: 2.3522219, City: "Paris", CountryCode: "FR"},
-	"cn-northeast-1": {Latitude: 38.91400300000001, Longitude: 121.614682, City: "Shanghai", CountryCode: "CN"},
-	"cn-southwest-2": {Latitude: 26.6470035286, Longitude: 106.6302113880, City: "Guiyang", CountryCode: "CN"},
+	"cn-east-2":      {Latitude: 31.210344, Longitude: 121.455364, City: api.CITY_SHANG_HAI, CountryCode: api.COUNTRY_CODE_CN},
+	"cn-north-1":     {Latitude: 39.997743, Longitude: 116.304542, City: api.CITY_BEI_JING, CountryCode: api.COUNTRY_CODE_CN},
+	"cn-north-4":     {Latitude: 39.997743, Longitude: 116.304542, City: api.CITY_BEI_JING, CountryCode: api.COUNTRY_CODE_CN},
+	"cn-south-1":     {Latitude: 23.12911, Longitude: 113.264385, City: api.CITY_GUANG_ZHOU, CountryCode: api.COUNTRY_CODE_CN},
+	"cn-south-2":     {Latitude: 23.12911, Longitude: 113.264385, City: api.CITY_GUANG_ZHOU, CountryCode: api.COUNTRY_CODE_CN},
+	"ap-southeast-1": {Latitude: 22.396428, Longitude: 114.109497, City: api.CITY_HONG_KONG, CountryCode: api.COUNTRY_CODE_CN},
+	"ap-southeast-2": {Latitude: 13.7563309, Longitude: 100.5017651, City: api.CITY_BANGKOK, CountryCode: api.COUNTRY_CODE_TH},
+	"ap-southeast-3": {Latitude: 1.360386, Longitude: 103.821195, City: api.CITY_SINGAPORE, CountryCode: api.COUNTRY_CODE_SG},
+	"eu-west-0":      {Latitude: 48.856614, Longitude: 2.3522219, City: api.CITY_PARIS, CountryCode: api.COUNTRY_CODE_FR},
+	"cn-northeast-1": {Latitude: 38.91400300000001, Longitude: 121.614682, City: api.CITY_SHANG_HAI, CountryCode: api.COUNTRY_CODE_CN},
+	"cn-southwest-2": {Latitude: 26.6470035286, Longitude: 106.6302113880, City: api.CITY_GUI_YANG, CountryCode: api.COUNTRY_CODE_CN},
 }
