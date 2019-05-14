a92f550d50f9cd506109e35c98644f7b1870c249
Yousong Zhou <zhouyousong@yunionyun.com>
2019-05-14 12:19:46 +0000

    pkg: ansible: add inventory, module, playbook

---
+++ pkg/util/ansible/module.go
@@ -0,0 +1,68 @@
+package ansible
+
+import (
+	"bytes"
+)
+
+// Module represents name and args of ansible module to execute
+type Module struct {
+	// Name is ansible module name
+	Name string
+	// Args is a list of module arguments in form of key=value
+	Args []string
+}
+
+// Host represents an ansible host
+type Host struct {
+	// Name denotes the host to operate on
+	Name string
+	// Vars is a map recording host vars
+	Vars map[string]string
+}
+
+// GetVar returns variable value.  Second return value will be false if the
+// variable does not exist
+func (h *Host) GetVar(k string) (v string, exist bool) {
+	if len(h.Vars) == 0 {
+		return
+	}
+	v, exist = h.Vars[k]
+	return
+}
+
+// SetVar sets variable k to value v
+func (h *Host) SetVar(k, v string) {
+	if h.Vars == nil {
+		h.Vars = map[string]string{
+			k: v,
+		}
+		return
+	}
+	h.Vars[k] = v
+}
+
+// Inventory contains a list of ansible hosts
+type Inventory struct {
+	Hosts []Host
+}
+
+// IsEmpty returns true if the inventory is empty
+func (i *Inventory) IsEmpty() bool {
+	return len(i.Hosts) == 0
+}
+
+// Data returns serialized form of the inventory as represented on disk
+func (i *Inventory) Data() []byte {
+	b := &bytes.Buffer{}
+	for _, h := range i.Hosts {
+		b.WriteString(h.Name)
+		for k, v := range h.Vars {
+			b.WriteRune(' ')
+			b.WriteString(k)
+			b.WriteRune('=')
+			b.WriteString(v)
+		}
+		b.WriteRune('\n')
+	}
+	return b.Bytes()
+}
