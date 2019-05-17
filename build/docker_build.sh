344a3c6fb666a8e8043faf39111c442db1e46c3e
Zexi <zexi.li@qq.com>
2019-05-17 10:37:05 +0800

    Use docker build binaries

--- build/docker_build.sh
+++ build/docker_build.sh
@@ -7,12 +7,13 @@ set -o pipefail
 SRC_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}")/..)
 BIN_DIR=$SRC_ROOT/_output/bin
 WORK_DIR="/go/src/yunion.io/x/onecloud"
+CMDS=$@
 
 docker run --rm -it \
     -w $WORK_DIR \
     -v $SRC_ROOT:$WORK_DIR \
     d3lx/golang:yunion \
-    make
+    make $CMDS
 
 echo "Build finish"
 ls -alh $BIN_DIR
