a717e8113c253ae9ec8a0f37bb5e825b9ffa337d
wanyaoqi <wanyaoqi@yunionyun.com>
2019-05-28 15:28:18 +0800

    support add pid file

--- cmd/host/main.go
+++ cmd/host/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "yunion.io/x/onecloud/pkg/hostman"
+import (
+	"yunion.io/x/onecloud/pkg/cloudcommon/service"
+	"yunion.io/x/onecloud/pkg/hostman"
+)
 
 func main() {
-	var srv = hostman.SHostService{}
+	var srv = &hostman.SHostService{}
+	srv.SServiceBase = &service.SServiceBase{
+		Service: srv,
+	}
 	srv.StartService()
 }
