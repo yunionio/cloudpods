550b3de19727909f929aa2eae90be6872b0822b2
Zexi <zexi.li@qq.com>
2019-06-11 22:58:15 +0800

    fix: region disk create domain id

--- pkg/compute/models/disks.go
+++ pkg/compute/models/disks.go
@@ -379,7 +379,7 @@ func (manager *SDiskManager) ValidateCreateData(ctx context.Context, userCred mc
 		return nil, err
 	}
 	input.Project = ownerId.GetProjectId()
-	input.Domain = ownerId.GetDomainId()
+	input.Domain = ownerId.GetProjectDomainId()
 
 	storageID := input.Storage
 	if storageID != "" {
