990eda0c07e48e236a563847a4fdc47622984b73
ioito <qu_xuan@icloud.com>
2019-06-24 18:51:54 +0800

    fix import order

--- pkg/util/openstack/flavor.go
+++ pkg/util/openstack/flavor.go
@@ -18,11 +18,10 @@ import (
 	"fmt"
 
 	"github.com/pkg/errors"
-	"yunion.io/x/onecloud/pkg/cloudprovider"
 
 	"yunion.io/x/jsonutils"
-
 	api "yunion.io/x/onecloud/pkg/apis/compute"
+	"yunion.io/x/onecloud/pkg/cloudprovider"
 	"yunion.io/x/onecloud/pkg/multicloud"
 )
 
