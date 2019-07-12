a203e5d5071cee52d36f1cdb9397fbc8c8556acd
ioito <qu_xuan@icloud.com>
2019-07-12 11:31:16 +0800

    同优先级规则deny优先于allow规则生效

--- pkg/compute/models/guests.go
+++ pkg/compute/models/guests.go
@@ -1815,7 +1815,7 @@ func (self *SGuest) getSecurityGroupsRules() string {
 		secgroupids = append(secgroupids, secgroup.Id)
 	}
 	q := SecurityGroupRuleManager.Query()
-	q.Filter(sqlchemy.In(q.Field("secgroup_id"), secgroupids)).Desc(q.Field("priority"))
+	q.Filter(sqlchemy.In(q.Field("secgroup_id"), secgroupids)).Desc(q.Field("priority"), q.Field("action"))
 	secrules := []SSecurityGroupRule{}
 	if err := db.FetchModelObjects(SecurityGroupRuleManager, q, &secrules); err != nil {
 		log.Errorf("Get rules error: %v", err)
