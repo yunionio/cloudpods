48c39c53540923216ea6fdc6103ec63b80c45d3b
wanyaoqi <wanyaoqi@yunionyun.com>
2019-08-14 20:41:37 +0800

    add distinct field manager

--- pkg/compute/models/vpcs.go
+++ pkg/compute/models/vpcs.go
@@ -647,6 +647,11 @@ func (manager *SVpcManager) QueryDistinctExtraField(q *sqlchemy.SQuery, field st
 		q = q.Join(cloudaccounts, sqlchemy.Equals(cloudproviders.Field("cloudaccount_id"), cloudaccounts.Field("id")))
 		q.GroupBy(cloudaccounts.Field("name"))
 		q.AppendField(cloudaccounts.Field("name", "account"))
+	case "manager":
+		cloudproviders := CloudproviderManager.Query("name", "id").Distinct().SubQuery()
+		q = q.Join(cloudproviders, sqlchemy.Equals(q.Field("manager_id"), cloudproviders.Field("id")))
+		q.GroupBy(cloudproviders.Field("name"))
+		q.AppendField(cloudproviders.Field("name", "manager"))
 	default:
 		return nil, httperrors.NewBadRequestError("unsupport field %s", field)
 	}
