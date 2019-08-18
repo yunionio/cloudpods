244be990ae1cc5a3e2c8067e8aba9bf9de182128
Qiu Jian <qiujian@yunionyun.com>
2019-08-19 01:57:32 +0800

    fix: not sync correctly aws VM's tempoaral public ip

--- pkg/mcclient/modules/mod_elasticips.go
+++ pkg/mcclient/modules/mod_elasticips.go
@@ -20,7 +20,7 @@ var (
 
 func init() {
 	Elasticips = NewComputeManager("eip", "eips",
-		[]string{"ID", "Name", "IP_Addr", "Status",
+		[]string{"ID", "Name", "Mode", "IP_Addr", "Status",
 			"Associate_Type", "Associate_ID",
 			"Associate_Name",
 			"Bandwidth", "Charge_Type",
