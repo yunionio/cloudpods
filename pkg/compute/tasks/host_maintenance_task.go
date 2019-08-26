8a0d239fcebcfc3bb996e0c2c030db56d0d606fe
wanyaoqi <wanyaoqi@yunionyun.com>
2019-08-26 11:30:00 +0800

    fix host maintenance

--- pkg/compute/tasks/host_maintenance_task.go
+++ pkg/compute/tasks/host_maintenance_task.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"yunion.io/x/jsonutils"
-	"yunion.io/x/log"
 
 	api "yunion.io/x/onecloud/pkg/apis/compute"
 	"yunion.io/x/onecloud/pkg/cloudcommon/db"
@@ -23,74 +22,34 @@ func init() {
 
 func (self *HostMaintainTask) OnInit(ctx context.Context, obj db.IStandaloneModel, data jsonutils.JSONObject) {
 	host := obj.(*models.SHost)
-	guests := host.GetGuests()
-	preferHostId, _ := data.GetString("prefer_host_id")
+	guests, _ := self.Params.Get("guests")
+	preferHostId, _ := self.Params.Get("prefer_host_id")
 
-	var doMigrate bool
-	var hasGuestCannotMigrate bool
-	for i := 0; i < len(guests); i++ {
-		if guests[i].Status == api.VM_READY || guests[i].Status == api.VM_UNKNOWN {
-			rescueMode := guests[i].Status == api.VM_UNKNOWN
-			if rescueMode {
-				guestDisks := guests[i].GetDisks()
-				var allDiskIsShared = true
-				for _, guestDisk := range guestDisks {
-					if guestDisk.GetDisk().GetStorage().StorageType == api.STORAGE_LOCAL {
-						allDiskIsShared = false
-						break
-					}
-				}
-				if !allDiskIsShared {
-					hasGuestCannotMigrate = true
-					continue
-				}
-			}
-			err := guests[i].StartMigrateTask(ctx, self.UserCred, rescueMode, false, guests[i].Status, preferHostId, self.Id)
-			if err != nil {
-				log.Errorf("Start migrate task failed %s", err)
-				hasGuestCannotMigrate = true
-				continue
-			} else {
-				log.Infof("Start migrate %s(%s)", guests[i].Name, guests[i].Id)
-				doMigrate = true
-				break
-			}
-		} else if guests[i].Status == api.VM_RUNNING {
-			err := guests[i].StartGuestLiveMigrateTask(ctx, self.UserCred, guests[i].Status, preferHostId, self.Id)
-			if err != nil {
-				log.Errorf("Start migrate task failed %s", err)
-				hasGuestCannotMigrate = true
-				continue
-			} else {
-				log.Infof("Start migrate %s(%s)", guests[i].Name, guests[i].Id)
-				doMigrate = true
-				break
-			}
-		}
-	}
-	if hasGuestCannotMigrate {
-		kwargs := jsonutils.NewDict()
-		kwargs.Set("some_guest_migrate_failed", jsonutils.JSONTrue)
-		self.SaveParams(kwargs)
-	}
-
-	if !doMigrate {
-		hostStatus := api.HOST_MAINTAINING
-		if jsonutils.QueryBoolean(self.Params, "some_guest_migrate_failed", false) {
-			hostStatus = api.HOST_MAINTAIN_FAILE
-		}
-		host.PerformDisable(ctx, self.UserCred, nil, nil)
-		host.SetStatus(self.UserCred, hostStatus, "On host maintain task complete")
-		logclient.AddSimpleActionLog(host, logclient.ACT_HOST_MAINTAINING, "host maintain", self.UserCred, hostStatus == api.HOST_MAINTAINING)
-		self.SetStageComplete(ctx, nil)
+	kwargs := jsonutils.NewDict()
+	kwargs.Set("guests", guests)
+	kwargs.Set("prefer_host_id", preferHostId)
+	self.SetStage("OnGuestsMigrate", nil)
+	err := models.GuestManager.StartHostGuestsMigrateTask(ctx, self.UserCred, host, self.Params, self.Id)
+	if err != nil {
+		self.TaskFailed(ctx, host, err.Error())
 		return
 	}
 }
 
-// Ignore guest migrate fail
-func (self *HostMaintainTask) OnInitFailed(ctx context.Context, obj db.IStandaloneModel, data jsonutils.JSONObject) {
-	kwargs := jsonutils.NewDict()
-	kwargs.Set("some_guest_migrate_failed", jsonutils.JSONTrue)
-	self.SaveParams(kwargs)
-	self.OnInit(ctx, obj, data)
+func (self *HostMaintainTask) OnGuestsMigrate(ctx context.Context, host *models.SHost, data jsonutils.JSONObject) {
+	host.PerformDisable(ctx, self.UserCred, nil, nil)
+	host.SetStatus(self.UserCred, api.HOST_MAINTAINING, "On host maintain task complete")
+	logclient.AddSimpleActionLog(host, logclient.ACT_HOST_MAINTAINING, "host maintain", self.UserCred, true)
+	self.SetStageComplete(ctx, nil)
+}
+
+func (self *HostMaintainTask) OnGuestsMigrateFailed(ctx context.Context, host *models.SHost, data jsonutils.JSONObject) {
+	self.TaskFailed(ctx, host, data.String())
+}
+
+func (self *HostMaintainTask) TaskFailed(ctx context.Context, host *models.SHost, reason string) {
+	host.PerformDisable(ctx, self.UserCred, nil, nil)
+	host.SetStatus(self.UserCred, api.HOST_MAINTAIN_FAILE, "On host maintain task complete failed")
+	logclient.AddSimpleActionLog(host, logclient.ACT_HOST_MAINTAINING, "host maintain", self.UserCred, false)
+	self.SetStageFailed(ctx, reason)
 }
