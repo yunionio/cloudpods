a82f8f6cffe628457c4606f9ed74e5904a37a602
Qiu Jian <qiujian@yunionyun.com>
2019-10-06 00:11:41 +0800

    add doc and copyright

--- pkg/util/redfish/doc.go
+++ pkg/util/redfish/doc.go
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package cache // import "yunion.io/x/onecloud/pkg/notify/cache"
+package redfish // import "yunion.io/x/onecloud/pkg/util/redfish"
