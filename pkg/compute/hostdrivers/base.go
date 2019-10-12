43825e3d3b8a2aefe539079245939e97397fb78a
TangBin <tangbin@yunion.cn>
2019-10-12 15:20:25 +0800

    huawei & aws bugfix

--- pkg/compute/hostdrivers/base.go
+++ pkg/compute/hostdrivers/base.go
@@ -38,6 +38,10 @@ func (self *SBaseHostDriver) ValidateUpdateDisk(ctx context.Context, userCred mc
 	return data, nil
 }
 
+func (self *SBaseHostDriver) ValidateResetDisk(ctx context.Context, userCred mcclient.TokenCredential, disk *models.SDisk, data *jsonutils.JSONDict) (*jsonutils.JSONDict, error) {
+	return data, nil
+}
+
 func (self *SBaseHostDriver) ValidateAttachStorage(ctx context.Context, userCred mcclient.TokenCredential, host *models.SHost, storage *models.SStorage, data *jsonutils.JSONDict) error {
 	return httperrors.NewNotImplementedError("Not Implement ValidateAttachStorage")
 }
