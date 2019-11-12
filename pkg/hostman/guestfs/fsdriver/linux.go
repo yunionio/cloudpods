2191ae62d660ea028813551079cec9bbec3d7669
Qiu Jian <qiujian@yunionyun.com>
2019-11-12 23:44:20 +0800

    fix: nic teaming/bonding not working properly

--- pkg/hostman/guestfs/fsdriver/linux.go
+++ pkg/hostman/guestfs/fsdriver/linux.go
@@ -594,6 +594,7 @@ func (d *sDebianLikeRootFs) DeployNetworkingScripts(rootFs IDiskPartition, nics 
 			cmds.WriteString("\n")
 		}
 	}
+	log.Debugf("%s", cmds.String())
 	return rootFs.FilePutContents(fn, cmds.String(), false, false)
 }
 
@@ -975,6 +976,7 @@ func (r *sRedhatLikeRootFs) deployNetworkingScripts(rootFs IDiskPartition, nics 
 			cmds.WriteString("BOOTPROTO=dhcp\n")
 		}
 		var fn = fmt.Sprintf("/etc/sysconfig/network-scripts/ifcfg-%s", nicDesc.Name)
+		log.Debugf("%s: %s", fn, cmds.String())
 		if err := rootFs.FilePutContents(fn, cmds.String(), false, false); err != nil {
 			return err
 		}
