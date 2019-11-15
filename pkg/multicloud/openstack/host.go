b4c4a14af06b1068ef955ee1f7372d7edf83683e
Qu Xuan <quxuan@yunionyun.com>
2019-11-15 12:06:38 +0800

    fix: cpuCmtbound sync for zstack

--- pkg/multicloud/openstack/host.go
+++ pkg/multicloud/openstack/host.go
@@ -27,6 +27,7 @@ import (
 
 	api "yunion.io/x/onecloud/pkg/apis/compute"
 	"yunion.io/x/onecloud/pkg/cloudprovider"
+	"yunion.io/x/onecloud/pkg/multicloud"
 )
 
 const (
@@ -56,6 +57,7 @@ type SResource struct {
 }
 
 type SHost struct {
+	multicloud.SHostBase
 	zone *SZone
 
 	CpuInfo string
