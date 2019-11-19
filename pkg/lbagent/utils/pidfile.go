26ea46bac271a512a9da4befad15737811cbe32b
Yousong Zhou <zhouyousong@yunionyun.com>
2019-11-19 08:21:53 +0000

    lbagent: pidfile: remove ReadPidFile()

--- pkg/lbagent/utils/pidfile.go
+++ pkg/lbagent/utils/pidfile.go
@@ -99,26 +99,6 @@ func (pf *PidFile) ConfirmOrUnlink() (proc *os.Process, confirmed bool, err erro
 	return
 }
 
-func ReadPidFile(pidFile string) *os.Process {
-	data, err := ioutil.ReadFile(pidFile)
-	if err != nil {
-		return nil
-	}
-	s := strings.TrimSpace(string(data))
-	pid, err := strconv.Atoi(s)
-	if err != nil {
-		return nil
-	}
-	proc, err := os.FindProcess(pid)
-	if err != nil {
-		return nil
-	}
-	if err := proc.Signal(syscall.Signal(0)); err != nil {
-		return nil
-	}
-	return proc
-}
-
 func WritePidFile(pid int, pidFile string) error {
 	data := fmt.Sprintf("%d\n", pid)
 	err := ioutil.WriteFile(pidFile, []byte(data), FileModeFile)
