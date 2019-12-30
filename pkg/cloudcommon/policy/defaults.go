5552d6ea8ef865d0b87d3fe4ea6d6b17332beee9
TangBin <tangbin@yunion.cn>
2019-12-30 18:39:49 +0800

    update rbac default policy

--- pkg/cloudcommon/policy/defaults.go
+++ pkg/cloudcommon/policy/defaults.go
@@ -180,6 +180,18 @@ var (
 					Action:   PolicyActionCreate,
 					Result:   rbacutils.Allow,
 				},
+				{
+					Service:  identityapi.SERVICE_TYPE,
+					Resource: "services",
+					Action:   PolicyActionGet,
+					Result:   rbacutils.Allow,
+				},
+				{
+					Service:  identityapi.SERVICE_TYPE,
+					Resource: "services",
+					Action:   PolicyActionList,
+					Result:   rbacutils.Allow,
+				},
 			},
 		},
 		{
@@ -451,18 +463,6 @@ var (
 					Service:  identityapi.SERVICE_TYPE,
 					Resource: "domains",
 					Action:   PolicyActionGet,
-					Result:   rbacutils.Allow,
-				},
-				{
-					Service:  identityapi.SERVICE_TYPE,
-					Resource: "services",
-					Action:   PolicyActionGet,
-					Result:   rbacutils.Allow,
-				},
-				{
-					Service:  identityapi.SERVICE_TYPE,
-					Resource: "services",
-					Action:   PolicyActionList,
 					Result:   rbacutils.Allow,
 				},
 			},
