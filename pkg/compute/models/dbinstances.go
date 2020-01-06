4bb58627e396573860092f4722ddc6a0e02a1482
Qu Xuan <quxuan@yunionyun.com>
2020-01-06 11:39:27 +0800

    fix: 加强密码校验

--- pkg/compute/models/dbinstances.go
+++ pkg/compute/models/dbinstances.go
@@ -39,6 +39,7 @@ import (
 	"yunion.io/x/onecloud/pkg/httperrors"
 	"yunion.io/x/onecloud/pkg/mcclient"
 	"yunion.io/x/onecloud/pkg/util/billing"
+	"yunion.io/x/onecloud/pkg/util/seclib2"
 	"yunion.io/x/onecloud/pkg/util/stringutils2"
 )
 
@@ -178,6 +179,12 @@ func (man *SDBInstanceManager) ValidateCreateData(ctx context.Context, userCred 
 	err := data.Unmarshal(input)
 	if err != nil {
 		return nil, errors.Wrapf(err, "Unmarshal input failed: %v", err)
+	}
+
+	if len(input.Password) > 0 {
+		if !seclib2.MeetComplxity(input.Password) {
+			return nil, httperrors.NewWeakPasswordError()
+		}
 	}
 
 	network := networkV.Model.(*SNetwork)
