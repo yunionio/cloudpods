34743eac7ed87a8e24043f1ab225db5e04235dc4
Yousong Zhou <zhouyousong@yunionyun.com>
2020-01-07 18:04:26 +0800

    hostman: distro name OpenWrt instead of OpenWRT

--- pkg/hostman/guestfs/fsdriver/linux.go
+++ pkg/hostman/guestfs/fsdriver/linux.go
@@ -1327,7 +1327,7 @@ func NewOpenWrtRootFs(part IDiskPartition) IRootFsDriver {
 }
 
 func (d *SOpenWrtRootFs) GetName() string {
-	return "OpenWRT"
+	return "OpenWrt"
 }
 
 func (d *SOpenWrtRootFs) String() string {
@@ -1356,7 +1356,7 @@ func (d *SOpenWrtRootFs) DeployHostname(rootFs IDiskPartition, hn, domain string
 func (d *SOpenWrtRootFs) GetReleaseInfo(rootFs IDiskPartition) *deployapi.ReleaseInfo {
 	ver, _ := rootFs.FileGetContents("/etc/openwrt_version", false)
 	return &deployapi.ReleaseInfo{
-		Distro:  "OpenWRT",
+		Distro:  "OpenWrt",
 		Version: string(ver),
 		Arch:    d.GetArch(rootFs),
 	}
