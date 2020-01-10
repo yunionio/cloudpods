496bf0729dab586aca6f0780c1c528859560f664
TangBin <tangbin@yunion.cn>
2020-01-10 17:15:57 +0800

    spelling mistakes fix

--- pkg/compute/models/capabilities.go
+++ pkg/compute/models/capabilities.go
@@ -128,11 +128,11 @@ func getRegionZoneSubq(region *SCloudregion) *sqlchemy.SSubQuery {
 	return ZoneManager.Query("id").Equals("cloudregion_id", region.GetId()).SubQuery()
 }
 
-func domainManagerFiledFilter(domainId, filed string) *sqlchemy.SSubQuery {
+func domainManagerFieldFilter(domainId, field string) *sqlchemy.SSubQuery {
 	providers := CloudproviderManager.Query().SubQuery()
 	accounts := CloudaccountManager.Query().SubQuery()
 
-	q := providers.Query(providers.Field(filed))
+	q := providers.Query(providers.Field(field))
 	q = q.Join(accounts, sqlchemy.Equals(accounts.Field("id"), providers.Field("cloudaccount_id")))
 	q = q.Filter(sqlchemy.OR(
 		sqlchemy.AND(
@@ -152,11 +152,11 @@ func domainManagerFiledFilter(domainId, filed string) *sqlchemy.SSubQuery {
 }
 
 func getDomainManagerSubq(domainId string) *sqlchemy.SSubQuery {
-	return domainManagerFiledFilter(domainId, "id")
+	return domainManagerFieldFilter(domainId, "id")
 }
 
 func getDomainManagerProviderSubq(domainId string) *sqlchemy.SSubQuery {
-	return domainManagerFiledFilter(domainId, "provider")
+	return domainManagerFieldFilter(domainId, "provider")
 }
 
 func getDBInstanceInfo(region *SCloudregion, zone *SZone) map[string]map[string]map[string][]string {
