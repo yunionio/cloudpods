a5528ecbaeefca7572c7be73ae5b41a19d5b3b99
zhaoxiangchun <zhaoxiangchun@yunion.cn>
2020-02-13 11:35:41 +0800

    zstack监控功能
    monitor.go中GetMonitorData函数通过传入的参数拼装请求参数，然后调用zstack.go中的getMonitor()，通过GET请求，获取相应的
    指标监控数据

--- pkg/util/iproute2/doc.go
+++ pkg/util/iproute2/doc.go
@@ -12,25 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package shell
-
-import (
-	"yunion.io/x/onecloud/pkg/multicloud/aws"
-	"yunion.io/x/onecloud/pkg/util/shellutils"
-)
-
-func init() {
-	type RouteTableListOptions struct {
-		VpcId string `vpc id`
-	}
-	shellutils.R(&RouteTableListOptions{}, "routetable-list", "List route tables", func(cli *aws.SRegion, args *RouteTableListOptions) error {
-		routetables, err := cli.GetRouteTables(args.VpcId, false)
-		if err != nil {
-			printObject(err)
-			return nil
-		}
-
-		printList(routetables, 0, 0, 0, nil)
-		return nil
-	})
-}
+package iproute2 // import "yunion.io/x/onecloud/pkg/util/iproute2"
---
+++ pkg/util/iproute2/route.go
@@ -0,0 +1,156 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package iproute2
+
+import (
+	"net"
+
+	"github.com/vishvananda/netlink"
+
+	"yunion.io/x/pkg/errors"
+)
+
+const (
+	errBadIP = errors.Error("bad ip")
+)
+
+type Route struct {
+	*Link
+}
+
+func NewRoute(ifname string) *Route {
+	l := NewLink(ifname)
+	route := &Route{
+		Link: l,
+	}
+	return route
+}
+
+func (route *Route) link() (link netlink.Link, ok bool) {
+	link = route.Link.link
+	if link != nil {
+		ok = true
+	}
+	return
+}
+
+func (route *Route) List4() ([]netlink.Route, error) {
+	link, ok := route.link()
+	if !ok {
+		return nil, route.Err()
+	}
+
+	rs, err := netlink.RouteList(link, netlink.FAMILY_V4)
+	if err != nil {
+		route.addErr(err, "route list")
+		return nil, route.Err()
+	}
+	for i := range rs {
+		if rs[i].Dst == nil {
+			// make the return value easier to work with
+			rs[i].Dst = &net.IPNet{
+				IP:   net.IPv4zero,
+				Mask: net.IPMask(net.IPv4zero),
+			}
+		}
+	}
+	return rs, nil
+}
+
+func (route *Route) AddByIPNet(ipnet *net.IPNet, gw net.IP) *Route {
+	link, ok := route.link()
+	if !ok {
+		return route
+	}
+
+	r := &netlink.Route{
+		LinkIndex: link.Attrs().Index,
+		Dst:       ipnet,
+	}
+	if len(gw) > 0 {
+		r.Gw = gw
+	}
+	if err := netlink.RouteReplace(r); err != nil {
+		route.addErr(err, "RouteReplace %s", r.String())
+	}
+	return route
+}
+
+func (route *Route) AddByCidr(cidr string, gwStr string) *Route {
+	var (
+		dst *net.IPNet
+		gw  net.IP
+		err error
+	)
+	if _, dst, err = net.ParseCIDR(cidr); err != nil {
+		route.addErr(err, "parse cidr")
+		return route
+	}
+
+	if gwStr != "" {
+		gw = net.ParseIP(gwStr)
+		if len(gw) == 0 {
+			route.addErr(errBadIP, "gwStr: %s", gwStr)
+			return route
+		}
+	}
+
+	return route.AddByIPNet(dst, gw)
+}
+
+func (route *Route) Add(netStr, maskStr, gwStr string) *Route {
+	var (
+		ip   net.IP
+		mask net.IPMask
+		gw   net.IP
+	)
+
+	if ip = net.ParseIP(netStr); len(ip) == 0 {
+		route.addErr(errBadIP, "netStr %s", netStr)
+		return route
+	}
+	if maskIp := net.ParseIP(maskStr); len(maskIp) == 0 {
+		route.addErr(errBadIP, "maskStr %s", maskStr)
+		return route
+	} else {
+		if ip := maskIp.To4(); len(ip) > 0 {
+			maskIp = ip
+		}
+		mask = net.IPMask(maskIp)
+		ones, bits := mask.Size()
+		if ones == 0 && bits == 0 {
+			route.addErr(errBadIP, "bad mask %s", maskStr)
+			return route
+		}
+	}
+	if gwStr != "" {
+		if gw = net.ParseIP(gwStr); len(gw) == 0 {
+			route.addErr(errBadIP, "gwStr %s", gwStr)
+			return route
+		}
+	}
+
+	ipnet := &net.IPNet{
+		IP:   ip,
+		Mask: mask,
+	}
+	return route.AddByIPNet(ipnet, gw)
+}
+
+func RouteGetByDst(dstStr string) ([]netlink.Route, error) {
+	dstIp := net.ParseIP(dstStr)
+	routes, err := netlink.RouteGet(dstIp)
+	return routes, err
+}
