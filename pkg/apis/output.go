1b861a41f9bbc7262affae671e11903e32b54cdf
Qiu Jian <qiujian@yunionyun.com>
2020-04-22 22:36:15 +0800

    fix: opslog create fail

--- pkg/apis/output.go
+++ pkg/apis/output.go
@@ -14,6 +14,10 @@
 
 package apis
 
+import (
+	"time"
+)
+
 type ModelBaseDetails struct {
 	Meta
 
@@ -159,4 +163,35 @@ type EnabledStatusInfrasResourceBaseDetails struct {
 
 type ChangeOwnerCandidateDomainsOutput struct {
 	Candidates []SharedDomain `json:"candidates"`
+}
+
+type OpsLogDetails struct {
+	ModelBaseDetails
+
+	Id      int64  `json:"id"`
+	ObjType string `json:"obj_type"`
+	ObjId   string `json:"obj_id"`
+	ObjName string `json:"obj_name"`
+	Action  string `json:"action"`
+	Notes   string `json:"notes"`
+
+	ProjectId string `json:"tenant_id"`
+	Project   string `json:"tenant"`
+
+	ProjectDomainId string `json:"project_domain_id"`
+	ProjectDomain   string `json:"project_domain"`
+
+	UserId   string `json:"user_id"`
+	User     string `json:"user"`
+	DomainId string `json:"domain_id"`
+	Domain   string `json:"domain"`
+	Roles    string `json:"roles"`
+
+	OpsTime time.Time `json:"ops_time"`
+
+	OwnerDomainId  string `json:"owner_domain_id"`
+	OwnerProjectId string `json:"owner_project_id"`
+
+	OwnerDomain  string `json:"owner_domain"`
+	OwnerProject string `json:"owner_tenant"`
 }
