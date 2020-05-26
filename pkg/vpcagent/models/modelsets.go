94d14b186316824fdc9f6665a03a7d0e87bde4a1
Yousong Zhou <zhouyousong@yunionyun.com>
2020-05-26 17:42:18 +0800

    vpcagent: models: separate Copy() and CopyJoined()

--- pkg/vpcagent/models/modelsets.go
+++ pkg/vpcagent/models/modelsets.go
@@ -71,7 +71,7 @@ func (mss *ModelSets) NewEmpty() apihelper.IModelSets {
 	return NewModelSets()
 }
 
-func (mss *ModelSets) Copy() apihelper.IModelSets {
+func (mss *ModelSets) copy_() *ModelSets {
 	mssCopy := &ModelSets{
 		Vpcs:          mss.Vpcs.Copy().(Vpcs),
 		Networks:      mss.Networks.Copy().(Networks),
@@ -79,12 +79,17 @@ func (mss *ModelSets) Copy() apihelper.IModelSets {
 		Hosts:         mss.Hosts.Copy().(Hosts),
 		Guestnetworks: mss.Guestnetworks.Copy().(Guestnetworks),
 	}
-	mssCopy.join()
 	return mssCopy
 }
 
+func (mss *ModelSets) Copy() apihelper.IModelSets {
+	return mss.copy_()
+}
+
 func (mss *ModelSets) CopyJoined() apihelper.IModelSets {
-	return mss.Copy()
+	mssCopy := mss.copy_()
+	mssCopy.join()
+	return mssCopy
 }
 
 func (mss *ModelSets) ApplyUpdates(mssNews apihelper.IModelSets) apihelper.ModelSetsUpdateResult {
