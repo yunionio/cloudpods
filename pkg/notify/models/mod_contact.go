17721c4d5967ff982c671500c7772e8d5f67eb35
rainzm <mjoycarry@gmail.com>
2020-07-01 16:49:54 +0800

    fix the bug introduced in #6643

--- pkg/notify/models/mod_contact.go
+++ pkg/notify/models/mod_contact.go
@@ -296,6 +296,8 @@ func (self *SContactManager) ListItemFilter(ctx context.Context, q *sqlchemy.SQu
 		q = q.Equals("uid", userCred.GetUserId())
 	}
 
+	q = q.GroupBy("uid").Desc("created_at")
+
 	return q, nil
 }
 
