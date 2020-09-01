06bd735f0bdb89a371a63c6f96c67e9d5065086e
zhaoxiangchun <1422928955@qq.com>
2020-09-01 20:07:14 +0800

    alert record bugfix
    1.return details fix

--- pkg/monitor/models/alertrecord.go
+++ pkg/monitor/models/alertrecord.go
@@ -137,11 +137,14 @@ func (man *SAlertRecordManager) FetchCustomizeColumns(
 
 func (record *SAlertRecord) GetMoreDetails(out monitor.AlertRecordDetails) (monitor.AlertRecordDetails, error) {
 	evalMatchs := make([]monitor.EvalMatch, 0)
-	err := record.EvalData.Unmarshal(evalMatchs)
-	if err != nil {
-		return out, errors.Wrap(err, "record Unmarshal evalMatchs error")
+	if record.EvalData != nil {
+		err := record.EvalData.Unmarshal(&evalMatchs)
+		if err != nil {
+			return out, errors.Wrap(err, "record Unmarshal evalMatchs error")
+		}
+		out.ResNum = int64(len(evalMatchs))
 	}
-	out.ResNum = int64(len(evalMatchs))
+
 	return out, nil
 }
 
