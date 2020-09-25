6037c84a58bbeded7d0dcedccc8fd414fc77651f
zhaoxiangchun <1422928955@qq.com>
2020-09-25 10:41:45 +0800

    bugfix:commonalert
    1.suggestrule cli 调整

--- pkg/mcclient/modules/mod_commonalert.go
+++ pkg/mcclient/modules/mod_commonalert.go
@@ -21,7 +21,7 @@ func init() {
 
 func NewCommonAlertManager() *SCommonAlertManager {
 	man := NewMonitorV2Manager("commonalert", "commonalerts",
-		[]string{"id", "name", "level", "alert_type", "period", "recipients", "channel"},
+		[]string{"id", "name", "enabled", "level", "alert_type", "period", "recipients", "channel"},
 		[]string{})
 	return &SCommonAlertManager{
 		ResourceManager: &man,
