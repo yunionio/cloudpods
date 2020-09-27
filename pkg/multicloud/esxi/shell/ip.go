8528d79817e30a067c0f85cfc4da70b9f9a21bdb
rainzm <mjoycarry@gmail.com>
2020-09-27 22:01:42 +0800

    feat: add 'ip-list' for esxicli

--- pkg/multicloud/esxi/shell/ip.go
+++ pkg/multicloud/esxi/shell/ip.go
@@ -15,41 +15,140 @@
 package shell
 
 import (
-	"fmt"
+	"sort"
+
+	"yunion.io/x/pkg/errors"
+	"yunion.io/x/pkg/util/netutils"
 
 	"yunion.io/x/onecloud/pkg/multicloud/esxi"
 	"yunion.io/x/onecloud/pkg/util/shellutils"
 )
 
 func init() {
-	type IPOption struct {
-		Host bool
+	type IPListOption struct {
+		Host bool   `help:"List only host ips"`
+		Vm   bool   `help:"List only vm ips"`
+		Cidr string `help:"Filter ips in this cidr, such as 192.168.10.0/24"`
 	}
-	shellutils.R(&IPOption{}, "ip-all", "List all ip", func(cli *esxi.SESXiClient, args *IPOption) error {
-		hostIps, _, err := cli.AllHostIP()
-		if err != nil {
-			return err
+	shellutils.R(&IPListOption{}, "ip-list", "List all ip", func(cli *esxi.SESXiClient, args *IPListOption) error {
+		host, vm := args.Host, args.Vm
+		if !host && !vm {
+			host, vm = true, true
 		}
-		for name, ip := range hostIps {
-			fmt.Printf("name: %s, ip: %s\n", name, ip)
+		var cidr netutils.IPV4Prefix
+		var err error
+		if len(args.Cidr) > 0 {
+			cidr, err = netutils.NewIPV4Prefix(args.Cidr)
+			if err != nil {
+				return errors.Wrap(err, "unable to parse cidr")
+			}
 		}
-		// for i := range hosts {
-		// 	fmt.Printf("host %s: \n", hosts[i].Name)
-		// 	vmips, err := cli.VMIP(hosts[i])
-		// 	if err != nil {
-		// 		return err
-		// 	}
-		// 	for name, ip := range vmips {
-		// 		fmt.Printf("\tname: %s, ip: %s\n", name, ip)
-		// 	}
-		// }
-		vmips, err := cli.VMIP2()
-		if err != nil {
-			return err
+		list := make([]IPDetails, 0, 5)
+		if host {
+			hostIps, _, err := cli.AllHostIP()
+			if err != nil {
+				return errors.Wrap(err, "AllHostIP")
+			}
+			for n, ip := range hostIps {
+				ipaddr, err := netutils.NewIPV4Addr(ip)
+				if err != nil {
+					return errors.Wrapf(err, "NewIPV4Addr for ip %s", ip)
+				}
+				if len(args.Cidr) > 0 && !cidr.Contains(ipaddr) {
+					continue
+				}
+				list = append(list, IPDetails{
+					Name:   n,
+					Type:   "host",
+					Ip:     ip,
+					Ipaddr: ipaddr,
+				})
+			}
 		}
-		for name, ip := range vmips {
-			fmt.Printf("\tname: %s, ip: %s\n", name, ip)
+		if vm {
+			vmipinfos, err := cli.VMIP2()
+			if err != nil {
+				return errors.Wrap(err, "VMIP2")
+			}
+			//split
+			for i := range vmipinfos {
+				for _, macip := range vmipinfos[i].MacIps {
+					mac := macip.Mac
+					if len(macip.IPs) == 0 {
+						list = append(list, IPDetails{
+							Name:       vmipinfos[i].Name,
+							Mac:        mac,
+							Uuid:       vmipinfos[i].Uuid,
+							Moid:       vmipinfos[i].Moid,
+							PowerState: vmipinfos[i].PowerState,
+							Type:       "vm",
+							Ip:         "",
+							Ipaddr:     0,
+						})
+						continue
+					}
+					ip := macip.IPs[0]
+					ipaddr, err := netutils.NewIPV4Addr(ip)
+					if err != nil {
+						return errors.Wrapf(err, "NewIPV4Addr for ip %s", ip)
+					}
+					if len(args.Cidr) > 0 && !cidr.Contains(ipaddr) {
+						continue
+					}
+
+					list = append(list, IPDetails{
+						Name:       vmipinfos[i].Name,
+						Mac:        mac,
+						Uuid:       vmipinfos[i].Uuid,
+						Moid:       vmipinfos[i].Moid,
+						PowerState: vmipinfos[i].PowerState,
+						Type:       "vm",
+						Ip:         ip,
+						Ipaddr:     ipaddr,
+					})
+				}
+			}
+
 		}
+		sort.Slice(list, func(i, j int) bool {
+			return list[i].Ipaddr < list[j].Ipaddr
+		})
+		printList(list, []string{"name", "type", "ip", "mac", "Power_State", "uuid", "moid"})
 		return nil
 	})
+}
+
+type IPDetails struct {
+	Moid       string
+	PowerState string
+	Uuid       string
+	Name       string
+	Type       string
+	Ip         string
+	Mac        string
+	Ipaddr     netutils.IPV4Addr
+}
+
+func (i IPDetails) GetMoid() string {
+	return i.Moid
+}
+func (i IPDetails) GetPowerState() string {
+	return i.PowerState
+}
+func (i IPDetails) GetUuid() string {
+	return i.Uuid
+}
+func (i IPDetails) GetMac() string {
+	return i.Mac
+}
+func (i IPDetails) GetName() string {
+	return i.Name
+}
+
+func (i IPDetails) GetType() string {
+	return i.Type
+}
+
+func (i IPDetails) GetIp() string {
+	return i.Ip
 }
