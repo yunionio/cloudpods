1a1d7c70ad2c8e529a8b5161d67cce0ea774bd5b
Zexi Li <zexi.li@qq.com>
2020-09-29 08:25:48 +0800

    mcclient modules: fix k8s fed rolebinding joint wrong resource manager

--- pkg/mcclient/modules/k8s/fed_rolebinding_cluster.go
+++ pkg/mcclient/modules/k8s/fed_rolebinding_cluster.go
@@ -29,7 +29,7 @@ func init() {
 		"federatedrolebindingclusters",
 		NewFedJointClusterCols(),
 		NewColumns(),
-		FederatedRoles,
+		FederatedRoleBindings,
 		KubeClusters,
 	)
 	modules.Register(&FederatedRoleBindingClusters)
