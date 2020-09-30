af9e911fd09bc65e2cf5e6817faaa3193e05eb5a
zhaoxiangchun <1422928955@qq.com>
2020-09-30 17:15:28 +0800

    modify suggest rule cli
    1.增加perform config方法，方便修改默认规则：执行周期，扫描窗口的属性

--- cmd/climc/shell/monitor/suggestsysrule.go
+++ cmd/climc/shell/monitor/suggestsysrule.go
@@ -15,70 +15,19 @@
 package monitor
 
 import (
-	"yunion.io/x/onecloud/pkg/mcclient"
-	"yunion.io/x/onecloud/pkg/mcclient/modules/monitor"
+	"yunion.io/x/onecloud/cmd/climc/shell"
+	"yunion.io/x/onecloud/pkg/mcclient/modules"
 	options "yunion.io/x/onecloud/pkg/mcclient/options/monitor"
 )
 
 func init() {
-	aN := cmdN("suggestsysrule")
-	R(&options.SuggestRuleListOptions{}, aN("list"), "List all suggestsysrules",
-		func(s *mcclient.ClientSession, args *options.SuggestRuleListOptions) error {
-			params, err := args.Params()
-			if err != nil {
-				return err
-			}
-			ret, err := monitor.SuggestSysRuleManager.List(s, params)
-			if err != nil {
-				return err
-			}
-			printList(ret, monitor.SuggestSysRuleManager.GetColumns(s))
-			return nil
-		})
 
-	R(&options.SuggestRuleCreateOptions{}, aN("create"), "Create suggestsys rule",
-		func(s *mcclient.ClientSession, args *options.SuggestRuleCreateOptions) error {
-			params, err := args.Params()
-			if err != nil {
-				return err
-			}
-			ret, err := monitor.SuggestSysRuleManager.Create(s, params)
-			if err != nil {
-				return err
-			}
-			printObject(ret)
-			return nil
-		})
-
-	R(&options.SuggestRuleShowOptions{}, aN("show"), "Show details of a alert rule",
-		func(s *mcclient.ClientSession, args *options.SuggestRuleShowOptions) error {
-			ret, err := monitor.SuggestSysRuleManager.Get(s, args.ID, nil)
-			if err != nil {
-				return err
-			}
-			printObject(ret)
-			return nil
-		})
-
-	R(&options.SuggestRuleUpdateOptions{}, aN("update"), "Update a alert rule",
-		func(s *mcclient.ClientSession, args *options.SuggestRuleUpdateOptions) error {
-			params, err := args.Params()
-			if err != nil {
-				return err
-			}
-			ret, err := monitor.SuggestSysRuleManager.Update(s, args.ID, params)
-			if err != nil {
-				return err
-			}
-			printObject(ret)
-			return nil
-		})
-
-	R(&options.SuggestRuleDeleteOptions{}, aN("delete"), "Delete alerts",
-		func(s *mcclient.ClientSession, args *options.SuggestRuleDeleteOptions) error {
-			ret := monitor.SuggestSysRuleManager.BatchDelete(s, args.ID, nil)
-			printBatchResults(ret, monitor.SuggestSysRuleManager.GetColumns(s))
-			return nil
-		})
+	cmd := shell.NewResourceCmd(modules.SuggestSysRuleManager)
+	cmd.List(new(options.SuggestRuleListOptions))
+	cmd.Show(new(options.SuggestRuleShowOptions))
+	cmd.Perform("enable", new(options.SuggestRuleShowOptions))
+	cmd.Perform("disable", new(options.SuggestRuleShowOptions))
+	cmd.Delete(new(options.CommonAlertDeleteOptions))
+	cmd.Perform("config", new(options.SuggestRuleConfigOptions))
 
 }
