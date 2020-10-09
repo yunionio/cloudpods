48d764dd36986ab07232baca931397a4486e659a
rainzm <mjoycarry@gmail.com>
2020-10-09 11:38:46 +0800

    fix(esxi): check nil value when NewVirtualMachine

--- pkg/multicloud/esxi/virtualmachine.go
+++ pkg/multicloud/esxi/virtualmachine.go
@@ -72,7 +72,11 @@ func (d byDiskType) Less(i, j int) bool {
 
 func NewVirtualMachine(manager *SESXiClient, vm *mo.VirtualMachine, dc *SDatacenter) *SVirtualMachine {
 	svm := &SVirtualMachine{SManagedObject: newManagedObject(manager, vm, dc)}
-	svm.fetchHardwareInfo()
+	err := svm.fetchHardwareInfo()
+	if err != nil {
+		log.Errorf("NewVirtualMachine: %v", err)
+		return nil
+	}
 	return svm
 }
 
@@ -705,7 +709,7 @@ func (self *SVirtualMachine) UpdateUserData(userData string) error {
 	return nil
 }
 
-func (self *SVirtualMachine) fetchHardwareInfo() {
+func (self *SVirtualMachine) fetchHardwareInfo() error {
 	self.vnics = make([]SVirtualNIC, 0)
 	self.vdisks = make([]SVirtualDisk, 0)
 	self.cdroms = make([]SVirtualCdrom, 0)
@@ -719,8 +723,7 @@ func (self *SVirtualMachine) fetchHardwareInfo() {
 	}
 
 	if moVM == nil || moVM.Config == nil || moVM.Config.Hardware.Device == nil {
-		log.Errorf("invalid vm config, moVM: %v", moVM)
-		return
+		return fmt.Errorf("invalid vm")
 	}
 
 	for i := 0; i < len(moVM.Config.Hardware.Device); i += 1 {
@@ -748,6 +751,7 @@ func (self *SVirtualMachine) fetchHardwareInfo() {
 	sort.Slice(self.vdisks, func(i, j int) bool {
 		return self.vdisks[i].GetIndex() < self.vdisks[j].GetIndex()
 	})
+	return nil
 }
 
 func (self *SVirtualMachine) getVdev(key int32) SVirtualDevice {
