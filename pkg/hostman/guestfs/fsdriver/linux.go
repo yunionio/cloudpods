6e66a129d65a8d2cbc05f4e41820554fde8c7728
wanyaoqi <wanyaoqi@yunionyun.com>
2020-11-16 20:44:06 +0800

    host-deployer: customize user cloudroot home dir

--- pkg/hostman/guestfs/fsdriver/linux.go
+++ pkg/hostman/guestfs/fsdriver/linux.go
@@ -94,7 +94,7 @@ func (l *sLinuxRootFs) DeployHosts(rootFs IDiskPartition, hostname, domain strin
 
 func (l *sLinuxRootFs) GetLoginAccount(rootFs IDiskPartition, sUser string, defaultRootUser bool, windowsDefaultAdminUser bool) (string, error) {
 	if len(sUser) > 0 {
-		if err := rootFs.UserAdd(sUser, false); err != nil && !strings.Contains(err.Error(), "already exists") {
+		if err := rootFs.UserAdd(sUser, "", false); err != nil && !strings.Contains(err.Error(), "already exists") {
 			return "", fmt.Errorf("UserAdd %s: %v", sUser, err)
 		}
 		if err := l.EnableUserSudo(rootFs, sUser); err != nil {
@@ -154,10 +154,11 @@ func (l *sLinuxRootFs) DeployYunionroot(rootFs IDiskPartition, pubkeys *deployap
 		l.DisableCloudinit(rootFs)
 	}
 	var yunionroot = YUNIONROOT_USER
-	if err := rootFs.UserAdd(yunionroot, false); err != nil && !strings.Contains(err.Error(), "already exists") {
+	rootdir := path.Join(cloudrootDirectory, yunionroot)
+	if err := rootFs.UserAdd(yunionroot, cloudrootDirectory, false); err != nil && !strings.Contains(err.Error(), "already exists") {
 		log.Errorf("UserAdd %s: %v", yunionroot, err)
 	}
-	err := DeployAuthorizedKeys(rootFs, path.Join("/home", yunionroot), pubkeys, true)
+	err := DeployAuthorizedKeys(rootFs, rootdir, pubkeys, true)
 	if err != nil {
 		return fmt.Errorf("DeployAuthorizedKeys: %v", err)
 	}
