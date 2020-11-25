348d2129f852cec8d80007da8a734fe722b699dd
Zexi Li <zexi.li@qq.com>
2020-11-25 21:48:22 +0800

    fix(scheduler): clean expire cache before schedule

--- pkg/scheduler/algorithm/predicates/disk_schedtag_predicate.go
+++ pkg/scheduler/algorithm/predicates/disk_schedtag_predicate.go
@@ -163,6 +163,7 @@ func (p *DiskSchedtagPredicate) DoSelect(
 	res []ISchedtagCandidateResource,
 ) []ISchedtagCandidateResource {
 	return p.GetUsedStorages(res, input.(*diskW).Backend)
+
 }
 
 func (p *DiskSchedtagPredicate) GetCandidateResourceSortScore(selectRes ISchedtagCandidateResource) int64 {
@@ -202,8 +203,8 @@ func (p *DiskSchedtagPredicate) GetUsedStorages(res []ISchedtagCandidateResource
 	} else {
 		backendStorages = storages
 	}
-	if len(backendStorages) == 0 {
+	/*if len(backendStorages) == 0 {
 		backendStorages = storages
-	}
+	}*/
 	return backendStorages
 }
