ef648b5b87c7a850ba4aa9a8b753341189ca7bcd
Qiu Jian <qiujian@yunionyun.com>
2020-12-02 16:24:55 +0800

    fix(keystone): not allow to disable sql driver
    not allow to disable default SQL identity_provider driver

--- pkg/keystone/models/identity_provider.go
+++ pkg/keystone/models/identity_provider.go
@@ -1481,6 +1481,9 @@ func (idp *SIdentityProvider) PerformDisable(
 	query jsonutils.JSONObject,
 	input apis.PerformDisableInput,
 ) (jsonutils.JSONObject, error) {
+	if idp.Driver == api.IdentityDriverSQL {
+		return nil, errors.Wrap(httperrors.ErrForbidden, "not allow to disable sql idp")
+	}
 	if idp.Driver == api.IdentityDriverLDAP || idp.AutoCreateUser.IsTrue() {
 		domains, _ := idp.getLinkedDomains()
 		for i := range domains {
