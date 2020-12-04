5d69ae9a0249d7c328d267a3076bffd7d8160d3b
wanyaoqi <wanyaoqi@yunionyun.com>
2020-12-04 11:09:06 +0800

    fix(region): attach network on register baremetal server

--- pkg/compute/models/hosts.go
+++ pkg/compute/models/hosts.go
@@ -3993,6 +3993,16 @@ func (self *SHost) PerformInitialize(
 	if err != nil {
 		log.Errorf("Host perform initialize failed on create disk %s", err)
 	}
+	net, err := self.getNetworkOfIPOnHost(self.AccessIp)
+	if err != nil {
+		log.Errorf("host perfrom initialize failed fetch net of access ip %s", err)
+	} else {
+		_, err = guest.attach2NetworkDesc(ctx, userCred, self, &api.NetworkConfig{
+			Address: self.AccessIp, Network: net.Id}, nil, nil)
+		if err != nil {
+			log.Errorf("host perform initialize failed on attach network %s", err)
+		}
+	}
 	return nil, nil
 }
 
