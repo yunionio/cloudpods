f20ca8b9c91fb47d88a62f0e57519edbec40d7d9
TangBin <tangbin@yunion.cn>
2020-12-11 10:34:37 +0800

    fix(region): server login info& keypair import input parameter trimspace

--- pkg/mcclient/modules/mod_servers.go
+++ pkg/mcclient/modules/mod_servers.go
@@ -64,6 +64,7 @@ func (this *ServerManager) GetLoginInfo(s *mcclient.ClientSession, id string, pa
 			}
 			if params != nil && !gotypes.IsNil(params) {
 				privateKey, _ := params.GetString("private_key")
+				privateKey = strings.TrimSpace(privateKey)
 				if len(privateKey) > 0 {
 					passwd, e = seclib2.DecryptBase64(privateKey, loginKey)
 					if e != nil {
