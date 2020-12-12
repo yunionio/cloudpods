bbfa072173382201c8d655450f73f0472197d0ab
TangBin <tangbin@yunion.cn>
2020-12-12 17:58:08 +0800

    fix(region): goimports fix

--- pkg/multicloud/aws/vpc.go
+++ pkg/multicloud/aws/vpc.go
@@ -20,8 +20,8 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/ec2"
 
-	"yunion.io/x/log"
 	"yunion.io/x/jsonutils"
+	"yunion.io/x/log"
 	"yunion.io/x/pkg/errors"
 
 	api "yunion.io/x/onecloud/pkg/apis/compute"
