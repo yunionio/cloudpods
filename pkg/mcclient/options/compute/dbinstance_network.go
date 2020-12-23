c5a974e744251d26bdb9f51b8422c19ffe9ae4ff
Qu Xuan <quxuan@yunionyun.com>
2020-12-23 21:23:27 +0800

    fix(region): support rds secgroup operation

---
+++ pkg/mcclient/options/compute/dbinstance_network.go
@@ -0,0 +1,35 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package compute
+
+import (
+	"yunion.io/x/jsonutils"
+
+	"yunion.io/x/onecloud/pkg/mcclient/options"
+)
+
+type DBInstanceNetworkListOptions struct {
+	options.BaseListOptions
+	DBInstance string `help:"ID or Name of DBInstance" json:"dbinstance"`
+	Network    string `help:"Network ID or name"`
+}
+
+func (opts *DBInstanceNetworkListOptions) GetContextId() string {
+	return opts.DBInstance
+}
+
+func (opts *DBInstanceNetworkListOptions) Params() (jsonutils.JSONObject, error) {
+	return options.ListStructToParams(opts)
+}
