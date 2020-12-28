6208111836a646bd8cad4926bcd1f23585822df9
lvyangyang <lvyangyang@yunion.cn>
2020-12-28 19:07:28 +0800

    refact(region): sync cloud tags to user tags

--- pkg/compute/models/dbinstances.go
+++ pkg/compute/models/dbinstances.go
@@ -2052,7 +2052,7 @@ func (self *SDBInstance) OnMetadataUpdated(ctx context.Context, userCred mcclien
 	if len(self.ExternalId) == 0 {
 		return
 	}
-	err := self.StartRemoteUpdateTask(ctx, userCred, false, "")
+	err := self.StartRemoteUpdateTask(ctx, userCred, true, "")
 	if err != nil {
 		log.Errorf("StartRemoteUpdateTask fail: %s", err)
 	}
--- pkg/compute/models/elasticcache_instances.go
+++ pkg/compute/models/elasticcache_instances.go
@@ -1925,7 +1925,7 @@ func (self *SElasticcache) OnMetadataUpdated(ctx context.Context, userCred mccli
 	if len(self.ExternalId) == 0 {
 		return
 	}
-	err := self.StartRemoteUpdateTask(ctx, userCred, false, "")
+	err := self.StartRemoteUpdateTask(ctx, userCred, true, "")
 	if err != nil {
 		log.Errorf("StartRemoteUpdateTask fail: %s", err)
 	}
