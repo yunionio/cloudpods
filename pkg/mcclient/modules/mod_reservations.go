66d2820f016c4b84f3099091ec5db7f3253ab755
bistuzx@163.com <bistuzx@163.com>
2020-12-29 22:33:21 +0800

    fix reservation columns

--- pkg/mcclient/modules/mod_reservations.go
+++ pkg/mcclient/modules/mod_reservations.go
@@ -23,8 +23,9 @@ var (
 func init() {
 	Reservations = NewMeterManager("reservation", "reservations",
 		[]string{"cloudaccount_id", "resource_type", "reservation_years", "lookback_days", "payment_option",
-			"offering_class", "category", "spec", "instance_amount", "monthly_savings_amount",
-			"monthly_usage_percentage", "monthly_cost", "prepaid_cost", "cost", "currency", "details"},
+			"offering_class", "category", "spec", "instance_amount", "monthly_savings_amount", "upfront_cost",
+			"average_utilization", "monthly_cost", "total_savings_amount", "total_savings_percentage",
+			"currency", "details"},
 		[]string{},
 	)
 	register(&Reservations)
