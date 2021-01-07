0413898aa7d71fa961a165a49bed29180fd92367
tangbin <tangbin@yunion.cn>
2021-01-07 20:09:06 +0800

    feat(region): server sku os_arch & cpu_arch checking

--- pkg/compute/models/guests.go
+++ pkg/compute/models/guests.go
@@ -1324,6 +1324,13 @@ func (manager *SGuestManager) validateCreateData(
 		}
 		log.Debugf("ROOT DISK: %#v", rootDiskConfig)
 		input.Disks[0] = rootDiskConfig
+		if sku != nil {
+			if len(rootDiskConfig.OsArch) >= 0 && len(sku.CpuArch) >= 0 {
+				if strings.Contains(rootDiskConfig.OsArch, sku.CpuArch) {
+					return nil, httperrors.NewConflictError("root disk image(%s) and sku(%s) architecture mismatch", rootDiskConfig.OsArch, sku.CpuArch)
+				}
+			}
+		}
 		//data.Set("disk.0", jsonutils.Marshal(rootDiskConfig))
 
 		for i := 0; i < len(dataDiskDefs); i += 1 {
