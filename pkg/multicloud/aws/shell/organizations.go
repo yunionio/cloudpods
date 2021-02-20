c6cd80351b92c9787cbd86edb59a97be477255c2
Qiu Jian <qiujian@yunionyun.com>
2021-02-20 22:01:10 +0800

    fix: add more organization command

--- pkg/multicloud/aws/shell/organizations.go
+++ pkg/multicloud/aws/shell/organizations.go
@@ -15,6 +15,8 @@
 package shell
 
 import (
+	"fmt"
+
 	"yunion.io/x/onecloud/pkg/multicloud/aws"
 	"yunion.io/x/onecloud/pkg/util/shellutils"
 )
@@ -28,6 +30,65 @@ func init() {
 			return err
 		}
 		printList(accounts, 0, 0, 0, []string{})
+		return nil
+	})
+
+	type OrganizationPoliciesListOptions struct {
+		FILTER string `json:"filter" choices:"SERVICE_CONTROL_POLICY|TAG_POLICY|BACKUP_POLICY|AISERVICES_OPT_OUT_POLICY"`
+	}
+	shellutils.R(&OrganizationPoliciesListOptions{}, "org-policy-list", "List policies of organizations", func(cli *aws.SRegion, args *OrganizationPoliciesListOptions) error {
+		policies, err := cli.ListPolicies(args.FILTER)
+		if err != nil {
+			return err
+		}
+		printList(policies, 0, 0, 0, []string{})
+		return nil
+	})
+
+	type OrganizationPolicyShowOptions struct {
+		ID string `json:"id"`
+	}
+	shellutils.R(&OrganizationPolicyShowOptions{}, "org-policy-show", "Show details of an organizations policy", func(cli *aws.SRegion, args *OrganizationPolicyShowOptions) error {
+		content, err := cli.DescribeOrgPolicy(args.ID)
+		if err != nil {
+			return err
+		}
+		fmt.Println(content.PrettyString())
+		return nil
+	})
+
+	type OrganizationPoliciesListForTargetOptions struct {
+		OrganizationPoliciesListOptions
+		TARGET string `json:"target"`
+	}
+	shellutils.R(&OrganizationPoliciesListForTargetOptions{}, "org-target-policy-list", "List policies for target of organizations", func(cli *aws.SRegion, args *OrganizationPoliciesListForTargetOptions) error {
+		policies, err := cli.ListPoliciesForTarget(args.FILTER, args.TARGET)
+		if err != nil {
+			return err
+		}
+		printList(policies, 0, 0, 0, []string{})
+		return nil
+	})
+
+	type OrganizationParentsListOptions struct {
+		ID string `json:"id"`
+	}
+	shellutils.R(&OrganizationParentsListOptions{}, "org-parent-list", "List parent nodes of a child node", func(cli *aws.SRegion, args *OrganizationParentsListOptions) error {
+		err := cli.ListParents(args.ID)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	type OrganizationalUnitShowOptions struct {
+		ID string `help:"Id of organizational unit"`
+	}
+	shellutils.R(&OrganizationalUnitShowOptions{}, "org-ou-show", "Show details of organizational unit", func(cli *aws.SRegion, args *OrganizationalUnitShowOptions) error {
+		err := cli.DescribeOrganizationalUnit(args.ID)
+		if err != nil {
+			return err
+		}
 		return nil
 	})
 }
