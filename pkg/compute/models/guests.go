02022c4c29d31907e3d62d3e56437657e546c153
Yousong Zhou <zhouyousong@yunionyun.com>
2021-03-22 18:42:56 +0800

    region: attachnetwork: allow attach by network name
    isValidNetworkInfo allows names.  Otherwise the code will panic when
    a named was passed and nil was returned by the fetch call

--- pkg/compute/models/guests.go
+++ pkg/compute/models/guests.go
@@ -1747,7 +1747,7 @@ func getGuestResourceRequirements(
 	eBw := 0
 	iBw := 0
 	for _, netConfig := range input.Networks {
-		if IsExitNetworkInfo(netConfig) {
+		if IsExitNetworkInfo(userCred, netConfig) {
 			eNicCnt += 1
 			eBw += netConfig.BwLimit
 		} else {
