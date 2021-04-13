cc7ffd707a8b246761928d37587afff364617816
Yousong Zhou <zhouyousong@yunionyun.com>
2021-04-13 19:27:30 +0800

    regions: lbagents: move pb update/create as mcclient module method

--- pkg/mcclient/modules/mod_ansibleplaybooks.go
+++ pkg/mcclient/modules/mod_ansibleplaybooks.go
@@ -14,7 +14,18 @@
 
 package modules
 
-import "yunion.io/x/onecloud/pkg/mcclient/modulebase"
+import (
+	"context"
+
+	"yunion.io/x/jsonutils"
+	"yunion.io/x/pkg/errors"
+
+	ansible_apis "yunion.io/x/onecloud/pkg/apis/ansible"
+	"yunion.io/x/onecloud/pkg/mcclient"
+	mcclient_models "yunion.io/x/onecloud/pkg/mcclient/models"
+	"yunion.io/x/onecloud/pkg/mcclient/modulebase"
+	"yunion.io/x/onecloud/pkg/util/ansible"
+)
 
 type AnsiblePlaybookManager struct {
 	modulebase.ResourceManager
@@ -40,4 +51,53 @@ func init() {
 		),
 	}
 	registerV2(&AnsiblePlaybooks)
+}
+
+func (man *AnsiblePlaybookManager) UpdateOrCreatePbModel(
+	ctx context.Context,
+	cliSess *mcclient.ClientSession,
+	pbId string,
+	pbName string,
+	pb *ansible.Playbook,
+) (*mcclient_models.AnsiblePlaybook, error) {
+	if pbId == "" {
+		pbJson, err := man.Get(cliSess, pbName, nil)
+		if err == nil {
+			pbModel := &mcclient_models.AnsiblePlaybook{}
+			if err := pbJson.Unmarshal(pbModel); err == nil {
+				pbId = pbModel.Id
+			}
+		}
+	}
+
+	var pbJson jsonutils.JSONObject
+	if pbId != "" {
+		var err error
+		ansiblePbInput := &ansible_apis.AnsiblePlaybookUpdateInput{
+			Name:     pbName,
+			Playbook: *pb,
+		}
+		params := ansiblePbInput.JSON(ansiblePbInput)
+		pbJson, err = man.Update(cliSess, pbId, params)
+		if err != nil {
+			return nil, errors.Wrap(err, "update ansibleplaybook")
+		}
+	} else {
+		var err error
+		ansiblePbInput := &ansible_apis.AnsiblePlaybookCreateInput{
+			Name:     pbName,
+			Playbook: *pb,
+		}
+		params := ansiblePbInput.JSON(ansiblePbInput)
+		pbJson, err = man.Create(cliSess, params)
+		if err != nil {
+			return nil, errors.Wrap(err, "create ansibleplaybook")
+		}
+	}
+
+	pbModel := &mcclient_models.AnsiblePlaybook{}
+	if err := pbJson.Unmarshal(pbModel); err != nil {
+		return nil, errors.Wrap(err, "unmarshal ansibleplaybook")
+	}
+	return pbModel, nil
 }
