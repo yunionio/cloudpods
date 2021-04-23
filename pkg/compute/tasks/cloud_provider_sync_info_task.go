520fae50a61c0ca2362fbcbef707ac704a2a83b6
rainzm <mjoycarry@gmail.com>
2021-04-23 16:22:14 +0800

    fix(region): use special localtask to run time-consuming provider synchronization task
    CloudProviderSyncInfoTask will local cloudprovider. Make locaktask run
    time-consuming task can avoid holding locks for a long time.

--- pkg/compute/tasks/cloud_provider_sync_info_task.go
+++ pkg/compute/tasks/cloud_provider_sync_info_task.go
@@ -34,9 +34,12 @@ type CloudProviderSyncInfoTask struct {
 	taskman.STask
 }
 
+var syncLocalTaskWorkerMan *appsrv.SWorkerManager
+
 func InitCloudproviderSyncWorkers(count int) {
 	syncWorker := appsrv.NewWorkerManager("CloudProviderSyncInfoTaskWorkerManager", count, 512, true)
 	taskman.RegisterTaskAndWorker(CloudProviderSyncInfoTask{}, syncWorker)
+	syncLocalTaskWorkerMan = appsrv.NewWorkerManager("CloudProviderSyncLocalTaskWorkerManager", count, 512, false)
 }
 
 func getAction(params *jsonutils.JSONDict) string {
@@ -94,17 +97,32 @@ func (self *CloudProviderSyncInfoTask) OnSyncCloudProviderPreInfoComplete(ctx co
 	syncRange := self.GetSyncRange()
 
 	db.OpsLog.LogEvent(provider, db.ACT_SYNCING_HOST, "", self.UserCred)
+	self.SetStage("OnSyncCloudProviderInfoComplete", nil)
 
-	provider.SyncCallSyncCloudproviderRegions(ctx, self.UserCred, syncRange)
-	provider.SyncCallSyncCloudproviderInterVpcNetwork(ctx, self.UserCred)
-
-	provider.CleanSchedCache()
-	self.SetStageComplete(ctx, nil)
-	db.OpsLog.LogEvent(provider, db.ACT_SYNC_HOST_COMPLETE, "", self.UserCred)
-	logclient.AddActionLogWithStartable(self, provider, getAction(self.Params), body, self.UserCred, true)
+	taskman.LocalTaskRunWithWorkers(self, func() (jsonutils.JSONObject, error) {
+		provider.SyncCallSyncCloudproviderRegions(ctx, self.UserCred, syncRange)
+		provider.SyncCallSyncCloudproviderInterVpcNetwork(ctx, self.UserCred)
+		return nil, nil
+	}, syncLocalTaskWorkerMan)
 }
 
 func (self *CloudProviderSyncInfoTask) OnSyncCloudProviderPreInfoCompleteFailed(ctx context.Context, obj db.IStandaloneModel, body jsonutils.JSONObject) {
 	log.Errorf("faild to sync provider quotas %s", body.String())
 	self.OnSyncCloudProviderPreInfoComplete(ctx, obj, body)
+}
+
+func (self *CloudProviderSyncInfoTask) OnSyncCloudProviderInfoComplete(ctx context.Context, obj db.IStandaloneModel, body jsonutils.JSONObject) {
+	provider := obj.(*models.SCloudprovider)
+	provider.CleanSchedCache()
+	db.OpsLog.LogEvent(provider, db.ACT_SYNC_HOST_COMPLETE, "", self.UserCred)
+	logclient.AddActionLogWithStartable(self, provider, getAction(self.Params), body, self.UserCred, true)
+	self.SetStageComplete(ctx, nil)
+}
+
+func (self *CloudProviderSyncInfoTask) OnSyncCloudProviderInfoCompleteFailed(ctx context.Context, obj db.IStandaloneModel, body jsonutils.JSONObject) {
+	provider := obj.(*models.SCloudprovider)
+	provider.CleanSchedCache()
+	db.OpsLog.LogEvent(provider, db.ACT_SYNC_HOST_FAILED, "", self.UserCred)
+	logclient.AddActionLogWithStartable(self, provider, getAction(self.Params), body, self.UserCred, false)
+	self.SetStageFailed(ctx, nil)
 }
