d948d60262a35c9cab8f97e618affd95a56d3e9b
Qu Xuan <quxuan@yunionyun.com>
2021-06-30 19:58:50 +0800

    fix(webconsole): aliyun vnc windows ctl+alt+del

--- pkg/webconsole/options/options.go
+++ pkg/webconsole/options/options.go
@@ -25,11 +25,12 @@ type WebConsoleOptions struct {
 
 	//ApiServer       string `help:"API server url to handle websocket connection, usually with public access" default:"http://webconsole.yunion.io"`
 
-	KubectlPath     string `help:"kubectl binary path used to connect k8s cluster" default:"/usr/bin/kubectl"`
-	IpmitoolPath    string `help:"ipmitool binary path used to connect baremetal sol" default:"/usr/bin/ipmitool"`
-	SshToolPath     string `help:"sshtool binary path used to connect server sol" default:"/usr/bin/ssh"`
-	SshpassToolPath string `help:"sshpass tool binary path used to connect server sol" default:"/usr/bin/sshpass"`
-	EnableAutoLogin bool   `help:"allow webconsole to log in directly with the cloudroot public key" default:"false"`
+	KubectlPath      string `help:"kubectl binary path used to connect k8s cluster" default:"/usr/bin/kubectl"`
+	IpmitoolPath     string `help:"ipmitool binary path used to connect baremetal sol" default:"/usr/bin/ipmitool"`
+	SshToolPath      string `help:"sshtool binary path used to connect server sol" default:"/usr/bin/ssh"`
+	SshpassToolPath  string `help:"sshpass tool binary path used to connect server sol" default:"/usr/bin/sshpass"`
+	EnableAutoLogin  bool   `help:"allow webconsole to log in directly with the cloudroot public key" default:"false"`
+	AliyunVncVersion string `help:"Aliyun vnc version" default:"0.0.8"`
 }
 
 func OnOptionsChange(oldO, newO interface{}) bool {
