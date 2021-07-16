zhaoxiangchun/onecloud

feat(cloudmon): add cloudmon component

cmd/cloudmon/main.go
@@ -1,0 +1,169 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path"
+	"time"
+
+	"yunion.io/x/log"
+	"yunion.io/x/pkg/errors"
+	"yunion.io/x/pkg/util/version"
+
+	_ "yunion.io/x/onecloud/pkg/cloudmon/collectors"
+	"yunion.io/x/onecloud/pkg/cloudmon/collectors/common"
+	"yunion.io/x/onecloud/pkg/cloudmon/options"
+	"yunion.io/x/onecloud/pkg/mcclient"
+)
+
+func showErrorAndExit(e error) {
+	fmt.Fprintf(os.Stderr, "%s", e)
+	fmt.Fprintln(os.Stderr)
+	os.Exit(1)
+}
+
+func newClientSession(options *options.CloudMonOptions) (*mcclient.ClientSession, error) {
+	if len(options.AuthURL) == 0 {
+		return nil, errors.Error("empty auth_url")
+	}
+	if len(options.AdminUser) == 0 {
+		return nil, errors.Error("empty admin_user")
+	}
+	if len(options.AdminPassword) == 0 {
+		return nil, errors.Error("empty admin_password")
+	}
+	if len(options.AdminProject) == 0 {
+		return nil, errors.Error("empty admin_project")
+	}
+
+	client := mcclient.NewClient(
+		options.AuthURL,
+		options.Timeout,
+		options.Debug,
+		options.Insecure,
+		options.CertFile,
+		options.KeyFile,
+	)
+
+	token, err := client.AuthenticateWithSource(
+		options.AdminUser,
+		options.AdminPassword,
+		options.AdminDomain,
+		options.AdminProject,
+		options.AdminProjectDomain,
+		mcclient.AuthSourceAPI)
+	if err != nil {
+		return nil, err
+	}
+
+	session := client.NewSession(
+		context.Background(),
+		options.Region,
+		"",
+		options.EndpointType,
+		token,
+		options.ApiVersion)
+
+	return session, nil
+}
+
+func main() {
+	parser, err := options.GetArgumentParser()
+	if err != nil {
+		showErrorAndExit(err)
+	}
+
+	err = parser.ParseArgs2(os.Args[1:], false, false)
+
+	opts := parser.Options().(*options.CloudMonOptions)
+
+	if opts.Help {
+		fmt.Println(parser.HelpString())
+		os.Exit(0)
+	}
+
+	if opts.Version {
+		fmt.Println(version.GetJsonString())
+		os.Exit(0)
+	}
+
+	if len(opts.Config) == 0 {
+		for _, p := range []string{"./etc", "/etc/yunion"} {
+			confTmp := path.Join(p, "cloudmon.conf")
+			if _, err := os.Stat(confTmp); err == nil {
+				opts.Config = confTmp
+				break
+			}
+		}
+	}
+
+	if len(opts.Config) > 0 {
+		err := parser.ParseFile(opts.Config)
+		if err != nil {
+			showErrorAndExit(err)
+		}
+	}
+
+	if err != nil {
+		showErrorAndExit(err)
+	}
+
+	parser.SetDefault()
+
+	subcmd := parser.GetSubcommand()
+	subparser := subcmd.GetSubParser()
+	if err != nil {
+		if subparser != nil {
+			fmt.Print(subparser.Usage())
+		} else {
+			fmt.Print(parser.Usage())
+		}
+		showErrorAndExit(err)
+	}
+
+	suboptions := subparser.Options()
+	if opts.SUBCOMMAND == "help" {
+		err = subcmd.Invoke(suboptions)
+	} else {
+		timout := suboptions.(*common.ReportOptions).Timeout
+		endChan := make(chan int, 1)
+		go func() {
+			ticker := time.NewTicker(time.Duration(timout) * time.Second)
+			for {
+				select {
+				case <-ticker.C:
+					log.Errorf("cmd: %s,provider: %v,end due to timeout: %s s", opts.SUBCOMMAND,
+						suboptions.(*common.ReportOptions).Provider, suboptions.(*common.ReportOptions).Timeout)
+					os.Exit(3)
+				case <-endChan:
+				}
+			}
+		}()
+
+		var session *mcclient.ClientSession
+		session, err = newClientSession(opts)
+		if err != nil {
+			showErrorAndExit(err)
+		}
+		err = subcmd.Invoke(session, suboptions)
+		endChan <- 1
+	}
+	if err != nil {
+		showErrorAndExit(err)
+	}
+}

pkg/cloudmon/collectors/alertrecordhistorymon/alertrecordhistoryreport.go
@@ -1,0 +1,133 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cloudaccountmon
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+
+	"yunion.io/x/jsonutils"
+	"yunion.io/x/pkg/errors"
+	"yunion.io/x/pkg/util/timeutils"
+
+	"yunion.io/x/onecloud/pkg/cloudmon/collectors/common"
+	"yunion.io/x/onecloud/pkg/mcclient"
+	"yunion.io/x/onecloud/pkg/mcclient/modules"
+)
+
+func init() {
+	factory := SAlertRecordHistoryFactory{}
+	common.RegisterFactory(&factory)
+}
+
+type SAlertRecordHistoryFactory struct {
+}
+
+func (self *SAlertRecordHistoryFactory) NewCloudReport(provider *common.SProvider, session *mcclient.ClientSession,
+	args *common.ReportOptions,
+	operatorType string) common.ICloudReport {
+	return &SAlertRecordHistoryReport{
+		common.CloudReportBase{
+			SProvider: nil,
+			Session:   session,
+			Args:      args,
+			Operator:  string(common.ALERT_RECORD),
+		},
+	}
+}
+
+func (S *SAlertRecordHistoryFactory) GetId() string {
+	return string(common.ALERT_RECORD)
+}
+
+type SAlertRecordHistoryReport struct {
+	common.CloudReportBase
+}
+
+func (self *SAlertRecordHistoryReport) Report() error {
+	alerts, err := self.getMonitorCommonAlert()
+	if err != nil {
+		return err
+	}
+	errs := make([]error, 0)
+	recordGroup, _ := errgroup.WithContext(context.Background())
+	count := 0
+	for i, _ := range alerts {
+		tmp := alerts[i]
+		count++
+		recordGroup.Go(func() error {
+			alert_id, _ := tmp.GetString("id")
+			alertRecords, err := self.getAlertRecordsByAlertId(alert_id)
+			if err != nil {
+				return errors.Wrapf(err, "getAlertRecordsByAlertId:%s error", alert_id)
+			}
+			return self.collectMetric(alertRecords)
+		})
+		if count == 4 {
+			err := recordGroup.Wait()
+			if err != nil {
+				errs = append(errs, errors.Wrap(err, "alertRecordHistoryReport collectMetric error"))
+			}
+			count = 0
+		}
+	}
+	err = recordGroup.Wait()
+	if err != nil {
+		errs = append(errs, errors.Wrap(err, "alertRecordHistoryReport collectMetric error"))
+	}
+	return errors.NewAggregate(errs)
+}
+
+func (self *SAlertRecordHistoryReport) getMonitorCommonAlert() ([]jsonutils.JSONObject, error) {
+	alerts := make([]jsonutils.JSONObject, 0)
+	query := jsonutils.NewDict()
+	query.Add(jsonutils.NewString("0"), common.KEY_LIMIT)
+	//query.Add(jsonutils.NewBool(true), common.DETAILS)
+	query.Add(jsonutils.NewString("system"), "scope")
+
+	alerts, err := self.ListAllResource(modules.CommonAlertManager, query)
+	if err != nil {
+		return nil, errors.Wrap(err, "getMonitorCommonAlert error")
+	}
+	return alerts, nil
+}
+
+func (self *SAlertRecordHistoryReport) getAlertRecordsByAlertId(id string) ([]jsonutils.JSONObject, error) {
+	now := time.Now().UTC()
+	period64, err := strconv.ParseInt(self.Args.Interval, 10, 8)
+	if err != nil {
+		return nil, err
+	}
+	query := jsonutils.NewDict()
+	query.Add(jsonutils.NewString("0"), common.KEY_LIMIT)
+	query.Add(jsonutils.NewBool(true), common.DETAILS)
+	query.Add(jsonutils.NewString("system"), "scope")
+	query.Add(jsonutils.NewString(id), "alert_id")
+	query.Add(jsonutils.NewString("alerting"), "state")
+	query.Add(jsonutils.NewString(fmt.Sprintf(`created_at.between("%s","%s")`,
+		now.Add(-time.Hour*24*time.Duration(period64)).Format(timeutils.MysqlTimeFormat),
+		now.Format(timeutils.MysqlTimeFormat))),
+		"filter")
+
+	alertRecords, err := self.ListAllResource(modules.AlertRecordManager, query)
+	if err != nil {
+		return nil, err
+	}
+	return alertRecords, nil
+}

pkg/cloudmon/collectors/alertrecordhistorymon/alertrecordhistoryservice.go
@@ -1,0 +1,54 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cloudaccountmon
+
+import (
+	"time"
+
+	"yunion.io/x/jsonutils"
+
+	"yunion.io/x/onecloud/pkg/cloudmon/collectors/common"
+	"yunion.io/x/onecloud/pkg/util/influxdb"
+)
+
+func (self *SAlertRecordHistoryReport) collectMetric(alertRecords []jsonutils.JSONObject) error {
+	dataList := make([]influxdb.SMetricData, 0)
+	matchRecord := self.getMaxEvalMatchOfAlertRecord(alertRecords)
+	if matchRecord == nil {
+		return nil
+	}
+
+	metric, err := self.NewMetricFromJson(matchRecord)
+	if err != nil {
+		return err
+	}
+	metric.Timestamp = time.Now()
+	metric.Name = ALERT_RECORD_HISTORY_MEASUREMENT
+	dataList = append(dataList, metric)
+	return common.SendMetrics(self.Session, dataList, self.Args.Debug, ALERT_RECORD_HISTORY_DATABASE)
+}
+
+func (self *SAlertRecordHistoryReport) getMaxEvalMatchOfAlertRecord(alertRecords []jsonutils.JSONObject) jsonutils.JSONObject {
+	var matchRecord jsonutils.JSONObject
+	maxCount := int64(0)
+	for _, record := range alertRecords {
+		resNum, _ := record.Int("res_num")
+		if resNum > maxCount {
+			maxCount = resNum
+			matchRecord = record
+		}
+	}
+	return matchRecord
+}

pkg/cloudmon/collectors/alertrecordhistorymon/alertrecordhistoryutil.go
@@ -1,0 +1,21 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cloudaccountmon
+
+const (
+	ALERT_RECORD_HISTORY_FIELD       = "res_num"
+	ALERT_RECORD_HISTORY_MEASUREMENT = "alert_record_history"
+	ALERT_RECORD_HISTORY_DATABASE    = "monitor"
+)

pkg/cloudmon/collectors/apsaramon/apsaracloudreport.go
@@ -1,0 +1,96 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package alimon
+
+import (
+	"yunion.io/x/jsonutils"
+
+	"yunion.io/x/onecloud/pkg/apis/compute"
+	"yunion.io/x/onecloud/pkg/cloudmon/collectors/common"
+	"yunion.io/x/onecloud/pkg/mcclient"
+	"yunion.io/x/onecloud/pkg/mcclient/modules"
+)
+
+func init() {
+	factory := SApsaraCloudReportFactory{}
+	common.RegisterFactory(&factory)
+}
+
+type SApsaraCloudReportFactory struct {
+}
+
+func (self *SApsaraCloudReportFactory) NewCloudReport(provider *common.SProvider, session *mcclient.ClientSession,
+	args *common.ReportOptions, operatorType string) common.ICloudReport {
+	return &SApsaraCloudReport{
+		common.CloudReportBase{
+			SProvider: provider,
+			Session:   session,
+			Args:      args,
+			Operator:  operatorType,
+		},
+	}
+}
+
+func (self *SApsaraCloudReportFactory) GetId() string {
+	return compute.CLOUD_PROVIDER_APSARA
+}
+
+type SApsaraCloudReport struct {
+	common.CloudReportBase
+}
+
+func (self *SApsaraCloudReport) Report() error {
+	var servers []jsonutils.JSONObject
+	var err error
+	switch self.Operator {
+	case "redis":
+		servers, err = self.GetAllserverOfThisProvider(&modules.ElasticCache)
+	case "rds":
+		servers, err = self.GetAllserverOfThisProvider(&modules.DBInstance)
+	case "oss":
+		servers, err = self.GetAllserverOfThisProvider(&modules.Buckets)
+	case "elb":
+		servers, err = self.GetAllserverOfThisProvider(&modules.Loadbalancers)
+	default:
+		servers, err = self.GetAllserverOfThisProvider(&modules.Servers)
+	}
+	providerInstance, err := self.InitProviderInstance()
+	if err != nil {
+		return err
+	}
+	regionList, regionServerMap, err := self.GetAllRegionOfServers(servers, providerInstance)
+	if err != nil {
+		return err
+	}
+	for _, region := range regionList {
+		servers := regionServerMap[region.GetGlobalId()]
+		switch self.Operator {
+		case "server":
+			err = self.collectRegionMetricOfHost(region, servers)
+		case "redis":
+			err = self.collectRegionMetricOfRedis(region, servers)
+		case "rds":
+			err = self.collectRegionMetricOfRds(region, servers)
+		case "oss":
+			err = self.collectRegionMetricOfOss(region, servers)
+		case "elb":
+			err = self.collectRegionMetricOfElb(region, servers)
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}

yunionio/cloudpods

cmd/cloudmon/main.go
@@ -1,0 +1,169 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path"
+	"time"
+
+	"yunion.io/x/log"
+	"yunion.io/x/pkg/errors"
+	"yunion.io/x/pkg/util/version"
+
+	_ "yunion.io/x/onecloud/pkg/cloudmon/collectors"
+	"yunion.io/x/onecloud/pkg/cloudmon/collectors/common"
+	"yunion.io/x/onecloud/pkg/cloudmon/options"
+	"yunion.io/x/onecloud/pkg/mcclient"
+)
+
+func showErrorAndExit(e error) {
+	fmt.Fprintf(os.Stderr, "%s", e)
+	fmt.Fprintln(os.Stderr)
+	os.Exit(1)
+}
+
+func newClientSession(options *options.CloudMonOptions) (*mcclient.ClientSession, error) {
+	if len(options.AuthURL) == 0 {
+		return nil, errors.Error("empty auth_url")
+	}
+	if len(options.AdminUser) == 0 {
+		return nil, errors.Error("empty admin_user")
+	}
+	if len(options.AdminPassword) == 0 {
+		return nil, errors.Error("empty admin_password")
+	}
+	if len(options.AdminProject) == 0 {
+		return nil, errors.Error("empty admin_project")
+	}
+
+	client := mcclient.NewClient(
+		options.AuthURL,
+		options.Timeout,
+		options.Debug,
+		options.Insecure,
+		options.CertFile,
+		options.KeyFile,
+	)
+
+	token, err := client.AuthenticateWithSource(
+		options.AdminUser,
+		options.AdminPassword,
+		options.AdminDomain,
+		options.AdminProject,
+		options.AdminProjectDomain,
+		mcclient.AuthSourceAPI)
+	if err != nil {
+		return nil, err
+	}
+
+	session := client.NewSession(
+		context.Background(),
+		options.Region,
+		"",
+		options.EndpointType,
+		token,
+		options.ApiVersion)
+
+	return session, nil
+}
+
+func main() {
+	parser, err := options.GetArgumentParser()
+	if err != nil {
+		showErrorAndExit(err)
+	}
+
+	err = parser.ParseArgs2(os.Args[1:], false, false)
+
+	opts := parser.Options().(*options.CloudMonOptions)
+
+	if opts.Help {
+		fmt.Println(parser.HelpString())
+		os.Exit(0)
+	}
+
+	if opts.Version {
+		fmt.Println(version.GetJsonString())
+		os.Exit(0)
+	}
+
+	if len(opts.Config) == 0 {
+		for _, p := range []string{"./etc", "/etc/yunion"} {
+			confTmp := path.Join(p, "cloudmon.conf")
+			if _, err := os.Stat(confTmp); err == nil {
+				opts.Config = confTmp
+				break
+			}
+		}
+	}
+
+	if len(opts.Config) > 0 {
+		err := parser.ParseFile(opts.Config)
+		if err != nil {
+			showErrorAndExit(err)
+		}
+	}
+
+	if err != nil {
+		showErrorAndExit(err)
+	}
+
+	parser.SetDefault()
+
+	subcmd := parser.GetSubcommand()
+	subparser := subcmd.GetSubParser()
+	if err != nil {
+		if subparser != nil {
+			fmt.Print(subparser.Usage())
+		} else {
+			fmt.Print(parser.Usage())
+		}
+		showErrorAndExit(err)
+	}
+
+	suboptions := subparser.Options()
+	if opts.SUBCOMMAND == "help" {
+		err = subcmd.Invoke(suboptions)
+	} else {
+		timout := suboptions.(*common.ReportOptions).Timeout
+		endChan := make(chan int, 1)
+		go func() {
+			ticker := time.NewTicker(time.Duration(timout) * time.Second)
+			for {
+				select {
+				case <-ticker.C:
+					log.Errorf("cmd: %s,provider: %v,end due to timeout: %d s", opts.SUBCOMMAND,
+						suboptions.(*common.ReportOptions).Provider, suboptions.(*common.ReportOptions).Timeout)
+					os.Exit(3)
+				case <-endChan:
+				}
+			}
+		}()
+
+		var session *mcclient.ClientSession
+		session, err = newClientSession(opts)
+		if err != nil {
+			showErrorAndExit(err)
+		}
+		err = subcmd.Invoke(session, suboptions)
+		endChan <- 1
+	}
+	if err != nil {
+		showErrorAndExit(err)
+	}
+}

pkg/cloudmon/collectors/alertrecordhistorymon/alertrecordhistoryreport.go
@@ -1,0 +1,133 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package alertrecordhistorymon
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+
+	"yunion.io/x/jsonutils"
+	"yunion.io/x/pkg/errors"
+	"yunion.io/x/pkg/util/timeutils"
+
+	"yunion.io/x/onecloud/pkg/cloudmon/collectors/common"
+	"yunion.io/x/onecloud/pkg/mcclient"
+	"yunion.io/x/onecloud/pkg/mcclient/modules"
+)
+
+func init() {
+	factory := SAlertRecordHistoryFactory{}
+	common.RegisterFactory(&factory)
+}
+
+type SAlertRecordHistoryFactory struct {
+}
+
+func (self *SAlertRecordHistoryFactory) NewCloudReport(provider *common.SProvider, session *mcclient.ClientSession,
+	args *common.ReportOptions,
+	operatorType string) common.ICloudReport {
+	return &SAlertRecordHistoryReport{
+		common.CloudReportBase{
+			SProvider: nil,
+			Session:   session,
+			Args:      args,
+			Operator:  string(common.ALERT_RECORD),
+		},
+	}
+}
+
+func (S *SAlertRecordHistoryFactory) GetId() string {
+	return string(common.ALERT_RECORD)
+}
+
+type SAlertRecordHistoryReport struct {
+	common.CloudReportBase
+}
+
+func (self *SAlertRecordHistoryReport) Report() error {
+	alerts, err := self.getMonitorCommonAlert()
+	if err != nil {
+		return err
+	}
+	errs := make([]error, 0)
+	recordGroup, _ := errgroup.WithContext(context.Background())
+	count := 0
+	for i, _ := range alerts {
+		tmp := alerts[i]
+		count++
+		recordGroup.Go(func() error {
+			alert_id, _ := tmp.GetString("id")
+			alertRecords, err := self.getAlertRecordsByAlertId(alert_id)
+			if err != nil {
+				return errors.Wrapf(err, "getAlertRecordsByAlertId:%s error", alert_id)
+			}
+			return self.collectMetric(alertRecords)
+		})
+		if count == 4 {
+			err := recordGroup.Wait()
+			if err != nil {
+				errs = append(errs, errors.Wrap(err, "alertRecordHistoryReport collectMetric error"))
+			}
+			count = 0
+		}
+	}
+	err = recordGroup.Wait()
+	if err != nil {
+		errs = append(errs, errors.Wrap(err, "alertRecordHistoryReport collectMetric error"))
+	}
+	return errors.NewAggregate(errs)
+}
+
+func (self *SAlertRecordHistoryReport) getMonitorCommonAlert() ([]jsonutils.JSONObject, error) {
+	alerts := make([]jsonutils.JSONObject, 0)
+	query := jsonutils.NewDict()
+	query.Add(jsonutils.NewString("0"), common.KEY_LIMIT)
+	//query.Add(jsonutils.NewBool(true), common.DETAILS)
+	query.Add(jsonutils.NewString("system"), "scope")
+
+	alerts, err := self.ListAllResource(modules.CommonAlertManager, query)
+	if err != nil {
+		return nil, errors.Wrap(err, "getMonitorCommonAlert error")
+	}
+	return alerts, nil
+}
+
+func (self *SAlertRecordHistoryReport) getAlertRecordsByAlertId(id string) ([]jsonutils.JSONObject, error) {
+	now := time.Now().UTC()
+	period64, err := strconv.ParseInt(self.Args.Interval, 10, 8)
+	if err != nil {
+		return nil, err
+	}
+	query := jsonutils.NewDict()
+	query.Add(jsonutils.NewString("0"), common.KEY_LIMIT)
+	query.Add(jsonutils.NewBool(true), common.DETAILS)
+	query.Add(jsonutils.NewString("system"), "scope")
+	query.Add(jsonutils.NewString(id), "alert_id")
+	query.Add(jsonutils.NewString("alerting"), "state")
+	query.Add(jsonutils.NewString(fmt.Sprintf(`created_at.between("%s","%s")`,
+		now.Add(-time.Hour*24*time.Duration(period64)).Format(timeutils.MysqlTimeFormat),
+		now.Format(timeutils.MysqlTimeFormat))),
+		"filter")
+
+	alertRecords, err := self.ListAllResource(modules.AlertRecordManager, query)
+	if err != nil {
+		return nil, err
+	}
+	return alertRecords, nil
+}

pkg/cloudmon/collectors/alertrecordhistorymon/alertrecordhistoryservice.go
@@ -1,0 +1,54 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package alertrecordhistorymon
+
+import (
+	"time"
+
+	"yunion.io/x/jsonutils"
+
+	"yunion.io/x/onecloud/pkg/cloudmon/collectors/common"
+	"yunion.io/x/onecloud/pkg/util/influxdb"
+)
+
+func (self *SAlertRecordHistoryReport) collectMetric(alertRecords []jsonutils.JSONObject) error {
+	dataList := make([]influxdb.SMetricData, 0)
+	matchRecord := self.getMaxEvalMatchOfAlertRecord(alertRecords)
+	if matchRecord == nil {
+		return nil
+	}
+
+	metric, err := self.NewMetricFromJson(matchRecord)
+	if err != nil {
+		return err
+	}
+	metric.Timestamp = time.Now()
+	metric.Name = ALERT_RECORD_HISTORY_MEASUREMENT
+	dataList = append(dataList, metric)
+	return common.SendMetrics(self.Session, dataList, self.Args.Debug, ALERT_RECORD_HISTORY_DATABASE)
+}
+
+func (self *SAlertRecordHistoryReport) getMaxEvalMatchOfAlertRecord(alertRecords []jsonutils.JSONObject) jsonutils.JSONObject {
+	var matchRecord jsonutils.JSONObject
+	maxCount := int64(0)
+	for _, record := range alertRecords {
+		resNum, _ := record.Int("res_num")
+		if resNum > maxCount {
+			maxCount = resNum
+			matchRecord = record
+		}
+	}
+	return matchRecord
+}

pkg/cloudmon/collectors/alertrecordhistorymon/alertrecordhistoryutil.go
@@ -1,0 +1,21 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package alertrecordhistorymon
+
+const (
+	ALERT_RECORD_HISTORY_FIELD       = "res_num"
+	ALERT_RECORD_HISTORY_MEASUREMENT = "alert_record_history"
+	ALERT_RECORD_HISTORY_DATABASE    = "monitor"
+)

pkg/cloudmon/collectors/apsaramon/apsaracloudreport.go
@@ -1,0 +1,96 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package apsaramon
+
+import (
+	"yunion.io/x/jsonutils"
+
+	"yunion.io/x/onecloud/pkg/apis/compute"
+	"yunion.io/x/onecloud/pkg/cloudmon/collectors/common"
+	"yunion.io/x/onecloud/pkg/mcclient"
+	"yunion.io/x/onecloud/pkg/mcclient/modules"
+)
+
+func init() {
+	factory := SApsaraCloudReportFactory{}
+	common.RegisterFactory(&factory)
+}
+
+type SApsaraCloudReportFactory struct {
+}
+
+func (self *SApsaraCloudReportFactory) NewCloudReport(provider *common.SProvider, session *mcclient.ClientSession,
+	args *common.ReportOptions, operatorType string) common.ICloudReport {
+	return &SApsaraCloudReport{
+		common.CloudReportBase{
+			SProvider: provider,
+			Session:   session,
+			Args:      args,
+			Operator:  operatorType,
+		},
+	}
+}
+
+func (self *SApsaraCloudReportFactory) GetId() string {
+	return compute.CLOUD_PROVIDER_APSARA
+}
+
+type SApsaraCloudReport struct {
+	common.CloudReportBase
+}
+
+func (self *SApsaraCloudReport) Report() error {
+	var servers []jsonutils.JSONObject
+	var err error
+	switch self.Operator {
+	case "redis":
+		servers, err = self.GetAllserverOfThisProvider(&modules.ElasticCache)
+	case "rds":
+		servers, err = self.GetAllserverOfThisProvider(&modules.DBInstance)
+	case "oss":
+		servers, err = self.GetAllserverOfThisProvider(&modules.Buckets)
+	case "elb":
+		servers, err = self.GetAllserverOfThisProvider(&modules.Loadbalancers)
+	default:
+		servers, err = self.GetAllserverOfThisProvider(&modules.Servers)
+	}
+	providerInstance, err := self.InitProviderInstance()
+	if err != nil {
+		return err
+	}
+	regionList, regionServerMap, err := self.GetAllRegionOfServers(servers, providerInstance)
+	if err != nil {
+		return err
+	}
+	for _, region := range regionList {
+		servers := regionServerMap[region.GetGlobalId()]
+		switch self.Operator {
+		case "server":
+			err = self.collectRegionMetricOfHost(region, servers)
+		case "redis":
+			err = self.collectRegionMetricOfRedis(region, servers)
+		case "rds":
+			err = self.collectRegionMetricOfRds(region, servers)
+		case "oss":
+			err = self.collectRegionMetricOfOss(region, servers)
+		case "elb":
+			err = self.collectRegionMetricOfElb(region, servers)
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}