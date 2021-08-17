0549d821f9853a0b78fa87807a8eddfad086b86f
Zexi Li <zexi.li@icloud.com>
2021-08-17 20:07:42 +0800

    feat(misc): update build and service base image to use go v1.15.15

--- Makefile.common.mk
+++ Makefile.common.mk
@@ -7,8 +7,8 @@ endif
 ModBaseName:=$(notdir $(ModName))
 
 DockerImageRegistry?=registry.cn-beijing.aliyuncs.com
-DockerImageAlpineBuild?=$(DockerImageRegistry)/yunionio/alpine-build:1.0-5
-DockerImageCentOSBuild?=$(DockerImageRegistry)/yunionio/centos-build:1.1-4
+DockerImageAlpineBuild?=$(DockerImageRegistry)/yunionio/alpine-build:1.1-5
+DockerImageCentOSBuild?=$(DockerImageRegistry)/yunionio/centos-build:1.1-5
 
 
 EnvIf=$(if $($(1)),$(1)=$($(1)))
