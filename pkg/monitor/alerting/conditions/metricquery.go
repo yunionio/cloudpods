c24c923a8fd70f13190d33b92149df9a8cd88893
zhaoxiangchun <1422928955@qq.com>
2021-08-25 10:00:02 +0800

    fix(monitor):fix monitor query top 5

--- pkg/monitor/alerting/conditions/metricquery.go
+++ pkg/monitor/alerting/conditions/metricquery.go
@@ -83,15 +83,11 @@ func (query *MetricQueryCondition) ExecuteQuery() (*mq.Metrics, error) {
 		Series: make(tsdb.TimeSeriesSlice, 0),
 		Metas:  queryResult.metas,
 	}
-	if len(query.QueryCons[0].ResType) == 0 ||
-		strings.HasPrefix(query.QueryCons[0].ResType, monitor.EXT_PREFIX) {
+	if query.noCheckSeries() {
 		metrics.Series = queryResult.series
 		return &metrics, nil
 	}
-	//allResources, err := query.QueryCons[0].GetQueryResources()
-	//if err != nil {
-	//	return nil, errors.Wrap(err, "MetricQueryCondition GetQueryResources err")
-	//}
+
 	for _, serie := range queryResult.series {
 		isLatestOfSerie, resource := query.QueryCons[0].serieIsLatestResource(nil, serie)
 		if !isLatestOfSerie {
@@ -101,6 +97,17 @@ func (query *MetricQueryCondition) ExecuteQuery() (*mq.Metrics, error) {
 		metrics.Series = append(metrics.Series, serie)
 	}
 	return &metrics, nil
+}
+
+func (query *MetricQueryCondition) noCheckSeries() bool {
+	if len(query.QueryCons[0].ResType) == 0 ||
+		strings.HasPrefix(query.QueryCons[0].ResType, monitor.EXT_PREFIX) {
+		return true
+	}
+	if len(query.QueryCons[0].Query.Model.GroupBy) == 0 {
+		return true
+	}
+	return false
 }
 
 func (c *MetricQueryCondition) executeQuery(context *alerting.EvalContext, timeRange *tsdb.TimeRange) (*queryResult, error) {
