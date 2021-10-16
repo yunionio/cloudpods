21cc4903f2091f162ad3fa0fef3aa4863d45b88c
Qiu Jian <qiujian@yunionyun.com>
2021-10-17 02:51:44 +0800

    fix: make minio endpoint compatible with multicloud objectstorage

--- pkg/image/drivers/s3/minio.go
+++ pkg/image/drivers/s3/minio.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 
 	"yunion.io/x/log"
 	"yunion.io/x/pkg/errors"
@@ -56,6 +57,9 @@ func (c *S3Client) getBucket() (cloudprovider.ICloudBucket, error) {
 func Init(endpoint, accessKey, secretKey, bucket string, useSSL bool) error {
 	if client != nil {
 		return nil
+	}
+	if !strings.HasPrefix(endpoint, "http://") && !strings.HasPrefix(endpoint, "https://") {
+		endpoint = "http://" + endpoint
 	}
 	cfg := objectstore.NewObjectStoreClientConfig(endpoint, accessKey, secretKey)
 	minioClient, err := objectstore.NewObjectStoreClient(cfg)
