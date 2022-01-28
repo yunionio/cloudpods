74c8f6d1dd76d22f26fbd53822c8b4cea4bcd658
屈轩 <qu_xuan@icloud.com>
2022-01-28 11:18:54 +0800

    fix(host,region): add action log (#13304)

--- pkg/hostman/hostinfo/hostinfo.go
+++ pkg/hostman/hostinfo/hostinfo.go
@@ -1185,7 +1185,7 @@ func (h *SHostInfo) onUpdateHostInfoSucc(hostbody jsonutils.JSONObject) {
 	if memReserved, _ := hostbody.Int("mem_reserved"); memReserved == 0 {
 		h.updateHostReservedMem()
 	} else {
-		h.PutHostOffline()
+		h.PutHostOffline("")
 	}
 }
 
@@ -1213,10 +1213,13 @@ func (h *SHostInfo) getReservedMem() int {
 	return reserved
 }
 
-func (h *SHostInfo) PutHostOffline() {
+func (h *SHostInfo) PutHostOffline(reason string) {
 	data := jsonutils.NewDict()
 	if options.HostOptions.EnableHealthChecker {
 		data.Set("update_health_status", jsonutils.JSONTrue)
+	}
+	if len(reason) > 0 {
+		data.Set("reason", jsonutils.NewString(reason))
 	}
 	_, err := modules.Hosts.PerformAction(
 		h.GetSession(), h.HostId, "offline", data)
