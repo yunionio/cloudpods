c3523cff4fca5a9007efef48ed7c50f93f8052cf
ioito <qu_xuan@icloud.com>
2022-02-11 11:40:02 +0800

    feat(region): bingo cloud cli

---
+++ pkg/multicloud/bingocloud/bingo.go
@@ -0,0 +1,203 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bingocloud
+
+import (
+	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"net/url"
+	"sort"
+	"strings"
+	"time"
+
+	xj "github.com/basgys/goxml2json"
+	"github.com/pkg/errors"
+
+	"yunion.io/x/jsonutils"
+	"yunion.io/x/log"
+
+	api "yunion.io/x/onecloud/pkg/apis/compute"
+	"yunion.io/x/onecloud/pkg/cloudprovider"
+	"yunion.io/x/onecloud/pkg/util/httputils"
+)
+
+const (
+	CLOUD_PROVIDER_BINGO_CLOUD = api.CLOUD_PROVIDER_BINGO_CLOUD
+)
+
+type BingoCloudConfig struct {
+	cpcfg     cloudprovider.ProviderConfig
+	endpoint  string
+	accessKey string
+	secretKey string
+
+	debug bool
+}
+
+func NewBingoCloudClientConfig(endpoint, accessKey, secretKey string) *BingoCloudConfig {
+	cfg := &BingoCloudConfig{
+		endpoint:  endpoint,
+		accessKey: accessKey,
+		secretKey: secretKey,
+	}
+	return cfg
+}
+
+func (cfg *BingoCloudConfig) CloudproviderConfig(cpcfg cloudprovider.ProviderConfig) *BingoCloudConfig {
+	cfg.cpcfg = cpcfg
+	return cfg
+}
+
+func (cfg *BingoCloudConfig) Debug(debug bool) *BingoCloudConfig {
+	cfg.debug = debug
+	return cfg
+}
+
+type SBingoCloudClient struct {
+	*BingoCloudConfig
+
+	regions []SRegion
+}
+
+func NewBingoCloudClient(cfg *BingoCloudConfig) (*SBingoCloudClient, error) {
+	client := &SBingoCloudClient{BingoCloudConfig: cfg}
+	var err error
+	client.regions, err = client.GetRegions()
+	if err != nil {
+		return nil, err
+	}
+	for i := range client.regions {
+		client.regions[i].client = client
+	}
+	return client, nil
+}
+
+func (self *SBingoCloudClient) GetAccountId() string {
+	return self.endpoint
+}
+
+func (self *SBingoCloudClient) GetRegion(id string) (*SRegion, error) {
+	for i := range self.regions {
+		if self.regions[i].RegionId == id {
+			return &self.regions[i], nil
+		}
+	}
+	if len(id) == 0 {
+		return &self.regions[0], nil
+	}
+	return nil, cloudprovider.ErrNotFound
+}
+
+func (cli *SBingoCloudClient) getDefaultClient(timeout time.Duration) *http.Client {
+	client := httputils.GetDefaultClient()
+	if timeout > 0 {
+		client = httputils.GetTimeoutClient(timeout)
+	}
+	proxy := func(req *http.Request) (*url.URL, error) {
+		if cli.cpcfg.ProxyFunc != nil {
+			cli.cpcfg.ProxyFunc(req)
+		}
+		return nil, nil
+	}
+	httputils.SetClientProxyFunc(client, proxy)
+	return client
+}
+
+func (self *SBingoCloudClient) sign(query string) string {
+	uri, _ := url.Parse(self.endpoint)
+	items := strings.Split(query, "&")
+	sort.Slice(items, func(i, j int) bool {
+		x0, y0 := strings.Split(items[i], "=")[0], strings.Split(items[j], "=")[0]
+		return x0 < y0
+	})
+	path := "/"
+	if len(uri.Path) > 0 {
+		path = uri.Path
+	}
+	stringToSign := fmt.Sprintf("POST\n%s\n%s\n", uri.Host, path) + strings.Join(items, "&")
+	hmac := hmac.New(sha256.New, []byte(self.secretKey))
+	hmac.Write([]byte(stringToSign))
+	return base64.StdEncoding.EncodeToString(hmac.Sum(nil))
+}
+
+func (self *SBingoCloudClient) invoke(action string, params map[string]string) (jsonutils.JSONObject, error) {
+	var encode = func(k, v string) string {
+		d := url.Values{}
+		d.Set(k, v)
+		return d.Encode()
+	}
+	query := encode("Action", action)
+	for k, v := range params {
+		query += "&" + encode(k, v)
+	}
+	// 2022-02-11T03:57:37.000Z
+	timeStamp := time.Now().Format("2006-01-02T15:04:05.000Z")
+	query += "&" + encode("Timestamp", timeStamp)
+	query += "&" + encode("AWSAccessKeyId", self.accessKey)
+	query += "&" + encode("Version", "2009-08-15")
+	query += "&" + encode("SignatureVersion", "2")
+	query += "&" + encode("SignatureMethod", "HmacSHA256")
+	query += "&" + encode("Signature", self.sign(query))
+	client := self.getDefaultClient(0)
+	resp, err := httputils.Request(client, context.Background(), httputils.POST, self.endpoint, nil, strings.NewReader(query), self.debug)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	result, err := xj.Convert(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if self.debug {
+		log.Debugf("response: %s", result.String())
+	}
+
+	obj, err := jsonutils.Parse([]byte(result.String()))
+	if err != nil {
+		return nil, errors.Wrapf(err, "jsonutils.Parse")
+	}
+
+	respKey := action + "Response"
+	if obj.Contains(respKey) {
+		obj, err = obj.Get(respKey)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// 处理请求单个资源情况
+	if strings.HasPrefix(action, "Describe") && strings.HasSuffix(action, "s") {
+		objDict := obj.(*jsonutils.JSONDict)
+		for k, v := range objDict.Value() {
+			if (strings.HasSuffix(k, "Set") || k == "regionInfo") && v.Contains("item") {
+				value := v.(*jsonutils.JSONDict)
+				item, _ := v.Get("item")
+				_, ok := item.(*jsonutils.JSONArray)
+				if !ok {
+					value.Set("item", jsonutils.NewArray(item))
+					objDict.Set(k, value)
+				}
+			}
+		}
+	}
+
+	return obj, nil
+}
---
+++ pkg/multicloud/bingocloud/instance.go
@@ -0,0 +1,152 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bingocloud
+
+import (
+	"fmt"
+	"time"
+)
+
+type SInstance struct {
+	ReservationId string `json:"reservationId"`
+	OwnerId       string
+	GroupSet      struct {
+		Item struct {
+			GroupId   string
+			GroupName string
+		}
+	}
+	InstancesSet struct {
+		Item struct {
+			InstanceId    string `json:"instanceId"`
+			InstanceName  string `json:"instanceName"`
+			HostName      string `json:"hostName"`
+			ImageId       string `json:"imageId"`
+			InstanceState struct {
+				Code            int    `json:"code"`
+				Name            string `json:"name"`
+				PendingProgress string `json:"pendingProgress"`
+			} `json:"instanceState"`
+			PrivateDNSName     string `json:"privateDnsName"`
+			DNSName            string `json:"dnsName"`
+			PrivateIPAddress   string `json:"privateIpAddress"`
+			PrivateIPAddresses string `json:"privateIpAddresses"`
+			IPAddress          string `json:"ipAddress"`
+			NifInfo            string `json:"nifInfo"`
+			KeyName            string `json:"keyName"`
+			AmiLaunchIndex     int    `json:"amiLaunchIndex"`
+			ProductCodesSet    struct {
+				Item struct {
+					ProductCode string `json:"productCode"`
+				} `json:"item"`
+			} `json:"productCodesSet"`
+			InstanceType   string    `json:"instanceType"`
+			VmtypeCPU      int       `json:"vmtype_cpu"`
+			VmtypeMem      int       `json:"vmtype_mem"`
+			VmtypeDisk     int       `json:"vmtype_disk"`
+			VmtypeGpu      int       `json:"vmtype_gpu"`
+			VmtypeSsd      int       `json:"vmtype_ssd"`
+			VmtypeHdd      int       `json:"vmtype_hdd"`
+			VmtypeHba      int       `json:"vmtype_hba"`
+			VmtypeSriov    int       `json:"vmtype_sriov"`
+			LaunchTime     time.Time `json:"launchTime"`
+			RootDeviceType string    `json:"rootDeviceType"`
+			HostAddress    string    `json:"hostAddress"`
+			Platform       string    `json:"platform"`
+			UseCompactMode bool      `json:"useCompactMode"`
+			ExtendDisk     bool      `json:"extendDisk"`
+			Placement      struct {
+				AvailabilityZone string `json:"availabilityZone"`
+			} `json:"placement"`
+			Namespace    string `json:"namespace"`
+			KernelId     string `json:"kernelId"`
+			RamdiskId    string `json:"ramdiskId"`
+			OperName     string `json:"operName"`
+			OperProgress int    `json:"operProgress"`
+			Features     string `json:"features"`
+			Monitoring   struct {
+				State string `json:"state"`
+			} `json:"monitoring"`
+			SubnetId              string    `json:"subnetId"`
+			VpcId                 string    `json:"vpcId"`
+			StorageId             string    `json:"storageId"`
+			DisableAPITermination bool      `json:"disableApiTermination"`
+			Vncdisabled           bool      `json:"vncdisabled"`
+			StartTime             time.Time `json:"startTime"`
+			CustomStatus          string    `json:"customStatus"`
+			SystemStatus          int       `json:"systemStatus"`
+			NetworkStatus         int       `json:"networkStatus"`
+			ScheduleTags          string    `json:"scheduleTags"`
+			StorageScheduleTags   string    `json:"storageScheduleTags"`
+			IsEncrypt             bool      `json:"isEncrypt"`
+			IsImported            bool      `json:"isImported"`
+			Ec2Version            string    `json:"ec2Version"`
+			Passphrase            string    `json:"passphrase"`
+			DrsEnabled            bool      `json:"drs_enabled"`
+			LaunchPriority        int       `json:"launchPriority"`
+			CPUPriority           int       `json:"cpuPriority"`
+			MemPriority           int       `json:"memPriority"`
+			CPUQuota              int       `json:"cpuQuota"`
+			AutoMigrate           bool      `json:"autoMigrate"`
+			DrMirrorId            string    `json:"drMirrorId"`
+			BlockDeviceMapping    struct {
+				Item struct {
+					DeviceName string `json:"deviceName"`
+					Ebs        struct {
+						AttachTime          time.Time `json:"attachTime"`
+						DeleteOnTermination bool      `json:"deleteOnTermination"`
+						Status              string    `json:"status"`
+						VolumeId            string    `json:"volumeId"`
+						Size                int       `json:"size"`
+					} `json:"ebs"`
+				} `json:"item"`
+			} `json:"blockDeviceMapping"`
+			EnableLiveScaleup bool   `json:"enableLiveScaleup"`
+			ImageBytes        int64  `json:"imageBytes"`
+			StatusReason      string `json:"statusReason"`
+			Hypervisor        string `json:"hypervisor"`
+			Bootloader        string `json:"bootloader"`
+			BmMachineId       string `json:"bmMachineId"`
+		}
+	}
+}
+
+func (self *SRegion) DescribeInstances(id string, maxResult int, nextToken string) ([]SInstance, string, error) {
+	params := map[string]string{}
+	if len(id) > 0 {
+		params["instanceId"] = id
+	}
+	if maxResult > 0 {
+		params["maxRecords"] = fmt.Sprintf("%d", maxResult)
+	}
+	if len(nextToken) > 0 {
+		params["nextToken"] = nextToken
+	}
+	resp, err := self.invoke("DescribeInstances", params)
+	if err != nil {
+		return nil, "", err
+	}
+	result := struct {
+		NextToken      string
+		ReservationSet struct {
+			Item []SInstance
+		}
+	}{}
+	err = resp.Unmarshal(&result)
+	if err != nil {
+		return nil, "", err
+	}
+	return result.ReservationSet.Item, result.NextToken, nil
+}
