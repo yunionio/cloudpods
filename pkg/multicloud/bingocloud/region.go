aa88ebaaad07163ba3dff86803c7daa68fe2a0b7
牛魔王爱紫霞 <74589425+SRC666-666@users.noreply.github.com>
2022-02-16 23:33:12 +0800

    Update region.go

--- pkg/multicloud/bingocloud/region.go
+++ pkg/multicloud/bingocloud/region.go
@@ -1,11 +1,3 @@
-/*
- * @Author: your name
- * @Date: 2022-02-16 18:07:12
- * @LastEditTime: 2022-02-16 22:37:06
- * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \cloudpods\pkg\multicloud\bingocloud\region.go
- */
 // Copyright 2019 Yunion
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
