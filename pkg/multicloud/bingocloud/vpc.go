c8c0f3a535f30cf7442cec9d2170373152f8416a
牛魔王爱紫霞 <74589425+SRC666-666@users.noreply.github.com>
2022-02-16 23:34:55 +0800

    Update vpc.go

--- pkg/multicloud/bingocloud/vpc.go
+++ pkg/multicloud/bingocloud/vpc.go
@@ -1,11 +1,3 @@
-/*
- * @Author: your name
- * @Date: 2022-02-16 18:23:22
- * @LastEditTime: 2022-02-16 21:16:12
- * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \cloudpods\pkg\multicloud\bingocloud\vpc.go
- */
 // Copyright 2019 Yunion
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
