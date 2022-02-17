295a4ab4b164557ebea3a4fda21dd2e8aec68407
牛魔王爱紫霞 <74589425+SRC666-666@users.noreply.github.com>
2022-02-17 22:00:07 +0800

    Add files via upload

--- pkg/multicloud/bingocloud/provider/provider.go
+++ pkg/multicloud/bingocloud/provider/provider.go
@@ -117,8 +117,10 @@ func (self *SBingoCloudProvider) GetVersion() string {
 	return "2009-08-15"
 }
 
+//	GetSubAccounts
 func (self *SBingoCloudProvider) GetSubAccounts() ([]cloudprovider.SSubAccount, error) {
-	return nil, cloudprovider.ErrNotImplemented
+	// return nil, cloudprovider.ErrNotImplemented
+	return self.client.GetSubAccounts()
 }
 
 func (self *SBingoCloudProvider) GetAccountId() string {
@@ -127,11 +129,15 @@ func (self *SBingoCloudProvider) GetAccountId() string {
 
 func (self *SBingoCloudProvider) GetIRegions() []cloudprovider.ICloudRegion {
 	// TODO
-	return nil
+	// return nil
+	return self.client.GetIRegions()
 }
 
+//	GetIRegionById
 func (self *SBingoCloudProvider) GetIRegionById(id string) (cloudprovider.ICloudRegion, error) {
-	return nil, cloudprovider.ErrNotImplemented
+	// return nil, cloudprovider.ErrNotImplemented
+	return self.client.GetIRegionById(id)
+
 }
 
 func (self *SBingoCloudProvider) GetBalance() (float64, string, error) {
