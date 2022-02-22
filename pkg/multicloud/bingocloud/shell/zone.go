79a5d321108ee6b8f790d0d90e9830a45d4a6005
牛魔王爱紫霞 <74589425+SRC666-666@users.noreply.github.com>
2022-02-22 17:36:41 +0800

    Update zone.go

--- pkg/multicloud/bingocloud/shell/zone.go
+++ pkg/multicloud/bingocloud/shell/zone.go
@@ -1,11 +1,3 @@
-/*
- * @Author: your name
- * @Date: 2022-02-17 23:35:30
- * @LastEditTime: 2022-02-17 23:58:34
- * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \cloudpods\pkg\multicloud\bingocloud\shell\zone.go
- */
 // Copyright 2019 Yunion
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
