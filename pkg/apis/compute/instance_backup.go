f3e95e6b424605108557b6bcd4a75c4d2a47b270
rainzm <mjoycarry@gmail.com>
2022-03-04 17:42:00 +0800

    feat(region): add status in snapshot or save phase for instancebackup

--- pkg/apis/compute/instance_backup.go
+++ pkg/apis/compute/instance_backup.go
@@ -1,3 +1,17 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package compute
 
 import "yunion.io/x/onecloud/pkg/apis"
@@ -15,6 +29,11 @@ const (
 
 	INSTANCE_BACKUP_STATUS_CREATING_FROM_PACKAGE      = "creating_from_package"
 	INSTANCE_BACKUP_STATUS_CREATE_FROM_PACKAGE_FAILED = "create_from_package_failed"
+
+	INSTANCE_BACKUP_STATUS_SNAPSHOT        = "snapshot"
+	INSTANCE_BACKUP_STATUS_SNAPSHOT_FAILED = "snapshot_failed"
+	INSTANCE_BACKUP_STATUS_SAVING          = "saving"
+	INSTANCE_BACKUP_STATUS_SAVE_FAILED     = "save_failed"
 )
 
 type InstanceBackupListInput struct {
