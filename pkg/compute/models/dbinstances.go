9b6df5d24d2789cf73e403cd690a6f617cd98f6b
ioito <qu_xuan@icloud.com>
2022-03-15 20:36:26 +0800

    fix(region): sync owner id

--- pkg/compute/models/dbinstances.go
+++ pkg/compute/models/dbinstances.go
@@ -1710,7 +1710,7 @@ func (self *SDBInstance) SyncWithCloudDBInstance(ctx context.Context, userCred m
 		return err
 	}
 	syncVirtualResourceMetadata(ctx, userCred, self, extInstance)
-	SyncCloudProject(userCred, self, userCred, extInstance, provider.Id)
+	SyncCloudProject(userCred, self, provider.GetOwnerId(), extInstance, provider.Id)
 	db.OpsLog.LogSyncUpdate(self, diff, userCred)
 	if len(diff) > 0 {
 		notifyclient.EventNotify(ctx, userCred, notifyclient.SEventNotifyParam{
@@ -1804,7 +1804,7 @@ func (manager *SDBInstanceManager) newFromCloudDBInstance(ctx context.Context, u
 	}
 
 	syncVirtualResourceMetadata(ctx, userCred, &instance, extInstance)
-	SyncCloudProject(userCred, &instance, ownerId, extInstance, provider.Id)
+	SyncCloudProject(userCred, &instance, provider.GetOwnerId(), extInstance, provider.Id)
 
 	db.OpsLog.LogEvent(&instance, db.ACT_CREATE, instance.GetShortDesc(ctx), userCred)
 
