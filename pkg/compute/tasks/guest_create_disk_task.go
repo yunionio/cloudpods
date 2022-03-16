cbee639b5c409b9641fbabe5d0c0e63ec412ad6a
rainzm <mjoycarry@gmail.com>
2022-03-16 14:42:29 +0800

    feat(region,esxiagent): support specifying storage when creating a data disk

--- pkg/compute/tasks/guest_create_disk_task.go
+++ pkg/compute/tasks/guest_create_disk_task.go
@@ -285,10 +285,16 @@ func (self *ESXiGuestCreateDiskTask) OnInit(ctx context.Context, obj db.IStandal
 			osProf := guest.GetOSProfile()
 			d.Driver = osProf.DiskDriver
 		}
+		storage, err := disk.GetStorage()
+		if err != nil {
+			self.SetStageFailed(ctx, jsonutils.NewString(err.Error()))
+			return
+		}
 		opts := cloudprovider.GuestDiskCreateOptions{
-			SizeMb: disk.DiskSize,
-			UUID:   disk.Id,
-			Driver: d.Driver,
+			SizeMb:    disk.DiskSize,
+			UUID:      disk.Id,
+			Driver:    d.Driver,
+			StorageId: storage.GetExternalId(),
 		}
 		_, err = ivm.CreateDisk(ctx, &opts)
 		if err != nil {
@@ -323,7 +329,6 @@ func (self *ESXiGuestCreateDiskTask) OnInit(ctx context.Context, obj db.IStandal
 		}
 
 		disk.SetStatus(self.UserCred, api.DISK_READY, "create disk success")
-		storage, _ := disk.GetStorage()
 		storage.ClearSchedDescCache()
 		db.OpsLog.LogEvent(disk, db.ACT_ALLOCATE, disk.GetShortDesc(ctx), self.UserCred)
 		db.OpsLog.LogAttachEvent(ctx, guest, disk, self.UserCred, disk.GetShortDesc(ctx))
