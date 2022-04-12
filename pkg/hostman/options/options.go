56d76b4682fb77cf50543f94fe39b22d769b2ee4
Qiu Jian <qiujian@yunionyun.com>
2022-04-12 12:06:35 +0800

    fix: change qemu default version to 4.2.0

--- pkg/hostman/options/options.go
+++ pkg/hostman/options/options.go
@@ -71,7 +71,7 @@ type SHostOptions struct {
 	LocalImagePath  []string `help:"Local image storage paths"`
 	SharedStorages  []string `help:"Path of shared storages"`
 
-	DefaultQemuVersion string `help:"Default qemu version" default:"2.12.1"`
+	DefaultQemuVersion string `help:"Default qemu version" default:"4.2.0"`
 
 	DhcpRelay       []string `help:"DHCP relay upstream"`
 	DhcpLeaseTime   int      `default:"100663296" help:"DHCP lease time in seconds"`
