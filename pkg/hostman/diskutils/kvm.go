2fdb1abf39b89b668e693dc7146ca2f22aafd942
Qiu Jian <qiujian@yunionyun.com>
2022-05-02 10:30:28 +0800

    fix: save guest image with empty partition fail

--- pkg/hostman/diskutils/kvm.go
+++ pkg/hostman/diskutils/kvm.go
@@ -15,7 +15,6 @@
 package diskutils
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -149,7 +148,7 @@ func (d *SKVMGuestDisk) mountKvmRootfs(readonly bool) (fsdriver.IRootFsDriver, e
 		}
 	}
 	if len(partitions) == 0 {
-		return nil, fmt.Errorf("not found any partitions")
+		return nil, errors.Wrap(errors.ErrNotFound, "not found any partitions")
 	}
 	return nil, errors.NewAggregate(errs)
 }
