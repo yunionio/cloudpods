6a80f0f15654f5e50182d7a9b622c9e32dcd5c24
Qiu Jian <qiujian@yunionyun.com>
2022-05-09 07:13:57 +0800

    fix: adjust syslog format

--- pkg/logger/extern/syslog.go
+++ pkg/logger/extern/syslog.go
@@ -15,16 +15,24 @@
 package extern
 
 import (
+	"fmt"
 	"log/syslog"
 	"strings"
 
 	"yunion.io/x/log"
 	"yunion.io/x/pkg/errors"
+
+	"yunion.io/x/onecloud/pkg/appsrv"
 )
 
 var (
-	sysLog *syslog.Writer
+	sysLog          *syslog.Writer
+	syslogWorkerMan *appsrv.SWorkerManager
 )
+
+func init() {
+	syslogWorkerMan = appsrv.NewWorkerManager("syslogSenderWorkerManager", 1, 50, false)
+}
 
 func InitSyslog(url string) error {
 	proto := "tcp"
@@ -55,49 +63,100 @@ func InitSyslog(url string) error {
 }
 
 func Emergency(msg string) {
-	if sysLog != nil {
-		sysLog.Emerg(msg)
+	if sysLog == nil {
+		return
 	}
+	sendSyslog("emerg", msg)
 }
 
 func Alert(msg string) {
-	if sysLog != nil {
-		sysLog.Alert(msg)
+	if sysLog == nil {
+		return
 	}
+	sendSyslog("alert", msg)
 }
 
 func Critical(msg string) {
-	if sysLog != nil {
-		sysLog.Crit(msg)
+	if sysLog == nil {
+		return
 	}
+	sendSyslog("crit", msg)
 }
 
 func Error(msg string) {
-	if sysLog != nil {
-		sysLog.Err(msg)
+	if sysLog == nil {
+		return
 	}
+	sendSyslog("err", msg)
 }
 
 func Warning(msg string) {
-	if sysLog != nil {
-		sysLog.Warning(msg)
+	if sysLog == nil {
+		return
 	}
+	sendSyslog("warning", msg)
 }
 
 func Notice(msg string) {
-	if sysLog != nil {
-		sysLog.Notice(msg)
+	if sysLog == nil {
+		return
 	}
+	sendSyslog("notice", msg)
 }
 
 func Info(msg string) {
-	if sysLog != nil {
-		sysLog.Info(msg)
+	if sysLog == nil {
+		return
 	}
+	sendSyslog("info", msg)
 }
 
 func Debug(msg string) {
-	if sysLog != nil {
-		sysLog.Debug(msg)
+	if sysLog == nil {
+		return
 	}
+	sendSyslog("debug", msg)
+}
+
+func sendSyslog(facility string, msg string) {
+	log.Infof("%s", msg)
+	syslogWorkerMan.Run(&syslogTask{
+		facility: facility,
+		msg:      msg,
+	}, nil, nil)
+}
+
+type syslogTask struct {
+	facility string
+	msg      string
+}
+
+func (t *syslogTask) Run() {
+	if sysLog == nil {
+		return
+	}
+	switch t.facility {
+	case "info":
+		sysLog.Info(t.msg)
+	case "debug":
+		sysLog.Debug(t.msg)
+	case "emerg":
+		sysLog.Emerg(t.msg)
+	case "warning":
+		sysLog.Warning(t.msg)
+	case "notice":
+		sysLog.Notice(t.msg)
+	case "err":
+		sysLog.Err(t.msg)
+	case "crit":
+		sysLog.Crit(t.msg)
+	case "Alert":
+		sysLog.Alert(t.msg)
+	default:
+		sysLog.Info(t.msg)
+	}
+}
+
+func (t *syslogTask) Dump() string {
+	return fmt.Sprintf("syslogTask %v %s", t.facility, t.msg)
 }
