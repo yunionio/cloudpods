27b7c6b617a2c932a0539e7784cf100935653945
Jian Qiu <swordqiu@gmail.com>
2022-05-08 23:42:16 +0800

    fix: adjust secadmin and opsadmin policies (#14219)
    Co-authored-by: Qiu Jian <qiujian@yunionyun.com>

--- pkg/keystone/locale/predefined_yaml.go
+++ pkg/keystone/locale/predefined_yaml.go
@@ -44,6 +44,7 @@ policy:
     secgroups:
       '*': deny
       list: allow
+      get: allow
     servers:
       '*': allow
       perform:
@@ -51,6 +52,12 @@ policy:
         start: deny
         stop: deny
         change-owner: deny
+        add-secgroup: deny
+        set-secgroup: deny
+        revoke-secgroup: deny
+        revoke-admin-secgroup: deny
+        assign-secgroup: deny
+        assign-admin-secgroup: deny
   image:
     '*':
       '*': allow
@@ -103,6 +110,8 @@ policy:
       '*': deny
     secgroups:
       '*': allow
+    secgrouprules:
+      '*': allow
     servers:
       '*': deny
       delete: allow
@@ -111,6 +120,12 @@ policy:
       perform:
         '*': deny
         change-owner: allow
+        add-secgroup: allow
+        set-secgroup: allow
+        revoke-secgroup: allow
+        revoke-admin-secgroup: allow
+        assign-secgroup: allow
+        assign-admin-secgroup: allow
         purge: allow
   monitor:
     events:
