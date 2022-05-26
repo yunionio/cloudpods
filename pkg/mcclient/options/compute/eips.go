507c2922ec0a3c213a34896afeb520b91a53111a
ioito <qu_xuan@icloud.com>
2022-05-26 18:59:26 +0800

    fix(region): add filter for eip

--- pkg/mcclient/options/compute/eips.go
+++ pkg/mcclient/options/compute/eips.go
@@ -24,7 +24,7 @@ type ElasticipListOptions struct {
 	Region string `help:"List eips in cloudregion"`
 
 	Usable                    *bool  `help:"List all zones that is usable"`
-	UsableEipForAssociateType string `help:"With associate id filter which eip can associate" choices:"server|natgateway"`
+	UsableEipForAssociateType string `help:"With associate id filter which eip can associate" choices:"server|natgateway|loadbalancer"`
 	UsableEipForAssociateId   string `help:"With associate type filter which eip can associate"`
 
 	options.BaseListOptions
