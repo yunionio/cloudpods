d061c10c3b86ad713009ffa783f94f0b8980c2e0
Zexi Li <zexi.li@icloud.com>
2022-06-09 12:54:15 +0800

    fix(host): Telegraf 1.19.2 conf

--- pkg/hostman/system_service/telegraf.go
+++ pkg/hostman/system_service/telegraf.go
@@ -176,8 +176,11 @@ func (s *STelegraf) GetConfig(kwargs map[string]interface{}) string {
 	conf += "[[inputs.internal]]\n"
 	conf += "  collect_memstats = false\n"
 	conf += "\n"
-	conf += "[[inputs.http_listener]]\n"
+	conf += "[[inputs.http_listener_v2]]\n"
 	conf += "  service_address = \"127.0.0.1:8087\"\n"
+	conf += "  path = \"/write\"\n"
+	conf += "  data_source = \"body\"\n"
+	conf += "  data_format = \"influx\"\n"
 	conf += "\n"
 	return conf
 }
