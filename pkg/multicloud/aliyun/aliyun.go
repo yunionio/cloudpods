a6a36cb74d3f80cbcbf72d518b0cf67f0d3be888
屈轩 <qu_xuan@icloud.com>
2022-06-14 00:44:33 +0800

    fix(region): aliyun readonly (#14451)

--- pkg/multicloud/aliyun/aliyun.go
+++ pkg/multicloud/aliyun/aliyun.go
@@ -430,7 +430,7 @@ func (self *SAliyunClient) getSdkClient(regionId string) (*sdk.Client, error) {
 						}
 					}
 				}
-				for _, prefix := range []string{"Get", "List", "Describe"} {
+				for _, prefix := range []string{"Get", "List", "Describe", "Query"} {
 					if strings.HasPrefix(action, prefix) {
 						return respCheck, nil
 					}
