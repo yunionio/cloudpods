0fc91f054d6a0235c93d3174b5b40b8bfb5eba7b
mhf <mhf1018763435@163.com>
2022-10-27 11:40:32 +0800

    cloudpodsMonitor

--- pkg/multicloud/cloudpods/instance.go
+++ pkg/multicloud/cloudpods/instance.go
@@ -29,6 +29,7 @@ import (
 	modules "yunion.io/x/onecloud/pkg/mcclient/modules/compute"
 	"yunion.io/x/onecloud/pkg/mcclient/modules/identity"
 	"yunion.io/x/onecloud/pkg/mcclient/modules/logger"
+	"yunion.io/x/onecloud/pkg/mcclient/modules/monitor"
 	"yunion.io/x/onecloud/pkg/mcclient/modules/webconsole"
 	"yunion.io/x/onecloud/pkg/multicloud"
 )
@@ -512,4 +513,57 @@ func (self *SRegion) CreateInstance(hostId, hypervisor string, opts *cloudprovid
 	})
 	ins := &SInstance{}
 	return ins, self.create(&modules.Servers, input, ins)
+}
+
+func (region *SRegion) Monitor(opts *cloudprovider.MetricListOptions) ([]cloudprovider.MetricValues, error) {
+	return region.cli.GetMetrics(opts)
+}
+
+type SMetricData struct {
+	Id    string    `json:"id"`
+	Time  time.Time `json:"time"`
+	Value float64   `json:"value"`
+}
+
+func (cli *SCloudpodsClient) GetMetrics(opts *cloudprovider.MetricListOptions) ([]cloudprovider.MetricValues, error) {
+	brandArr := []string{"OneCloud", "VMware"}
+	metrics := []SMetricData{}
+	for i := 0; i < len(brandArr); i++ {
+		params := map[string]interface{}{
+			"metric_name": opts.MetricType,
+			"tags": map[string]interface{}{
+				"brand": brandArr[i],
+			},
+		}
+		onecloudObj, err := monitor.UnifiedMonitorManager.Get(cli.s, "simple-query", jsonutils.Marshal(params))
+		if err != nil {
+			return nil, err
+		}
+		tmp := []SMetricData{}
+		onecloudObj.Unmarshal(&tmp, "values")
+		metrics = append(metrics, tmp...)
+	}
+
+	idWithMetric := map[string][]cloudprovider.MetricValue{}
+	for _, v := range metrics {
+		if _, isExist := idWithMetric[v.Id]; !isExist {
+			idWithMetric[v.Id] = []cloudprovider.MetricValue{{
+				Timestamp: v.Time,
+				Value:     v.Value}}
+		} else {
+			idWithMetric[v.Id] = append(idWithMetric[v.Id], cloudprovider.MetricValue{
+				Timestamp: v.Time,
+				Value:     v.Value})
+		}
+	}
+
+	res := []cloudprovider.MetricValues{}
+	for _, metric := range metrics {
+		res = append(res, cloudprovider.MetricValues{
+			Id:         metric.Id,
+			MetricType: opts.MetricType,
+			Values:     idWithMetric[metric.Id],
+		})
+	}
+	return res, nil
 }
