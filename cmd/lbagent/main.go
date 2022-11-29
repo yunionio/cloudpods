5cc28818cad38ce5cb1df8b9108800282cf284b2
Qiu Jian <qiujian@yunionyun.com>
2022-11-30 07:58:28 +0800

    fix: lbagent switch to backup

--- cmd/lbagent/main.go
+++ cmd/lbagent/main.go
@@ -16,77 +16,16 @@ package main
 
 import (
 	"context"
-	"os"
-	"os/signal"
-	"sync"
-	"syscall"
 
-	"yunion.io/x/log"
-
-	app_common "yunion.io/x/onecloud/pkg/cloudcommon/app"
-	common_options "yunion.io/x/onecloud/pkg/cloudcommon/options"
 	"yunion.io/x/onecloud/pkg/lbagent"
 	"yunion.io/x/onecloud/pkg/util/atexit"
+	"yunion.io/x/onecloud/pkg/util/procutils"
 )
 
 func main() {
 	defer atexit.Handle()
 
-	opts := &lbagent.Options{}
-	commonOpts := &opts.CommonOptions
-	{
-		common_options.ParseOptions(opts, os.Args, "lbagent.conf", "lbagent")
-		app_common.InitAuth(commonOpts, func() {
-			log.Infof("auth finished ok")
-		})
-	}
-	if err := opts.ValidateThenInit(); err != nil {
-		log.Fatalf("opts validate: %s", err)
-	}
+	go procutils.WaitZombieLoop(context.TODO())
 
-	var haproxyHelper *lbagent.HaproxyHelper
-	var apiHelper *lbagent.ApiHelper
-	var haStateWatcher *lbagent.HaStateWatcher
-	var err error
-	{
-		haStateWatcher, err = lbagent.NewHaStateWatcher(opts)
-		if err != nil {
-			log.Fatalf("init ha state watcher failed: %s", err)
-		}
-	}
-	{
-		haproxyHelper, err = lbagent.NewHaproxyHelper(opts)
-		if err != nil {
-			log.Fatalf("init haproxy helper failed: %s", err)
-		}
-	}
-	{
-		apiHelper, err = lbagent.NewApiHelper(opts)
-		if err != nil {
-			log.Fatalf("init api helper failed: %s", err)
-		}
-		apiHelper.SetHaStateProvider(haStateWatcher)
-	}
-
-	{
-		wg := &sync.WaitGroup{}
-		cmdChan := make(chan *lbagent.LbagentCmd) // internal
-		ctx, cancelFunc := context.WithCancel(context.Background())
-		ctx = context.WithValue(ctx, "wg", wg)
-		ctx = context.WithValue(ctx, "cmdChan", cmdChan)
-		wg.Add(3)
-		go haStateWatcher.Run(ctx)
-		go haproxyHelper.Run(ctx)
-		go apiHelper.Run(ctx)
-
-		go func() {
-			sigChan := make(chan os.Signal)
-			signal.Notify(sigChan, syscall.SIGINT)
-			signal.Notify(sigChan, syscall.SIGTERM)
-			sig := <-sigChan
-			log.Infof("signal received: %s", sig)
-			cancelFunc()
-		}()
-		wg.Wait()
-	}
+	lbagent.StartService()
 }
