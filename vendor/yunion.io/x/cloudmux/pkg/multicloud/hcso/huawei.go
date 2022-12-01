9aacd60ee708da17a7f6cd44da1322425ce74632
ioito <qu_xuan@icloud.com>
2022-12-01 15:57:49 +0800

    fix(region): hcs vpc emulated

--- vendor/yunion.io/x/cloudmux/pkg/multicloud/hcso/huawei.go
+++ vendor/yunion.io/x/cloudmux/pkg/multicloud/hcso/huawei.go
@@ -484,6 +484,7 @@ func (self *SHuaweiClient) GetCapabilities() []string {
 		cloudprovider.CLOUD_CAPABILITY_NAT,
 		cloudprovider.CLOUD_CAPABILITY_NAS,
 		cloudprovider.CLOUD_CAPABILITY_MODELARTES,
+		cloudprovider.CLOUD_CAPABILITY_VPC_PEER,
 	}
 	// huawei objectstore is shared across projects(subscriptions)
 	// to avoid multiple project access the same bucket
--- vendor/yunion.io/x/cloudmux/pkg/multicloud/huawei/huawei.go
+++ vendor/yunion.io/x/cloudmux/pkg/multicloud/huawei/huawei.go
@@ -775,6 +775,7 @@ func (self *SHuaweiClient) GetCapabilities() []string {
 		cloudprovider.CLOUD_CAPABILITY_NAS,
 		cloudprovider.CLOUD_CAPABILITY_QUOTA + cloudprovider.READ_ONLY_SUFFIX,
 		cloudprovider.CLOUD_CAPABILITY_MODELARTES,
+		cloudprovider.CLOUD_CAPABILITY_VPC_PEER,
 	}
 	// huawei objectstore is shared across projects(subscriptions)
 	// to avoid multiple project access the same bucket
