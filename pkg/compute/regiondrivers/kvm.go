7c1d6f913dd87867e97353cc710c09baa03fdbe2
Qiu Jian <qiujian@yunionyun.com>
2023-02-26 09:12:44 +0800

    fix: set eipid after successful eip association

--- pkg/compute/regiondrivers/kvm.go
+++ pkg/compute/regiondrivers/kvm.go
@@ -1192,7 +1192,7 @@ func (self *SKVMRegionDriver) RequestPreSnapshotPolicyApply(ctx context.Context,
 }
 
 func (self *SKVMRegionDriver) ValidateCacheSecgroup(ctx context.Context, userCred mcclient.TokenCredential, secgroup *models.SSecurityGroup, vpc *models.SVpc, classic bool) error {
-	return fmt.Errorf("No need to cache secgroup for onecloud region")
+	return errors.Wrap(httperrors.ErrNotSupported, "No need to cache secgroup for onecloud region")
 }
 
 func (self *SKVMRegionDriver) ValidateCreateElasticcacheData(ctx context.Context, userCred mcclient.TokenCredential, ownerId mcclient.IIdentityProvider, input *api.ElasticcacheCreateInput) (*api.ElasticcacheCreateInput, error) {
@@ -1397,6 +1397,9 @@ func (self *SKVMRegionDriver) RequestCreateBackup(ctx context.Context, backup *m
 
 func (self *SKVMRegionDriver) RequestAssociateEip(ctx context.Context, userCred mcclient.TokenCredential, eip *models.SElasticip, input api.ElasticipAssociateInput, obj db.IStatusStandaloneModel, task taskman.ITask) error {
 	taskman.LocalTaskRun(task, func() (jsonutils.JSONObject, error) {
+		if err := eip.AssociateInstance(ctx, userCred, input.InstanceType, obj); err != nil {
+			return nil, errors.Wrapf(err, "associate eip %s(%s) to %s %s(%s)", eip.Name, eip.Id, obj.Keyword(), obj.GetName(), obj.GetId())
+		}
 		switch input.InstanceType {
 		case api.EIP_ASSOCIATE_TYPE_SERVER:
 			err := self.requestAssociateEipWithServer(ctx, userCred, eip, input, obj, task)
@@ -1416,11 +1419,8 @@ func (self *SKVMRegionDriver) RequestAssociateEip(ctx context.Context, userCred 
 		default:
 			return nil, errors.Wrapf(cloudprovider.ErrNotSupported, "instance type %s", input.InstanceType)
 		}
-		if err := eip.AssociateInstance(ctx, userCred, input.InstanceType, obj); err != nil {
-			return nil, errors.Wrapf(err, "associate eip %s(%s) to %s %s(%s)", eip.Name, eip.Id, obj.Keyword(), obj.GetName(), obj.GetId())
-		}
 		if err := eip.SetStatus(userCred, api.EIP_STATUS_READY, api.EIP_STATUS_ASSOCIATE); err != nil {
-			return nil, errors.Wrapf(err, "set eip status to %s", api.EIP_STATUS_ALLOCATE)
+			return nil, errors.Wrapf(err, "set eip status to %s", api.EIP_STATUS_READY)
 		}
 		return nil, nil
 	})
