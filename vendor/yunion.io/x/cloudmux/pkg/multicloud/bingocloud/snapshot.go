e50290b3c5b8740ea283e8147ed9e71653fa0544
屈轩 <qu_xuan@icloud.com>
2023-03-14 21:56:23 +0800

    fix(region): vender update (#16229)

--- vendor/yunion.io/x/cloudmux/pkg/multicloud/bingocloud/snapshot.go
+++ vendor/yunion.io/x/cloudmux/pkg/multicloud/bingocloud/snapshot.go
@@ -13,6 +13,7 @@ type SSnapshot struct {
 	region       *SRegion
 	SnapshotId   string
 	SnapshotName string
+	BackupId     string
 	VolumeId     string
 	Status       string
 	StartTime    string
@@ -37,6 +38,10 @@ func (self SSnapshot) GetGlobalId() string {
 func (self SSnapshot) GetCreatedAt() time.Time {
 	ct, _ := time.Parse("2006-01-02T15:04:05.000Z", self.StartTime)
 	return ct
+}
+
+func (self SSnapshot) GetDescription() string {
+	return self.Description
 }
 
 func (self SSnapshot) GetStatus() string {
@@ -88,6 +93,9 @@ func (self SSnapshot) GetDiskType() string {
 }
 
 func (self SSnapshot) Delete() error {
+	if self.BackupId != "" {
+		return self.region.deleteInstanceBackup(self.BackupId)
+	}
 	return self.region.deleteSnapshot(self.SnapshotId)
 }
 
