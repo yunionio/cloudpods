da6e696b80ef051645be60ad7fdb07e0695b757f
ioito <qu_xuan@icloud.com>
2023-04-11 20:54:43 +0800

    fix(common): show request time with rfc3339

--- pkg/mcclient/modulebase/base.go
+++ pkg/mcclient/modulebase/base.go
@@ -20,9 +20,11 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 
 	"yunion.io/x/jsonutils"
 
+	"yunion.io/x/onecloud/pkg/httperrors"
 	"yunion.io/x/onecloud/pkg/mcclient"
 	"yunion.io/x/onecloud/pkg/util/httputils"
 )
@@ -243,11 +245,13 @@ type SubmitResult struct {
 
 func SubmitResults2JSON(results []SubmitResult) jsonutils.JSONObject {
 	arr := jsonutils.NewArray()
+	now := time.Now().In(httperrors.GetTimeZone())
 	for _, r := range results {
 		obj := jsonutils.NewDict()
 		obj.Add(jsonutils.NewInt(int64(r.Status)), "status")
 		obj.Add(jsonutils.Marshal(r.Id), "id")
 		obj.Add(r.Data, "data")
+		obj.Add(jsonutils.NewString(now.Format(time.RFC3339)), "data", "time")
 		arr.Add(obj)
 	}
 	body := jsonutils.NewDict()
