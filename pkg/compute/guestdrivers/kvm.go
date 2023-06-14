933520f89c6d8d28dbdd9ed8988e5f5a9fdc9f9f
wanyaoqi <d3lx.yq@gmail.com>
2023-06-14 16:56:24 +0800

    fix(region): guest detach disk task duplicate callback

--- pkg/compute/guestdrivers/kvm.go
+++ pkg/compute/guestdrivers/kvm.go
@@ -465,23 +465,9 @@ func (self *SKVMGuestDriver) RequestDetachDisk(ctx context.Context, guest *model
 	host, _ := guest.GetHost()
 	header := task.GetTaskRequestHeader()
 	url := fmt.Sprintf("%s/servers/%s/status", host.ManagerUri, guest.Id)
-	_, res, _ := httputils.JSONRequest(httputils.GetDefaultClient(), ctx, "GET", url, header, nil, false)
-	if res != nil {
-		status, _ := res.GetString("status")
-		if status == "notfound" {
-			taskman.LocalTaskRun(task, func() (jsonutils.JSONObject, error) {
-				return nil, nil
-			})
-			return nil
-		}
-
-	}
-	return guest.StartSyncTask(
-		ctx,
-		task.GetUserCred(),
-		jsonutils.QueryBoolean(task.GetParams(), "sync_desc_only", false),
-		task.GetTaskId(),
-	)
+	task.SetStage("OnGetGuestStatus", nil)
+	_, _, err := httputils.JSONRequest(httputils.GetDefaultClient(), ctx, "GET", url, header, nil, false)
+	return err
 }
 
 func (self *SKVMGuestDriver) RequestAttachDisk(ctx context.Context, guest *models.SGuest, disk *models.SDisk, task taskman.ITask) error {
