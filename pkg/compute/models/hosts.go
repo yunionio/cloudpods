f5cbba1b61b5a75a684221cacb77924939ca8db9
wanyaoqi <d3lx.yq@gmail.com>
2023-09-18 15:42:51 +0800

    fix(region): host delete validate backup guests

--- pkg/compute/models/hosts.go
+++ pkg/compute/models/hosts.go
@@ -772,6 +772,14 @@ func (hh *SHost) validateDeleteCondition(ctx context.Context, purge bool) error 
 	if cnt > 0 {
 		return httperrors.NewNotEmptyError("Not an empty host")
 	}
+	cnt, err = hh.GetBackupGuestCount()
+	if err != nil {
+		return httperrors.NewInternalServerError("GetBackupGuestCount fail %s", err)
+	}
+	if cnt > 0 {
+		return httperrors.NewNotEmptyError("Not an empty host")
+	}
+
 	for _, hoststorage := range hh.GetHoststorages() {
 		storage := hoststorage.GetStorage()
 		if storage != nil && storage.IsLocal() {
@@ -1610,6 +1618,11 @@ func (hh *SHost) GetGuestsBackupOnThisHost() []SGuest {
 		return nil
 	}
 	return guests
+}
+
+func (hh *SHost) GetBackupGuestCount() (int, error) {
+	q := GuestManager.Query().Equals("backup_host_id", hh.Id)
+	return q.CountWithError()
 }
 
 func (hh *SHost) GetGuestCount() (int, error) {
