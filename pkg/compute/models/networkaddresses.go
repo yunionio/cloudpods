2aa4995b97bfcefc24e2e71fdc4acf5cfc241de4
Qiu Jian <qiujian@yunionyun.com>
2023-09-19 21:16:32 +0800

    minor fixes

--- pkg/compute/models/networkaddresses.go
+++ pkg/compute/models/networkaddresses.go
@@ -538,7 +538,7 @@ func (man *SNetworkAddressManager) ListItemFilter(ctx context.Context, q *sqlche
 		return subq
 	})
 	if err != nil {
-		return nil, errors.Wrap(err, "managedResourceFilterByAccount")
+		return nil, errors.Wrap(err, "ManagedResourceFilterByAccount")
 	}
 
 	return q, nil
