758026a92eb93cf0dd30424cdfae22750590c902
Zexi Li <zexi.li@icloud.com>
2023-09-27 16:39:59 +0800

    fix(scheduler): some joint resources are not synced

--- pkg/scheduler/data_manager/netinterface/netinterface.go
+++ pkg/scheduler/data_manager/netinterface/netinterface.go
@@ -2,10 +2,13 @@ package netinterface
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"yunion.io/x/jsonutils"
+	"yunion.io/x/pkg/errors"
 
+	"yunion.io/x/onecloud/pkg/cloudcommon/db"
 	"yunion.io/x/onecloud/pkg/compute/models"
 	"yunion.io/x/onecloud/pkg/mcclient/modules/compute"
 	"yunion.io/x/onecloud/pkg/scheduler/data_manager/common"
@@ -46,6 +49,26 @@ func NewResourceStore() common.IResourceStore[models.SNetInterface] {
 			wireId, _ := o.GetString("wire_id")
 			mac, _ := o.GetString("mac")
 			return GetId(hostId, wireId, mac)
+		},
+		func(man db.IModelManager, id string, obj *jsonutils.JSONDict) (db.IModel, error) {
+			ids := strings.Split(id, "/")
+			if len(ids) != 3 {
+				return nil, errors.Errorf("Invalid id: %q", id)
+			}
+			q := man.Query()
+			hostId := ids[0]
+			wireId := ids[1]
+			mac := ids[2]
+			q = q.Equals("host_id", hostId).Equals("wire_id", wireId).Equals("mac", mac)
+			objs, err := db.FetchIModelObjects(man, q)
+			errHint := fmt.Sprintf("hostId %q, wireId %q, mac %q", hostId, wireId, mac)
+			if err != nil {
+				return nil, errors.Wrapf(err, "db.FetchIModelObjects by %s", errHint)
+			}
+			if len(objs) != 1 {
+				return nil, errors.Errorf("Found %d objects by %s", len(objs), errHint)
+			}
+			return objs[0], nil
 		},
 	)
 }
