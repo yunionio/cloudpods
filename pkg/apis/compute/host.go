68286e38075b51986f2718aa71fc73ac4f6140b7
Qiu Jian <qiujian@yunionyun.com>
2023-10-01 20:59:40 +0800

    fix: add apis for logininfo

--- pkg/apis/compute/host.go
+++ pkg/apis/compute/host.go
@@ -615,3 +615,12 @@ type HostError struct {
 type HostSyncErrorsInput struct {
 	HostErrors []HostError
 }
+
+type HostLoginInfoInput struct {
+}
+
+type HostLoginInfoOutput struct {
+	Ip       string `json:"ip"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
