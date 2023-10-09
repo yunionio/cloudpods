971857205387fe3d6e4e98e81ce1e48e0eb84d52
wanyaoqi <18528551+wanyaoqi@users.noreply.github.com>
2023-10-09 18:36:32 +0800

    fix(host): host sysinfo metadata double quota (#18239)

--- pkg/hostman/hostinfo/hostinfo.go
+++ pkg/hostman/hostinfo/hostinfo.go
@@ -1444,7 +1444,8 @@ func (h *SHostInfo) updateOrCreateHost(hostId string) (*api.HostDetails, error) 
 	meta, _ := jsonutils.Marshal(h.getSysInfo()).GetMap()
 	input.Metadata = map[string]string{}
 	for k, v := range meta {
-		input.Metadata[k] = v.String()
+		val, _ := v.GetString()
+		input.Metadata[k] = val
 	}
 	input.Version = version.GetShortString()
 
