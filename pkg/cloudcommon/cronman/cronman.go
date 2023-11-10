f52e78a64d19c18fbff4a49f0fbd73038341c604
屈轩 <qu_xuan@icloud.com>
2023-11-10 15:46:07 +0800

    fix(common): support report bug (#18626)

--- pkg/cloudcommon/cronman/cronman.go
+++ pkg/cloudcommon/cronman/cronman.go
@@ -25,12 +25,14 @@ import (
 	"yunion.io/x/log"
 	"yunion.io/x/pkg/appctx"
 	"yunion.io/x/pkg/errors"
+	"yunion.io/x/pkg/util/version"
 
 	"yunion.io/x/onecloud/pkg/appsrv"
 	"yunion.io/x/onecloud/pkg/cloudcommon/consts"
 	"yunion.io/x/onecloud/pkg/cloudcommon/elect"
 	"yunion.io/x/onecloud/pkg/mcclient"
 	"yunion.io/x/onecloud/pkg/mcclient/auth"
+	"yunion.io/x/onecloud/pkg/mcclient/modules/yunionconf"
 )
 
 var (
@@ -444,6 +446,7 @@ func (job *SCronJob) runJobInWorker(isStart bool, startTime time.Time) {
 		if r := recover(); r != nil {
 			log.Errorf("CronJob task %s run error: %s", job.Name, r)
 			debug.PrintStack()
+			yunionconf.BugReport.SendBugReport(context.Background(), version.GetShortString(), string(debug.Stack()), errors.Errorf("%s", r))
 		}
 	}()
 
---
+++ pkg/mcclient/modules/yunionconf/mod_bugreport.go
@@ -0,0 +1,60 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package yunionconf
+
+import (
+	"context"
+
+	"yunion.io/x/jsonutils"
+
+	"yunion.io/x/onecloud/pkg/mcclient"
+	"yunion.io/x/onecloud/pkg/mcclient/auth"
+	"yunion.io/x/onecloud/pkg/mcclient/modulebase"
+	"yunion.io/x/onecloud/pkg/mcclient/modules"
+)
+
+type BugReportManager struct {
+	modulebase.ResourceManager
+}
+
+var (
+	BugReport BugReportManager
+)
+
+func init() {
+	BugReport = BugReportManager{modules.NewYunionConfManager("bug-report", "bug-report",
+		[]string{},
+		[]string{},
+	)}
+	modules.Register(&BugReport)
+}
+
+func (m BugReportManager) DoBugReportEnable(s *mcclient.ClientSession, _ jsonutils.JSONObject) (jsonutils.JSONObject, error) {
+	return modulebase.Post(m.ResourceManager, s, "enable-bug-report", nil, "")
+}
+
+func (m BugReportManager) GetBugReportEnabled(s *mcclient.ClientSession, params jsonutils.JSONObject) (jsonutils.JSONObject, error) {
+	return modulebase.Get(m.ResourceManager, s, "bug-report-status", "")
+}
+
+func (m BugReportManager) SendBugReport(ctx context.Context, version, stack string, err error) (jsonutils.JSONObject, error) {
+	msg := map[string]interface{}{
+		"version": version,
+		"stack":   stack,
+		"message": err.Error(),
+	}
+	s := auth.GetAdminSession(ctx, "")
+	return modulebase.Post(m.ResourceManager, s, "send-bug-report", jsonutils.Marshal(msg), "")
+}
