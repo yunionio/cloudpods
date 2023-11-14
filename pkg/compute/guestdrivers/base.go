34ae3d1e667d2a00bdf589ff258217764ebfc80b
Zexi Li <zexi.li@icloud.com>
2023-11-14 18:58:05 +0800

    feat(region,climc): add set-os-info api

--- pkg/compute/guestdrivers/base.go
+++ pkg/compute/guestdrivers/base.go
@@ -553,3 +553,7 @@ func (drv *SBaseGuestDriver) RequestStartRescue(ctx context.Context, task taskma
 func (drv *SBaseGuestDriver) RequestStopRescue(ctx context.Context, task taskman.ITask, body jsonutils.JSONObject, host *models.SHost, guest *models.SGuest) error {
 	return httperrors.ErrNotImplemented
 }
+
+func (self *SBaseGuestDriver) ValidateSetOSInfo(ctx context.Context, userCred mcclient.TokenCredential, _ *models.SGuest, _ *api.ServerSetOSInfoInput) error {
+	return nil
+}
