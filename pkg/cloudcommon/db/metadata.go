81c1731812e2bf8e6fc393162b23bfafac2989c0
ioito <qu_xuan@icloud.com>
2023-11-24 19:12:16 +0800

    fix(region): trim metadata key

--- pkg/cloudcommon/db/metadata.go
+++ pkg/cloudcommon/db/metadata.go
@@ -532,6 +532,7 @@ func (manager *SMetadataManager) rawSetValues(ctx context.Context, objType strin
 	keys := make([]string, 0, len(store))
 	changes := make([]sMetadataChange, 0)
 	for key, value := range store {
+		key = strings.TrimSpace(key)
 		keys = append(keys, key)
 
 		record := SMetadata{}
@@ -696,7 +697,7 @@ func (manager *SMetadataManager) rawGetAll(objType, objId string, keys []string,
 	ret := make(map[string]string)
 	for _, rec := range records {
 		if len(rec.Value) > 0 || strings.HasPrefix(rec.Key, USER_TAG_PREFIX) || strings.HasPrefix(rec.Key, CLOUD_TAG_PREFIX) {
-			ret[rec.Key] = rec.Value
+			ret[strings.TrimSpace(rec.Key)] = rec.Value
 		}
 	}
 	return ret, nil
@@ -759,7 +760,7 @@ func metaList2Map(manager IMetadataBaseModelManager, userCred mcclient.TokenCred
 	hiddenKeys := manager.GetMetadataHiddenKeys()
 	for _, meta := range metaList {
 		if IsMetadataKeyVisible(meta.Key) && !utils.IsInStringArray(meta.Key, hiddenKeys) {
-			metaMap[meta.Key] = meta.Value
+			metaMap[strings.TrimSpace(meta.Key)] = meta.Value
 		}
 	}
 
