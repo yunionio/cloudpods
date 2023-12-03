f243c3d66f85df6b5358715821e6bb38bbdb0e41
Jian Qiu <swordqiu@gmail.com>
2023-12-03 15:34:36 +0800

    fix: force assign ip address when sync vm vnics (#18853)
    Co-authored-by: Qiu Jian <qiujian@yunionyun.com>

--- pkg/compute/models/guestnetworks.go
+++ pkg/compute/models/guestnetworks.go
@@ -314,17 +314,24 @@ func (manager *SGuestnetworkManager) newGuestNetwork(
 			// if reuse Ip address, no need to check address availability
 			// assign it anyway
 			gn.IpAddr = address
-		} else if provider == api.CLOUD_PROVIDER_ONECLOUD || options.Options.EnablePreAllocateIpAddr {
-			addrTable := network.GetUsedAddresses()
-			recentAddrTable := manager.getRecentlyReleasedIPAddresses(network.Id, network.getAllocTimoutDuration())
-			ipAddr, err := network.GetFreeIP(ctx, userCred, addrTable, recentAddrTable, address, allocDir, reserved)
-			if err != nil {
-				return nil, err
+		} else {
+			// 如果是不具备IPAM能力的平台（主要是OneCloud和VMware，也就是VPC为ONECLOUD的平台 provider == api.CLOUD_PROVIDER_ONECLOUD ），则需要分配IP地址
+			// 如果是其他云平台（具体IPAM能力的平台），则
+			// * 开启options.Options.EnablePreAllocateIpAddr，也就是把IPAM的任务交给平台的，则需要分配IP地址
+			// * IP地址为空并且 !options.Options.EnablePreAllocateIpAddr 时，不需要分配IP，等创建后自动同步过来
+			// * 否则，还是需要先分配了
+			if provider == api.CLOUD_PROVIDER_ONECLOUD || options.Options.EnablePreAllocateIpAddr || (!options.Options.EnablePreAllocateIpAddr && len(address) > 0) {
+				addrTable := network.GetUsedAddresses()
+				recentAddrTable := manager.getRecentlyReleasedIPAddresses(network.Id, network.getAllocTimoutDuration())
+				ipAddr, err := network.GetFreeIP(ctx, userCred, addrTable, recentAddrTable, address, allocDir, reserved)
+				if err != nil {
+					return nil, err
+				}
+				if len(address) > 0 && ipAddr != address && requiredDesignatedIp {
+					return nil, fmt.Errorf("candidate ip %s is occupied!", address)
+				}
+				gn.IpAddr = ipAddr
 			}
-			if len(address) > 0 && ipAddr != address && requiredDesignatedIp {
-				return nil, fmt.Errorf("candidate ip %s is occupied!", address)
-			}
-			gn.IpAddr = ipAddr
 		}
 
 		if vpc.Id != api.DEFAULT_VPC_ID && provider == api.CLOUD_PROVIDER_ONECLOUD {
