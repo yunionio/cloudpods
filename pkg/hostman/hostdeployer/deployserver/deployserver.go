8ebdd7ba2a788e43fac393da4fc909e43508925b
wanyaoqi <18528551+wanyaoqi@users.noreply.github.com>
2023-12-05 07:07:58 +0800

    fix(host-deployer): qemu deployer without kvm support (#18876)

--- pkg/hostman/hostdeployer/deployserver/deployserver.go
+++ pkg/hostman/hostdeployer/deployserver/deployserver.go
@@ -98,11 +98,11 @@ func (*DeployerServer) DeployGuestFs(ctx context.Context, req *deployapi.DeployP
 		req.GuestDesc.Hypervisor = comapi.HYPERVISOR_KVM
 	}
 
+	defer disk.Disconnect()
 	if err := disk.Connect(req.GuestDesc); err != nil {
 		log.Errorf("Failed to connect %s disk: %s", req.GuestDesc.Hypervisor, err)
 		return new(deployapi.DeployGuestFsResponse), errors.Wrap(err, "Connect")
 	}
-	defer disk.Disconnect()
 
 	ret, err := disk.DeployGuestfs(req)
 	if ret == nil {
@@ -136,10 +136,11 @@ func (*DeployerServer) ResizeFs(ctx context.Context, req *deployapi.ResizeFsPara
 		return new(deployapi.Empty), errors.Wrap(err, "GetIDisk fail")
 	}
 	defer disk.Cleanup()
+
+	defer disk.Disconnect()
 	if err := disk.Connect(nil); err != nil {
 		return new(deployapi.Empty), errors.Wrap(err, "disk connect failed")
 	}
-	defer disk.Disconnect()
 
 	return disk.ResizeFs()
 }
