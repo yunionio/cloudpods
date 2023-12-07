35462d4b1503d1bb4143fb9e6546e335d1d1b84e
Qiu Jian <qiujian@yunionyun.com>
2023-12-08 00:37:20 +0800

    fix: no default fallback dns server

--- pkg/compute/models/networks.go
+++ pkg/compute/models/networks.go
@@ -390,24 +390,25 @@ func (self *SNetwork) GetNetAddr() netutils.IPV4Addr {
 func (self *SNetwork) GetDNS() string {
 	if len(self.GuestDns) > 0 {
 		return self.GuestDns
-	} else {
-		zoneName := ""
-		wire, _ := self.GetWire()
-		if wire != nil {
-			zone, _ := wire.GetZone()
-			if zone != nil {
-				zoneName = zone.Name
-			}
-		}
-		srvs, _ := auth.GetDNSServers(options.Options.Region, zoneName)
-		if len(srvs) > 0 {
-			return strings.Join(srvs, ",")
-		}
-		if len(options.Options.DNSServer) > 0 {
-			return options.Options.DNSServer
-		}
-		return api.DefaultDNSServers
 	}
+	zoneName := ""
+	wire, _ := self.GetWire()
+	if wire != nil {
+		zone, _ := wire.GetZone()
+		if zone != nil {
+			zoneName = zone.Name
+		}
+	}
+
+	srvs, _ := auth.GetDNSServers(options.Options.Region, zoneName)
+	if len(srvs) > 0 {
+		return strings.Join(srvs, ",")
+	}
+	if len(options.Options.DNSServer) > 0 {
+		return options.Options.DNSServer
+	}
+
+	return ""
 }
 
 func (self *SNetwork) GetNTP() string {
