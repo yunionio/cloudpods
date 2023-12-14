97826501d50b19cf5353974fdccc1075938636dd
wanyaoqi <18528551+wanyaoqi@users.noreply.github.com>
2023-12-14 09:41:33 +0800

    feat(region,host): clvm support (#18949)

--- pkg/hostman/storageman/storage_lvm.go
+++ pkg/hostman/storageman/storage_lvm.go
@@ -1,3 +1,17 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package storageman
 
 import (
@@ -23,14 +37,11 @@ import (
 
 type SLVMStorage struct {
 	SBaseStorage
-
-	Index int
 }
 
-func NewLVMStorage(manager *SStorageManager, vgName string, index int) *SLVMStorage {
+func NewLVMStorage(manager *SStorageManager, vgName string) *SLVMStorage {
 	var ret = new(SLVMStorage)
 	ret.SBaseStorage = *NewBaseStorage(manager, vgName)
-	ret.Index = index
 	return ret
 }
 
@@ -39,7 +50,7 @@ func (s *SLVMStorage) StorageType() string {
 }
 
 func (s *SLVMStorage) GetComposedName() string {
-	return fmt.Sprintf("host_%s_%s_storage_%d", s.Manager.host.GetMasterIp(), s.StorageType(), s.Index)
+	return fmt.Sprintf("host_%s_%s_storage_%s", s.Manager.host.GetMasterIp(), s.StorageType(), s.Path)
 }
 
 func (s *SLVMStorage) GetMediumType() (string, error) {
@@ -68,7 +79,7 @@ func (s *SLVMStorage) getAvailSizeMb() (int64, error) {
 		return -1, err
 	}
 
-	log.Infof("LVM Storage %s sizeMb %d", s.GetPath(), vgProps.VgSize/1024/1024)
+	log.Debugf("LVM Storage %s sizeMb %d", s.GetPath(), vgProps.VgSize/1024/1024)
 	return vgProps.VgSize / 1024 / 1024, nil
 }
 
@@ -305,6 +316,13 @@ func (s *SLVMStorage) GetImgsaveBackupPath() string {
 }
 
 func (s *SLVMStorage) Accessible() error {
+	out, err := procutils.NewRemoteCommandAsFarAsPossible("pvscan", "--cache").Output()
+	if err != nil {
+		return errors.Wrapf(err, "pvscan --cache failed %s", out)
+	}
+	if err := lvmutils.VgDisplay(s.Path); err != nil {
+		return err
+	}
 	return nil
 }
 
