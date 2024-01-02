3b8549d8c7f5e94586e971ad0ec85853fc421086
屈轩 <qu_xuan@icloud.com>
2024-01-02 16:49:26 +0800

    fix(region): add translate (#19157)

--- pkg/i18n/print.go
+++ pkg/i18n/print.go
@@ -18,10 +18,10 @@ import (
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
 
-	"yunion.io/x/onecloud/locales"
+	_ "yunion.io/x/onecloud/locales"
 )
 
 func P(tag language.Tag, id string, a ...interface{}) string {
-	p := message.NewPrinter(tag, message.Catalog(locales.Catalog))
+	p := message.NewPrinter(tag)
 	return p.Sprintf(id, a...)
 }
