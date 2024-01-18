82fb4a663ebc9fee5553d91c5f6df210c9a8f748
马鸿飞 <mahongfei@yunion.cn>
2024-01-18 19:41:11 +0800

    feat(region): support notify for guest_status changed

--- pkg/compute/models/guest_actions.go
+++ pkg/compute/models/guest_actions.go
@@ -3184,7 +3184,12 @@ func (self *SGuest) SetStatus(userCred mcclient.TokenCredential, status, reason 
 		}
 	}
 
-	return self.SVirtualResourceBase.SetStatus(userCred, status, reason)
+	err := self.SVirtualResourceBase.SetStatus(userCred, status, reason)
+	if err != nil {
+		return errors.Wrap(err, "setStatus")
+	}
+	db.CallUpdateNotifyHook(context.Background(), userCred, self)
+	return nil
 }
 
 func (self *SGuest) SetPowerStates(powerStates string) error {
