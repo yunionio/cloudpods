e9a29645eb1f544c16ed5f90553e311c092db8aa
屈轩 <qu_xuan@icloud.com>
2024-02-18 10:22:47 +0800

    fix(region): vendor update for openstack https support (#19495)

--- vendor/yunion.io/x/cloudmux/pkg/multicloud/esxi/virtualmachine.go
+++ vendor/yunion.io/x/cloudmux/pkg/multicloud/esxi/virtualmachine.go
@@ -1601,7 +1601,18 @@ func (self *SVirtualMachine) relocate(hostId string) error {
 		}
 	}
 	if !isShared {
-		config.Datastore = &targetHs.Datastore[0]
+		err := host.fetchDatastores()
+		if err != nil {
+			return errors.Wrapf(err, "fetchDatastores")
+		}
+		max := int64(0)
+		for i := range host.datastores {
+			ds := host.datastores[i].(*SDatastore)
+			if ds.GetCapacityFreeMB() > max {
+				max = ds.GetCapacityFreeMB()
+				config.Datastore = &targetHs.Datastore[i]
+			}
+		}
 	}
 	task, err := self.getVmObj().Relocate(ctx, config, types.VirtualMachineMovePriorityDefaultPriority)
 	if err != nil {
