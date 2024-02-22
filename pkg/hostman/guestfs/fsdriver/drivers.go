0d370d19f12f096fb22061285e470e5c2d29bff6
Qiu Jian <qiujian@yunionyun.com>
2024-02-22 21:10:36 +0800

    fix: support OpenCloudOS, OpenKylin, Alma, UOSDesktop

--- pkg/hostman/guestfs/fsdriver/drivers.go
+++ pkg/hostman/guestfs/fsdriver/drivers.go
@@ -43,7 +43,8 @@ func Init(initPrivatePrefixes []string, cloudrootDir string) error {
 
 	linuxFsDrivers := []newRootFsDriverFunc{
 		NewFangdeRootFs, NewUnionOSRootFs,
-		NewAnolisRootFs, NewRockyRootFs,
+		NewAnolisRootFs, NewRockyRootFs, NewOpenCloudOsRootFs, NewAlmaLinuxRootFs,
+		NewOpenKylinRootfs, NewUOSDesktopRootfs,
 		NewGalaxyKylinRootFs, NewNeoKylinRootFs,
 		NewFangdeDeskRootfs, NewUKylinRootfs,
 		NewCentosRootFs, NewFedoraRootFs,
