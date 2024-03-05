aa473d0b1aa3991ffda0463c6134a628064efc3e
wanyaoqi <18528551+wanyaoqi@users.noreply.github.com>
2024-03-06 02:11:28 +0800

    Automated cherry pick of #19558: Fix/guest image live migrate (#19615)
    * fix(host): slvm storage active on init
    
    * fix(region): guest live migrate cache all of guestimage caches

--- pkg/compute/tasks/guest_live_migrate_task.go
+++ pkg/compute/tasks/guest_live_migrate_task.go
@@ -139,26 +139,55 @@ func (self *GuestMigrateTask) SaveScheduleResult(ctx context.Context, obj ISched
 		body.Set("is_local_storage", jsonutils.JSONFalse)
 	}
 
-	self.SetStage("OnCachedImageComplete", body)
 	// prepare disk for migration
 	if len(disk.TemplateId) > 0 && isLocalStorage {
-		targetStorageCache := targetHost.GetLocalStoragecache()
-		if targetStorageCache != nil {
-			input := api.CacheImageInput{
-				ImageId:      disk.TemplateId,
-				Format:       disk.DiskFormat,
-				IsForce:      false,
-				SourceHostId: guest.HostId,
-				ParentTaskId: self.GetTaskId(),
+		templates := []string{}
+		guestdisks, _ := guest.GetDisks()
+		for i := range guestdisks {
+			if guestdisks[i].TemplateId != "" {
+				templates = append(templates, guestdisks[i].TemplateId)
 			}
-			err := targetStorageCache.StartImageCacheTask(ctx, self.UserCred, input)
-			if err != nil {
-				self.TaskFailed(ctx, guest, jsonutils.NewString(err.Error()))
-			}
-			return
+		}
+		if len(templates) > 0 {
+			body.Set("cache_templates", jsonutils.NewStringArray(templates))
 		}
 	}
-	self.OnCachedImageComplete(ctx, guest, nil)
+	self.SetStage("OnStartCacheImages", body)
+	self.OnStartCacheImages(ctx, guest, nil)
+}
+
+func (task *GuestMigrateTask) OnStartCacheImages(ctx context.Context, guest *models.SGuest, data jsonutils.JSONObject) {
+	templates, _ := task.Params.GetArray("cache_templates")
+	if len(templates) == 0 {
+		task.OnCachedImageComplete(ctx, guest, nil)
+		return
+	}
+
+	templateId, _ := templates[0].GetString()
+	task.Params.Set("cache_templates", jsonutils.NewArray(templates[1:]...))
+	task.SetStage("OnStartCacheImages", nil)
+
+	targetHostId, _ := task.Params.GetString("target_host_id")
+	targetHost := models.HostManager.FetchHostById(targetHostId)
+	targetStorageCache := targetHost.GetLocalStoragecache()
+	if targetStorageCache != nil {
+		input := api.CacheImageInput{
+			ImageId:      templateId,
+			IsForce:      false,
+			SourceHostId: guest.HostId,
+			ParentTaskId: task.GetTaskId(),
+		}
+		err := targetStorageCache.StartImageCacheTask(ctx, task.UserCred, input)
+		if err != nil {
+			task.TaskFailed(ctx, guest, jsonutils.NewString(err.Error()))
+		}
+		return
+	}
+	task.OnStartCacheImages(ctx, guest, nil)
+}
+
+func (task *GuestMigrateTask) OnStartCacheImagesFailed(ctx context.Context, guest *models.SGuest, data jsonutils.JSONObject) {
+	task.TaskFailed(ctx, guest, data)
 }
 
 // For local storage get disk info
