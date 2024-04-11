e920b6bcdfd2456f984534038123a34741da3b3d
Jian Qiu <swordqiu@gmail.com>
2024-04-11 18:11:52 +0800

    fix: netplan empty bond address @ release/3.10 (#19951)
    Co-authored-by: Qiu Jian <qiujian@yunionyun.com>

--- pkg/hostman/guestfs/fsdriver/linux.go
+++ pkg/hostman/guestfs/fsdriver/linux.go
@@ -912,6 +912,7 @@ func (d *sDebianLikeRootFs) DeployNetworkingScripts(rootFs IDiskPartition, nics 
 			rootFs.Remove(netplanDir+f, false)
 		}
 		netplanConfig := NewNetplanConfig(allNics, bondNics)
+		log.Debugf("netplanConfig:\n %s", netplanConfig.YAMLString())
 		if err := rootFs.FilePutContents(path.Join(netplanDir, "config.yaml"), netplanConfig.YAMLString(), false, false); err != nil {
 			return errors.Wrap(err, "Put netplan config")
 		}
@@ -946,6 +947,7 @@ func (d *sDebianLikeRootFs) DeployNetworkingScripts(rootFs IDiskPartition, nics 
 			cmds.WriteString(fmt.Sprintf("iface %s inet static\n", nicDesc.Name))
 			cmds.WriteString(fmt.Sprintf("    address %s\n", nicDesc.Ip))
 			cmds.WriteString(fmt.Sprintf("    netmask %s\n", netmask))
+			cmds.WriteString(fmt.Sprintf("    hwaddress ether %s\n", nicDesc.Mac))
 			if len(nicDesc.Gateway) > 0 && nicDesc.Ip == mainIp {
 				cmds.WriteString(fmt.Sprintf("    gateway %s\n", nicDesc.Gateway))
 			}
@@ -1362,15 +1364,20 @@ func (r *sRedhatLikeRootFs) deployNetworkingScripts(rootFs IDiskPartition, nics 
 			cmds.WriteString(fmt.Sprintf("MTU=%d\n", nicDesc.Mtu))
 		}
 		if len(nicDesc.Mac) > 0 {
-			cmds.WriteString("HWADDR=")
-			cmds.WriteString(nicDesc.Mac)
-			cmds.WriteString("\n")
+			if len(nicDesc.TeamingSlaves) == 0 {
+				// only real physical nic can set HWADDR
+				cmds.WriteString("HWADDR=")
+				cmds.WriteString(nicDesc.Mac)
+				cmds.WriteString("\n")
+			}
 			cmds.WriteString("MACADDR=")
 			cmds.WriteString(nicDesc.Mac)
 			cmds.WriteString("\n")
 		}
-		if len(nicDesc.TeamingSlaves) != 0 {
-			cmds.WriteString(`BONDING_OPTS="mode=4 miimon=100"\n`)
+		if len(nicDesc.TeamingSlaves) > 0 {
+			// bonding
+			cmds.WriteString(`BONDING_OPTS="mode=4 miimon=100"`)
+			cmds.WriteString("\n")
 		}
 		if nicDesc.TeamingMaster != nil {
 			cmds.WriteString("BOOTPROTO=none\n")
---
+++ pkg/hostman/guestfs/fsdriver/netplan_test.go
@@ -0,0 +1,148 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fsdriver
+
+import (
+	"testing"
+
+	"yunion.io/x/jsonutils"
+
+	"yunion.io/x/onecloud/pkg/cloudcommon/types"
+	"yunion.io/x/onecloud/pkg/util/netplan"
+)
+
+func TestNewNetplanConfig(t *testing.T) {
+	cases := []struct {
+		nics []*types.SServerNic
+		want *netplan.Configuration
+	}{
+		{
+			nics: []*types.SServerNic{
+				&types.SServerNic{
+					Name:      "eth0",
+					Index:     0,
+					Bridge:    "br0",
+					Domain:    "cloud.local",
+					Ip:        "10.168.222.175",
+					Vlan:      1,
+					Driver:    "virtio",
+					Masklen:   24,
+					Virtual:   false,
+					Manual:    true,
+					WireId:    "399a06f3-7925-46c1-8b9f-d5a8580a74df",
+					NetId:     "22c93412-5882-4de0-8357-45ce647ceada",
+					Mac:       "00:24:c7:16:80:f2",
+					BandWidth: 1000,
+					Mtu:       1500,
+					Dns:       "8.8.8.8",
+					Ntp:       "",
+					Net:       "vnet222",
+					Interface: "ens5",
+					Gateway:   "10.168.222.1",
+					Ifname:    "vnet222-175",
+					Routes:    nil,
+
+					LinkUp: true,
+				},
+				&types.SServerNic{
+					Name:      "eth1",
+					Index:     1,
+					Bridge:    "br0",
+					Domain:    "cloud.local",
+					Ip:        "",
+					Vlan:      1,
+					Driver:    "virtio",
+					Masklen:   0,
+					Virtual:   true,
+					Manual:    true,
+					WireId:    "399a06f3-7925-46c1-8b9f-d5a8580a74df",
+					NetId:     "22c93412-5882-4de0-8357-45ce647ceada",
+					Mac:       "00:24:c7:16:80:f3",
+					BandWidth: 1000,
+					Mtu:       1500,
+					Dns:       "8.8.8.8",
+					Ntp:       "",
+					Net:       "vnet222",
+					Interface: "ens5",
+					Gateway:   "",
+					Ifname:    "vnet222-bpg",
+					Routes:    nil,
+
+					LinkUp:   true,
+					TeamWith: "00:24:c7:16:80:f2",
+				},
+			},
+			want: &netplan.Configuration{
+				Network: &netplan.Network{
+					Version:  2,
+					Renderer: netplan.NetworkRendererNetworkd,
+					Ethernets: map[string]*netplan.EthernetConfig{
+						"eth0": &netplan.EthernetConfig{
+							MacAddress: "00:24:c7:16:80:f2",
+							Match: &netplan.EthernetConfigMatch{
+								MacAddress: "00:24:c7:16:80:f2",
+							},
+							Mtu: 1500,
+						},
+						"eth1": &netplan.EthernetConfig{
+							MacAddress: "00:24:c7:16:80:f3",
+							Match: &netplan.EthernetConfigMatch{
+								MacAddress: "00:24:c7:16:80:f3",
+							},
+							Mtu: 1500,
+						},
+					},
+					Bonds: map[string]*netplan.Bond{
+						"bond0": &netplan.Bond{
+							EthernetConfig: netplan.EthernetConfig{
+								Addresses: []string{
+									"10.168.222.175/24",
+								},
+								MacAddress: "00:24:c7:16:80:f2",
+								Gateway4:   "10.168.222.1",
+								Nameservers: &netplan.Nameservers{
+									Search: []string{
+										"cloud.local",
+									},
+									Addresses: []string{
+										"8.8.8.8",
+									},
+								},
+								Mtu: 1500,
+							},
+							Interfaces: []string{
+								"eth0",
+								"eth1",
+							},
+							Parameters: &netplan.BondMode4Params{
+								BondModeBaseParams: &netplan.BondModeBaseParams{
+									Mode:               "802.3ad",
+									MiiMonitorInterval: 100,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, c := range cases {
+		allNics, bondNics := convertNicConfigs(c.nics)
+		netplanConfig := NewNetplanConfig(allNics, bondNics)
+		if jsonutils.Marshal(netplanConfig).String() != jsonutils.Marshal(c.want).String() {
+			t.Errorf("nics: %s want: %s got: %s", jsonutils.Marshal(c.nics), jsonutils.Marshal(c.want).PrettyString(), jsonutils.Marshal(netplanConfig).PrettyString())
+		}
+	}
+}
