b4e65893ecb5c42c39a9d3ea96b30e9bddcea158
Qiu Jian <qiujian@yunionyun.com>
2024-04-16 00:01:50 +0800

    fix: fail to create encrypted nfs disk

--- pkg/hostman/storageman/disk_nas.go
+++ pkg/hostman/storageman/disk_nas.go
@@ -41,18 +41,7 @@ func NewNasDisk(storage IStorage, id string) *SNasDisk {
 
 func (d *SNasDisk) CreateFromTemplate(ctx context.Context, imageId, format string, size int64, encryptInfo *apis.SEncryptInfo) (jsonutils.JSONObject, error) {
 	imageCacheManager := storageManager.GetStoragecacheById(d.Storage.GetStoragecacheId())
-	ret, err := d.SLocalDisk.createFromTemplate(ctx, imageId, format, imageCacheManager, encryptInfo)
-	if err != nil {
-		return nil, err
-	}
-	retSize, _ := ret.Int("disk_size")
-	log.Infof("REQSIZE: %d, RETSIZE: %d", size, retSize)
-	if size > retSize {
-		params := jsonutils.NewDict()
-		params.Set("size", jsonutils.NewInt(size))
-		return d.Resize(ctx, params)
-	}
-	return ret, nil
+	return d.SLocalDisk.createFromTemplateAndResize(ctx, imageId, format, imageCacheManager, encryptInfo, size)
 }
 
 func (d *SNasDisk) CreateFromImageFuse(ctx context.Context, url string, size int64, encryptInfo *apis.SEncryptInfo) error {
