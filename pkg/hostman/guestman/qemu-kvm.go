df4ae274fc253bc82810e58ea4bcdc1b78981291
wanyaoqi <d3lx.yq@gmail.com>
2024-04-19 17:12:27 +0800

    fix(regin,host): delete snapshots after stream complete

--- pkg/hostman/guestman/qemu-kvm.go
+++ pkg/hostman/guestman/qemu-kvm.go
@@ -2935,6 +2935,8 @@ func (s *SKVMGuestInstance) sendStreamDisksComplete(ctx context.Context) {
 	if err := s.SaveLiveDesc(s.Desc); err != nil {
 		log.Errorf("save guest desc failed %s", err)
 	}
+
+	s.SyncStatus("")
 }
 
 func (s *SKVMGuestInstance) GetQemuVersionStr() string {
