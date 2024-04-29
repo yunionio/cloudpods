1c57bc944e220ffcfcde07c2638b5bef54f2205f
wanyaoqi <18528551+wanyaoqi@users.noreply.github.com>
2024-04-30 07:14:21 +0800

    fix(host): ignore disk not found on disk delete handler (#20178)

--- pkg/hostman/storageman/diskhandlers/diskhandler.go
+++ pkg/hostman/storageman/diskhandlers/diskhandler.go
@@ -245,8 +245,10 @@ func performDiskActions(ctx context.Context, w http.ResponseWriter, r *http.Requ
 	if !utils.IsInStringArray(action, []string{"create", "migrate"}) || rebuild {
 		disk, err = storage.GetDiskById(diskId)
 		if err != nil {
-			hostutils.Response(ctx, w, httperrors.NewGeneralError(errors.Wrapf(err, "GetDiskById(%s)", diskId)))
-			return
+			if errors.Cause(err) != cloudprovider.ErrNotFound || action != "delete" {
+				hostutils.Response(ctx, w, httperrors.NewGeneralError(errors.Wrapf(err, "GetDiskById(%s)", diskId)))
+				return
+			}
 		}
 	}
 
