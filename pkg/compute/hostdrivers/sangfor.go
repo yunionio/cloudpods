8fdae45931e1c63b2da0ac3c9f60a2b9f0dbcf75
Qu Xuan <qu_xuan@icloud.com>
2024-06-13 20:19:44 +0800

    fix(region): support sangfor

---
+++ pkg/compute/hostdrivers/sangfor.go
@@ -0,0 +1,41 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hostdrivers
+
+import (
+	api "yunion.io/x/onecloud/pkg/apis/compute"
+	"yunion.io/x/onecloud/pkg/compute/models"
+)
+
+type SSangForHostDriver struct {
+	SManagedVirtualizationHostDriver
+}
+
+func init() {
+	driver := SSangForHostDriver{}
+	models.RegisterHostDriver(&driver)
+}
+
+func (self *SSangForHostDriver) GetHostType() string {
+	return api.HOST_TYPE_SANGFOR
+}
+
+func (self *SSangForHostDriver) GetHypervisor() string {
+	return api.HYPERVISOR_SANGFOR
+}
+
+func (self *SSangForHostDriver) GetProvider() string {
+	return api.CLOUD_PROVIDER_SANGFOR
+}
