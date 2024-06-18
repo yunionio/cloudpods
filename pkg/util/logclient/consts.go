1a2d7db01c3bc99f45f9116d3050c87538eccdb2
Zexi Li <zexi.li@icloud.com>
2024-06-18 19:33:53 +0800

    feat(log): add i18n consts for itsm service

--- pkg/util/logclient/consts.go
+++ pkg/util/logclient/consts.go
@@ -246,4 +246,12 @@ const (
 	ACT_CANCEL = "cancel"
 	ACT_START  = "start"
 	ACT_DONE   = "done"
+
+	ACT_CONFIGURE            = "configure"
+	ACT_ACTIVATE             = "activate"
+	ACT_SUSPEND              = "suspend"
+	ACT_APPROVED             = "approved"
+	ACT_REJECTED             = "rejected"
+	ACT_TRANSFERRED          = "transferred"
+	ACT_TRANSFERRED_REJECTED = "trans_rejected"
 )
