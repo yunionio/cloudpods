2f849b318de50ab8ef58a6ca885fbfa88dad7a8b
wanyaoqi <18528551+wanyaoqi@users.noreply.github.com>
2024-08-26 17:28:47 +0800

    fix(host): nbd worker count add options (#21094)

--- pkg/cloudcommon/options/options.go
+++ pkg/cloudcommon/options/options.go
@@ -155,6 +155,7 @@ type HostCommonOptions struct {
 	ExecutorConnectTimeoutSeconds int    `help:"executor client connection timeout in seconds, default is 30" default:"30"`
 	EnableIsolatedDeviceWhitelist bool   `help:"enable isolated device white list" default:"false"`
 	ImageDeployDriver             string `help:"Image deploy driver" default:"qemu-kvm" choices:"qemu-kvm|nbd|libguestfs"`
+	DeployConcurrent              int    `help:"qemu-kvm deploy driver concurrent" default:"5"`
 }
 
 type DBOptions struct {
