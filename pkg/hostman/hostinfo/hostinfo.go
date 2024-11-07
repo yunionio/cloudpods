f42b4326a672af4590d26fc46dba345f4f57dad8
Jian Qiu <swordqiu@gmail.com>
2024-11-07 09:49:21 +0800

    fix: telegraf kakfa ordered configuration (#21548)
    Co-authored-by: Qiu Jian <qiujian@yunionyun.com>

--- pkg/hostman/hostinfo/hostinfo.go
+++ pkg/hostman/hostinfo/hostinfo.go
@@ -2496,6 +2496,7 @@ func (h *SHostInfo) OnCatalogChanged(catalog mcclient.KeystoneServiceCatalogV3) 
 		}
 	}
 	if !reflect.DeepEqual(telegraf.GetConf(), conf) || (!strings.Contains(svcs, "telegraf") && !telegraf.IsActive()) {
+		log.Infof("telegraf configuration change, to reload ...")
 		log.Debugf("telegraf config: %s", conf)
 		telegraf.SetConf(conf)
 		if !strings.Contains(svcs, "telegraf") {
@@ -2503,6 +2504,8 @@ func (h *SHostInfo) OnCatalogChanged(catalog mcclient.KeystoneServiceCatalogV3) 
 		} else {
 			telegraf.BgReloadConf(conf)
 		}
+	} else {
+		log.Infof("telegraf configuration no change")
 	}
 
 	/*urls, _ = catalog.GetServiceURLs("elasticsearch",
