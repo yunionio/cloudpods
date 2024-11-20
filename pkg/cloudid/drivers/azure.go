3a2e013fb6039b7a4ac4fd963bfde511120540fc
Qu Xuan <qu_xuan@icloud.com>
2024-11-20 19:37:18 +0800

    fix(cloudid): vendor update for azure iam

--- pkg/cloudid/drivers/azure.go
+++ pkg/cloudid/drivers/azure.go
@@ -15,8 +15,16 @@
 package drivers
 
 import (
+	"context"
+
+	"yunion.io/x/cloudmux/pkg/cloudprovider"
+	"yunion.io/x/log"
+	"yunion.io/x/pkg/errors"
+
 	api "yunion.io/x/onecloud/pkg/apis/compute"
+	"yunion.io/x/onecloud/pkg/cloudcommon/db/lockman"
 	"yunion.io/x/onecloud/pkg/cloudid/models"
+	"yunion.io/x/onecloud/pkg/mcclient"
 )
 
 type SAzureDriver struct {
@@ -29,4 +37,83 @@ func (driver SAzureDriver) GetProvider() string {
 
 func init() {
 	models.RegisterProviderDriver(&SAzureDriver{})
+}
+
+func (base SAzureDriver) RequestSyncCloudaccountResources(ctx context.Context, userCred mcclient.TokenCredential, account *models.SCloudaccount, provider cloudprovider.ICloudProvider) error {
+
+	func() {
+		lockman.LockRawObject(ctx, account.Id, models.SAMLProviderManager.Keyword())
+		defer lockman.ReleaseRawObject(ctx, account.Id, models.SAMLProviderManager.Keyword())
+
+		samls, err := provider.GetICloudSAMLProviders()
+		if err != nil {
+			if errors.Cause(err) != cloudprovider.ErrNotSupported && errors.Cause(err) != cloudprovider.ErrNotImplemented {
+				log.Errorf("get saml providers for account %s error: %v", account.Name, err)
+			}
+			return
+		}
+		result := account.SyncSAMLProviders(ctx, userCred, samls, "")
+		log.Infof("Sync SAMLProviders for account %s(%s) result: %s", account.Name, account.Provider, result.Result())
+	}()
+
+	func() {
+		policies, err := provider.GetICloudpolicies()
+		if err != nil {
+			if errors.Cause(err) != cloudprovider.ErrNotSupported && errors.Cause(err) != cloudprovider.ErrNotImplemented {
+				log.Errorf("get system policies for account %s error: %v", account.Name, err)
+			}
+			return
+		}
+		result := account.SyncPolicies(ctx, userCred, policies, "")
+		log.Infof("Sync policies for account %s(%s) result: %s", account.Name, account.Provider, result.Result())
+	}()
+
+	func() {
+		iGroups, err := provider.GetICloudgroups()
+		if err != nil {
+			if errors.Cause(err) != cloudprovider.ErrNotSupported && errors.Cause(err) != cloudprovider.ErrNotImplemented {
+				log.Errorf("get groups for account %s error: %v", account.Name, err)
+			}
+			return
+		}
+		localGroups, remoteGroups, result := account.SyncCloudgroups(ctx, userCred, iGroups, "")
+		log.Infof("SyncCloudgroups for account %s(%s) result: %s", account.Name, account.Provider, result.Result())
+		for i := 0; i < len(localGroups); i += 1 {
+			func() {
+				// lock cloudgroup
+				lockman.LockObject(ctx, &localGroups[i])
+				defer lockman.ReleaseObject(ctx, &localGroups[i])
+
+				localGroups[i].SyncCloudpolicies(ctx, userCred, remoteGroups[i])
+			}()
+		}
+	}()
+
+	func() {
+		iUsers, err := provider.GetICloudusers()
+		if err != nil {
+			if errors.Cause(err) != cloudprovider.ErrNotSupported && errors.Cause(err) != cloudprovider.ErrNotImplemented {
+				log.Errorf("get users for account %s error: %v", account.Name, err)
+			}
+			return
+		}
+		localUsers, remoteUsers, result := account.SyncCloudusers(ctx, userCred, iUsers, "")
+		log.Infof("SyncCloudusers for account %s(%s) result: %s", account.Name, account.Provider, result.Result())
+		for i := 0; i < len(localUsers); i += 1 {
+			func() {
+				// lock clouduser
+				lockman.LockObject(ctx, &localUsers[i])
+				defer lockman.ReleaseObject(ctx, &localUsers[i])
+
+				localUsers[i].SyncCloudpolicies(ctx, userCred, remoteUsers[i])
+				localUsers[i].SyncCloudgroups(ctx, userCred, remoteUsers[i])
+			}()
+		}
+	}()
+
+	return nil
+}
+
+func (base SAzureDriver) RequestSyncCloudproviderResources(ctx context.Context, userCred mcclient.TokenCredential, cp *models.SCloudprovider, provider cloudprovider.ICloudProvider) error {
+	return nil
 }
