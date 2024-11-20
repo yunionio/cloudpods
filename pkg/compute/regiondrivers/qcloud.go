e6a48994be06cd90c01ccb46a91f448edf4e5fdb
Qu Xuan <qu_xuan@icloud.com>
2024-11-20 14:33:33 +0800

    fix(region): vendor update for azure vm create

--- pkg/compute/regiondrivers/qcloud.go
+++ pkg/compute/regiondrivers/qcloud.go
@@ -590,6 +590,6 @@ func (self *SQcloudRegionDriver) ValidateUpdateSecurityGroupRuleInput(ctx contex
 
 func (self *SQcloudRegionDriver) GetSecurityGroupFilter(vpc *models.SVpc) (func(q *sqlchemy.SQuery) *sqlchemy.SQuery, error) {
 	return func(q *sqlchemy.SQuery) *sqlchemy.SQuery {
-		return q.Equals("cloudregion_id", vpc.CloudregionId)
+		return q.Equals("cloudregion_id", vpc.CloudregionId).Equals("manager_id", vpc.ManagerId)
 	}, nil
 }
