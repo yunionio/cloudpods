d2bef87f7a00a65375a873cc04527953e590b71a
wanyaoqi <18528551+wanyaoqi@users.noreply.github.com>
2024-12-20 11:41:10 +0800

    fix(region): disk show io throttles (#21831)

--- pkg/compute/models/disks.go
+++ pkg/compute/models/disks.go
@@ -2420,6 +2420,8 @@ func (manager *SDiskManager) FetchCustomizeColumns(
 		gds.Field("index"),
 		gds.Field("driver"),
 		gds.Field("cache_mode"),
+		gds.Field("iops"),
+		gds.Field("bps"),
 	).
 		Join(gds, sqlchemy.Equals(gds.Field("guest_id"), guestSQ.Field("id"))).
 		Filter(sqlchemy.In(gds.Field("disk_id"), diskIds))
@@ -2433,6 +2435,8 @@ func (manager *SDiskManager) FetchCustomizeColumns(
 		Index     int
 		Driver    string
 		CacheMode string
+		Iops      int
+		Bps       int
 	}{}
 	err := q.All(&guestInfo)
 	if err != nil {
@@ -2454,6 +2458,8 @@ func (manager *SDiskManager) FetchCustomizeColumns(
 			Index:     guest.Index,
 			Driver:    guest.Driver,
 			CacheMode: guest.CacheMode,
+			Iops:      guest.Iops,
+			Bps:       guest.Bps,
 		})
 	}
 
@@ -2500,9 +2506,12 @@ func (manager *SDiskManager) FetchCustomizeColumns(
 	for i := range rows {
 		rows[i].Guests, _ = guests[diskIds[i]]
 		names, status := []string{}, []string{}
+		var iops, bps int
 		for _, guest := range rows[i].Guests {
 			names = append(names, guest.Name)
 			status = append(status, guest.Status)
+			iops = guest.Iops
+			bps = guest.Bps
 		}
 		rows[i].GuestCount = len(rows[i].Guests)
 		rows[i].Guest = strings.Join(names, ",")
@@ -2515,8 +2524,13 @@ func (manager *SDiskManager) FetchCustomizeColumns(
 			rows[i].Brand = "Unknown"
 			rows[i].Provider = "Unknown"
 		}
+		if len(rows[i].ExternalId) == 0 {
+			//rows[i].Iops = iops
+			//rows[i].Throughput = bps
+			disk.Iops = iops
+			disk.Throughput = bps
+		}
 	}
-
 	return rows
 }
 
