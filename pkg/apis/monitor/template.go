65e5e0e6e14f0e7f827fce3650382994b500b85a
Zexi Li <zexi.li@icloud.com>
2025-02-06 19:01:17 +0800

    fix(monitor): not split resource when notifying

--- pkg/apis/monitor/template.go
+++ pkg/apis/monitor/template.go
@@ -19,10 +19,12 @@ type NotificationTemplateCreateInput struct {
 }
 
 type NotificationTemplateConfig struct {
-	Title        string       `json:"title"`
-	Name         string       `json:"name"`
-	ResourceName string       `json:"resource_name"`
-	Matches      []*EvalMatch `json:"matches"`
+	Title        string              `json:"title"`
+	Name         string              `json:"name"`
+	ResourceName string              `json:"resource_name"`
+	Matches      []*EvalMatch        `json:"matches"`
+	MatchTags    []map[string]string `json:"match_tags"`
+	MatchTagsStr []string            `json:"match_tags_str"`
 	// PrevAlertState AlertStateType `json:"prev_alert_state"`
 	// State AlertStateType `json:"state"`
 	NoDataFound bool   `json:"no_data"`
