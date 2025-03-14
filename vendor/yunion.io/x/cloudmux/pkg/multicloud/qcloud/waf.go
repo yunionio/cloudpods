27eb26e96ce8f152b2a7db8d1ff1285a50809117
Qu Xuan <qu_xuan@icloud.com>
2025-03-14 15:34:32 +0800

    fix(region): vendor update

--- vendor/yunion.io/x/cloudmux/pkg/multicloud/qcloud/waf.go
+++ vendor/yunion.io/x/cloudmux/pkg/multicloud/qcloud/waf.go
@@ -79,7 +79,7 @@ func (self *SWafInstance) GetName() string {
 }
 
 func (self *SWafInstance) GetGlobalId() string {
-	return self.Domain
+	return self.DomainId
 }
 
 func (self *SWafInstance) GetId() string {
