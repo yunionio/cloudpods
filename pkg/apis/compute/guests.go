58c7a264c73524acc2154a0c9f72a26e09cf5225
Qu Xuan <qu_xuan@icloud.com>
2025-04-07 10:28:32 +0800

    fix(cloudmon): add host access ip tag for metric

--- pkg/apis/compute/guests.go
+++ pkg/apis/compute/guests.go
@@ -333,6 +333,9 @@ func (self ServerDetails) GetMetricTags() map[string]string {
 		"account_id":          self.AccountId,
 		"external_id":         self.ExternalId,
 	}
+	if len(self.HostAccessIp) > 0 {
+		ret["host_ip"] = self.HostAccessIp
+	}
 
 	return AppendMetricTags(ret, self.MetadataResourceInfo, self.ProjectizedResourceInfo)
 }
