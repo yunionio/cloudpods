fffab8bc66876aef8033f0873ebf373545d98883
wanyaoqi <wanyaoqi@yunion.cn>
2025-04-18 10:04:44 +0800

    fix(region,host): add auto merge snapshots options, preallocation options

--- pkg/cloudcommon/options/options.go
+++ pkg/cloudcommon/options/options.go
@@ -166,6 +166,7 @@ type HostCommonOptions struct {
 	ExecutorConnectTimeoutSeconds int    `help:"executor client connection timeout in seconds, default is 30" default:"30"`
 	ImageDeployDriver             string `help:"Image deploy driver" default:"qemu-kvm" choices:"qemu-kvm|nbd|libguestfs"`
 	DeployConcurrent              int    `help:"qemu-kvm deploy driver concurrent" default:"5"`
+	Qcow2Preallocation            string `help:"Qcow2 image create preallocation" default:"metadata" choices:"disable|metadata|falloc|full"`
 }
 
 type DBOptions struct {
