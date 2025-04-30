f298fece89bbea234c38f323c75bc571cc69e129
Qiu Jian <qiujian@yunionyun.com>
2025-04-30 14:46:03 +0800

    fix: scopedpolicy edit climc command

--- cmd/climc/shell/yunionconf/scopedpolicies.go
+++ cmd/climc/shell/yunionconf/scopedpolicies.go
@@ -15,17 +15,50 @@
 package yunionconf
 
 import (
+	"yunion.io/x/jsonutils"
+	"yunion.io/x/pkg/util/shellutils"
+
 	"yunion.io/x/onecloud/cmd/climc/shell"
-	modules "yunion.io/x/onecloud/pkg/mcclient/modules/yunionconf"
+	api "yunion.io/x/onecloud/pkg/apis/yunionconf"
+	"yunion.io/x/onecloud/pkg/mcclient"
+	"yunion.io/x/onecloud/pkg/mcclient/modules/yunionconf"
 	options "yunion.io/x/onecloud/pkg/mcclient/options/yunionconf"
 )
 
 func init() {
-	cmd := shell.NewResourceCmd(&modules.ScopedPolicies)
+	cmd := shell.NewResourceCmd(&yunionconf.ScopedPolicies)
 	cmd.List(new(options.ScopedPolicyListOptions))
 	cmd.Show(new(options.ScopedPolicyShowOptions))
 	cmd.Create(new(options.ScopedPolicyCreateOptions))
 	cmd.Update(new(options.ScopedPolicyUpdateOptions))
+	R(&options.ScopedPolicyUpdateOptions{}, "scopedpolicy-edit", "edit scoped policy", func(s *mcclient.ClientSession, args *options.ScopedPolicyUpdateOptions) error {
+		policyJson, err := yunionconf.ScopedPolicies.Get(s, args.ID, nil)
+		if err != nil {
+			return err
+		}
+		valueJson, err := policyJson.Get("policies")
+		if err != nil {
+			return err
+		}
+		valueYaml := valueJson.YAMLString()
+		valueYaml, err = shellutils.Edit(valueYaml)
+		if err != nil {
+			return err
+		}
+		valueJson, err = jsonutils.ParseYAML(valueYaml)
+		if err != nil {
+			return err
+		}
+		input := api.ScopedPolicyUpdateInput{
+			Policies: valueJson,
+		}
+		policyJson, err = yunionconf.ScopedPolicies.Update(s, args.ID, jsonutils.Marshal(input))
+		if err != nil {
+			return err
+		}
+		printObject(policyJson)
+		return nil
+	})
 	cmd.Delete(new(options.ScopedPolicyIDOptions))
 	cmd.Perform("bind", new(options.ScopedPolicyBindOptions))
 }
