d8b00188a01b64a8a882d501d5929b8833808256
wanyaoqi <18528551+wanyaoqi@users.noreply.github.com>
2025-05-30 10:29:48 +0800

    feat(region,host): qga deploy network config on attach nics (#22548)

--- pkg/hostman/guestman/guest-agent.go
+++ pkg/hostman/guestman/guest-agent.go
@@ -20,6 +20,7 @@ import (
 	"yunion.io/x/jsonutils"
 	"yunion.io/x/pkg/errors"
 
+	"yunion.io/x/onecloud/pkg/hostman/guestman/desc"
 	deployapi "yunion.io/x/onecloud/pkg/hostman/hostdeployer/apis"
 	"yunion.io/x/onecloud/pkg/hostman/monitor"
 	"yunion.io/x/onecloud/pkg/httperrors"
@@ -107,7 +108,6 @@ func (m *SGuestManager) QgaSetNetwork(ctx context.Context, params interface{}) (
 		Gateway: input.Gateway,
 	}
 
-	//func (m *SGuestManager) QgaSetNetwork(netmod *monitor.NetworkModify, sid string, execTimeout int) (string, error) {
 	guest, err := m.checkAndInitGuestQga(input.Sid)
 	if err != nil {
 		return nil, err
@@ -143,4 +143,16 @@ func (m *SGuestManager) QgaGetOsInfo(sid string) (jsonutils.JSONObject, error) {
 		return nil, errors.Wrap(err, "qga get os info fail")
 	}
 	return jsonutils.Marshal(res), nil
+}
+
+func (guest *SKVMGuestInstance) QgaAddNicsConfigure(addNics []*desc.SGuestNetwork) error {
+	if guest.guestAgent == nil {
+		if err := guest.InitQga(); err != nil {
+			return errors.Wrap(err, "init qga")
+		}
+	}
+	if err := guest.guestAgent.GuestPing(1); err != nil {
+		return errors.Wrap(err, "Qga ping")
+	}
+	return guest.guestAgent.QgaDeployNics(deployapi.GuestNicsToServerNics(addNics))
 }
