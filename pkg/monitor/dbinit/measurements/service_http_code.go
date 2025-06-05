fa3b257302c136e3186980e608de6259bee9d591
Qiu Jian <qiujian@yunionyun.com>
2025-06-06 01:33:54 +0800

    fix: http perf stats monitor

--- pkg/monitor/dbinit/measurements/service_http_code.go
+++ pkg/monitor/dbinit/measurements/service_http_code.go
@@ -25,22 +25,154 @@ var serviceHttpCode = SMeasurement{
 	},
 	Metrics: []SMetric{
 		{
-			"duration.2xx", "http code 2xxx duration", monitor.METRIC_UNIT_NULL,
+			Name:        "duration_ms_any",
+			DisplayName: "Accumulated request duration in milliseconds",
+			Unit:        monitor.METRIC_UNIT_MS,
 		},
 		{
-			"duration.4xx", "http code 4xxx duration", monitor.METRIC_UNIT_NULL,
+			Name:        "dura_ms_delta_any",
+			DisplayName: "Accumulated request duration in milliseconds in last interval",
+			Unit:        monitor.METRIC_UNIT_MS,
 		},
 		{
-			"duration.5xx", "http code 5xxx duration", monitor.METRIC_UNIT_NULL,
+			Name:        "hit_any",
+			DisplayName: "Accumulated request count",
+			Unit:        monitor.METRIC_UNIT_NULL,
 		},
 		{
-			"hit.2xx", "http code 2xxx hit", monitor.METRIC_UNIT_NULL,
+			Name:        "hit_delta_any",
+			DisplayName: "Accumulated request count in last interval",
+			Unit:        monitor.METRIC_UNIT_NULL,
 		},
 		{
-			"hit.4xx", "http code 4xxx hit", monitor.METRIC_UNIT_NULL,
+			Name:        "delay_ms_any",
+			DisplayName: "Average request delay in miilliseconds",
+			Unit:        monitor.METRIC_UNIT_MS,
 		},
 		{
-			"hit.5xx", "http code 5xxx hit", monitor.METRIC_UNIT_NULL,
+			Name:        "qps_any",
+			DisplayName: "Averatge request per second",
+			Unit:        monitor.METRIC_UNIT_NULL,
+		},
+		{
+			Name:        "duration_ms_2xx",
+			DisplayName: "Accumulated request duration in milliseconds for 2xx http code",
+			Unit:        monitor.METRIC_UNIT_MS,
+		},
+		{
+			Name:        "dura_ms_delta_2xx",
+			DisplayName: "Accumulated request duration in milliseconds in last interval for 2xx http code",
+			Unit:        monitor.METRIC_UNIT_MS,
+		},
+		{
+			Name:        "hit_2xx",
+			DisplayName: "Accumulated request count for 2xx http code",
+			Unit:        monitor.METRIC_UNIT_NULL,
+		},
+		{
+			Name:        "hit_delta_2xx",
+			DisplayName: "Accumulated request count in last interval for 2xx http code",
+			Unit:        monitor.METRIC_UNIT_NULL,
+		},
+		{
+			Name:        "delay_ms_2xx",
+			DisplayName: "Average request delay in miilliseconds for 2xx http code",
+			Unit:        monitor.METRIC_UNIT_MS,
+		},
+		{
+			Name:        "percent_hit_2xx",
+			DisplayName: "Request hit weight in percentage for 2xx http code",
+			Unit:        monitor.METRIC_UNIT_PERCENT,
+		},
+		{
+			Name:        "percent_duration_2xx",
+			DisplayName: "Request duration weight in percentage for 2xx http code",
+			Unit:        monitor.METRIC_UNIT_PERCENT,
+		},
+		{
+			Name:        "qps_2xx",
+			DisplayName: "Averatge request per second for 2xx http code",
+			Unit:        monitor.METRIC_UNIT_NULL,
+		},
+		{
+			Name:        "duration_ms_4xx",
+			DisplayName: "Accumulated request duration in milliseconds for 4xx http code",
+			Unit:        monitor.METRIC_UNIT_MS,
+		},
+		{
+			Name:        "dura_ms_delta_4xx",
+			DisplayName: "Accumulated request duration in milliseconds in last interval for 4xx http code",
+			Unit:        monitor.METRIC_UNIT_MS,
+		},
+		{
+			Name:        "hit_4xx",
+			DisplayName: "Accumulated request count for 4xx http code",
+			Unit:        monitor.METRIC_UNIT_NULL,
+		},
+		{
+			Name:        "hit_delta_4xx",
+			DisplayName: "Accumulated request count in last interval for 4xx http code",
+			Unit:        monitor.METRIC_UNIT_NULL,
+		},
+		{
+			Name:        "delay_ms_4xx",
+			DisplayName: "Average request delay in miilliseconds for 4xx http code",
+			Unit:        monitor.METRIC_UNIT_MS,
+		},
+		{
+			Name:        "percent_hit_4xx",
+			DisplayName: "Request hit weight in percentage for 4xx http code",
+			Unit:        monitor.METRIC_UNIT_PERCENT,
+		},
+		{
+			Name:        "percent_duration_4xx",
+			DisplayName: "Request duration weight in percentage for 4xx http code",
+			Unit:        monitor.METRIC_UNIT_PERCENT,
+		},
+		{
+			Name:        "qps_4xx",
+			DisplayName: "Averatge request per second for 4xx http code",
+			Unit:        monitor.METRIC_UNIT_NULL,
+		},
+		{
+			Name:        "duration_ms_5xx",
+			DisplayName: "Accumulated request duration in milliseconds for 5xx http code",
+			Unit:        monitor.METRIC_UNIT_MS,
+		},
+		{
+			Name:        "dura_ms_delta_5xx",
+			DisplayName: "Accumulated request duration in milliseconds in last interval for 5xx http code",
+			Unit:        monitor.METRIC_UNIT_MS,
+		},
+		{
+			Name:        "hit_5xx",
+			DisplayName: "Accumulated request count for 5xx http code",
+			Unit:        monitor.METRIC_UNIT_NULL,
+		},
+		{
+			Name:        "hit_delta_5xx",
+			DisplayName: "Accumulated request count in last interval for 5xx http code",
+			Unit:        monitor.METRIC_UNIT_NULL,
+		},
+		{
+			Name:        "delay_ms_5xx",
+			DisplayName: "Average request delay in miilliseconds for 5xx http code",
+			Unit:        monitor.METRIC_UNIT_MS,
+		},
+		{
+			Name:        "percent_hit_5xx",
+			DisplayName: "Request hit weight in percentage for 5xx http code",
+			Unit:        monitor.METRIC_UNIT_PERCENT,
+		},
+		{
+			Name:        "percent_duration_5xx",
+			DisplayName: "Request duration weight in percentage for 5xx http code",
+			Unit:        monitor.METRIC_UNIT_PERCENT,
+		},
+		{
+			Name:        "qps_5xx",
+			DisplayName: "Averatge request per second for 5xx http code",
+			Unit:        monitor.METRIC_UNIT_NULL,
 		},
 	},
 }
