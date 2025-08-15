1cdead82e7c08b50e33ef2e1d0e940789e675647
wanyaoqi <d3lx.yq@gmail.com>
2025-08-15 18:04:12 +0800

    fix(host): run ifdown script on guest delete

--- pkg/hostman/guestman/qemu-kvm.go
+++ pkg/hostman/guestman/qemu-kvm.go
@@ -2179,6 +2179,14 @@ func (s *SKVMGuestInstance) StartDelete(ctx context.Context, migrated bool) erro
 		s.ForceStop()
 		time.Sleep(time.Second * 1)
 	}
+	// ensure interface down
+	for i := range s.Desc.Nics {
+		scriptPath := s.getNicDownScriptPath(s.Desc.Nics[i])
+		out, err := procutils.NewRemoteCommandAsFarAsPossible("bash", scriptPath).Output()
+		if err != nil {
+			log.Errorf("failed run nic down script %s: %s", out, err)
+		}
+	}
 	return s.Delete(ctx, migrated, false)
 }
 
