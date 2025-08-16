7733907f9beedbb3c5a05f4822951935cffd9e21
Jian Qiu <swordqiu@gmail.com>
2025-08-16 14:45:45 +0800

    fix: webconsole skip refererer check by default (#23092)
    Co-authored-by: Qiu Jian <qiujian@yunionyun.com>

--- pkg/webconsole/options/options.go
+++ pkg/webconsole/options/options.go
@@ -40,7 +40,7 @@ type WebConsoleOptions struct {
 
 	KeepWebsocketSession bool `help:"keep websocket session" default:"false"`
 
-	RefererWhitelist []string `help:"referer whitelist"`
+	RefererWhitelist []string `help:"referer whitelist" default:"skip_check"`
 }
 
 func OnOptionsChange(oldO, newO interface{}) bool {
