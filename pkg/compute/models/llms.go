a0b92cd5d677a3ea1b3397e1770e8a59b9ef9f99
cwz <cwz_eikoh@163.com>
2025-08-28 23:13:03 +0800

    fear(llm): add exec stream

--- pkg/compute/models/llms.go
+++ pkg/compute/models/llms.go
@@ -17,17 +17,22 @@ package models
 import (
 	"context"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"regexp"
 	"strings"
-	"time"
+	"sync"
 
 	"yunion.io/x/jsonutils"
 	"yunion.io/x/log"
+	apis "yunion.io/x/onecloud/pkg/apis"
 	api "yunion.io/x/onecloud/pkg/apis/compute"
 	"yunion.io/x/onecloud/pkg/cloudcommon/db"
 	"yunion.io/x/onecloud/pkg/cloudcommon/db/taskman"
-	"yunion.io/x/onecloud/pkg/cloudcommon/notifyclient"
 	"yunion.io/x/onecloud/pkg/httperrors"
 	"yunion.io/x/onecloud/pkg/mcclient"
+	"yunion.io/x/onecloud/pkg/util/pod/remotecommand"
 	"yunion.io/x/pkg/errors"
 	"yunion.io/x/pkg/util/sets"
 	"yunion.io/x/pkg/util/stringutils"
@@ -51,7 +56,7 @@ func init() {
 	LLMManager.SetVirtualObject(LLMManager)
 	LLMManager.SetAlias("llm", "llms")
 	LLMManager.NameRequireAscii = false
-	notifyclient.AddNotifyDBHookResources(LLMManager.KeywordPlural(), LLMManager.AliasPlural())
+	// notifyclient.AddNotifyDBHookResources(LLMManager.KeywordPlural(), LLMManager.AliasPlural())
 }
 
 func (manager *SLLMManager) DeleteByContainerId(ctx context.Context, userCred mcclient.TokenCredential, ctrId string) error {
@@ -61,9 +66,8 @@ func (manager *SLLMManager) DeleteByContainerId(ctx context.Context, userCred mc
 		return errors.Wrap(err, "db.FetchModelObjects")
 	}
 
-	// log.Infoln("get in delete by container id", llms)
-
 	for _, llm := range llms {
+		// log.Infoln("get in delete by container id", llm)
 		if err := llm.RealDelete(ctx, userCred); nil != err {
 			return err
 		}
@@ -85,26 +89,16 @@ func (llm *SLLM) CustomizeCreate(ctx context.Context, userCred mcclient.TokenCre
 	llm.Id = stringutils.UUID4()
 
 	// init task
-	task, err := taskman.TaskManager.NewTask(ctx, "LLMPullModelTask", llm, userCred, data.(*jsonutils.JSONDict), "", "", nil)
+	task, err := taskman.TaskManager.NewTask(ctx, "LLMPullModelTask", llm, userCred, jsonutils.NewDict(), "", "", nil)
 	if err != nil {
 		return errors.Wrap(err, "NewTask")
 	}
 
-	// init params and set parend task id
-	params := jsonutils.NewDict()
-	params.Update(data)
-	params.Remove("model")
-	params.Set("__parent_task_id", jsonutils.NewString(task.GetId()))
-
-	// set auto_start
-	params.Set("auto_start", jsonutils.NewBool(true))
-
-	// set enviroment OLLAMA_HOST=0.0.0.0:11434
-	// envs := []*apis.ContainerKeyValue{&apis.ContainerKeyValue{
-	// 	Key:   api.LLM_OLLAMA_EXPORT_ENV_KEY,
-	// 	Value: api.LLM_OLLAMA_EXPORT_ENV_VALUE,
-	// }}
-	// params.Set("envs", envs)
+	// init params
+	params, err := llm.InitPodCreateData(data, task.GetId())
+	if err != nil {
+		return errors.Wrap(err, "Customize pod create")
+	}
 
 	// use data to create a pod
 	handler := db.NewModelHandler(GuestManager)
@@ -122,38 +116,98 @@ func (llm *SLLM) CustomizeCreate(ctx context.Context, userCred mcclient.TokenCre
 	return llm.SVirtualResourceBase.CustomizeCreate(ctx, userCred, ownerId, query, data)
 }
 
-func (llm *SLLM) CheckModelExists(ctx context.Context, userCred mcclient.TokenCredential, ctr *SContainer, model string) (bool, error) {
-	listInput := &api.ContainerExecSyncInput{
-		Command: []string{api.LLM_OLLAMA_EXEC_PATH, api.LLM_OLLAMA_LIST_ACTION},
-		Timeout: 0,
-	}
-
-	list, err := ctr.PerformExecSync(ctx, userCred, nil, listInput)
+func (llm *SLLM) exec(userCred mcclient.TokenCredential, command ...string) (string, error) {
+	// get container
+	ctr, err := llm.GetContainer()
 	if nil != err {
-		errors.Wrap(err, "Ollama list")
-		return false, err
+		return "", err
 	}
 
-	listResp := new(api.ContainerExecSyncResponse)
-	if err = list.Unmarshal(listResp); err != nil {
-		errors.Wrap(err, "Unmarshal list response")
-		return false, err
+	// check status
+	if sets.NewString(
+		api.CONTAINER_STATUS_EXITED,
+		api.CONTAINER_STATUS_CREATED,
+	).Has(ctr.GetStatus()) {
+		return "", httperrors.NewInvalidStatusError("Can't exec container in status %s", ctr.Status)
 	}
 
-	// Check if the model exists in the output
-	// ollama list output format: NAME  ID  SIZE  MODIFIED
-	line := strings.TrimSpace(listResp.Stdout)
+	// get Guest and Host
+	guest := ctr.GetPod()
+	host, _ := guest.GetHost()
 
-	fields := strings.Fields(line)
+	// exec command
+	input := &api.ContainerExecInput{
+		Command: command,
+		Tty:     false,
+		SetIO:   true,
+		Stdin:   false,
+		Stdout:  true,
+	}
+	urlLoc := fmt.Sprintf("%s/pods/%s/containers/%s/exec?%s", host.ManagerUri, guest.GetId(), ctr.GetId(), jsonutils.Marshal(input).QueryString())
+	url, err := url.Parse(urlLoc)
+	if err != nil {
+		return "", errors.Wrapf(err, "parse url: %s", urlLoc)
+	}
+	ret, err := execStream(url, mcclient.GetTokenHeaders(userCred))
+	if nil != err {
+		return "", errors.Wrapf(err, "LLM exec error")
+	}
+	log.Infoln("get exec stream result", ret)
 
-	for i := 4; i < len(fields); i += 4 {
-		// log.Infoln("check model list, fields:", fields[i], model)
-		if fields[i] == model {
-			return true, nil
+	// check error and return result
+	return ret, nil
+}
+
+func (llm *SLLM) InitPodCreateData(data jsonutils.JSONObject, taskId string) (jsonutils.JSONObject, error) {
+	// init input and set parend task id
+	input := &api.ServerCreateInput{}
+	if err := data.Unmarshal(input); err != nil {
+		return nil, errors.Wrap(err, "Unmarshal ServerCreateInput")
+	}
+	input.ParentTaskId = taskId
+
+	// set auto_start
+	input.AutoStart = true
+
+	// get pod container
+	if len(input.Pod.Containers) == 0 {
+		return nil, errors.Errorf("Miss container in llm create")
+	}
+	var ctr *api.PodContainerCreateInput
+	finded := false
+	for _, c := range input.Pod.Containers {
+		if strings.Contains(c.Image, api.LLM_OLLAMA) {
+			ctr = c
+			finded = true
+			break
 		}
 	}
+	if !finded {
+		return nil, errors.Errorf("Image must be ollama")
+	}
 
-	return false, nil
+	// mount volume
+	hostPath := &apis.ContainerVolumeMountHostPath{
+		Path:       api.LLM_OLLAMA_CACHE_HOST_PATH,
+		AutoCreate: true,
+	}
+	volumeMount := &apis.ContainerVolumeMount{
+		HostPath:  hostPath,
+		ReadOnly:  false,
+		MountPath: api.LLM_OLLAMA_CACHE_MOUNT_PATH,
+		Type:      apis.CONTAINER_VOLUME_MOUNT_TYPE_HOST_PATH,
+	}
+	ctr.VolumeMounts = append(ctr.VolumeMounts, volumeMount)
+
+	// set enviroment OLLAMA_HOST=0.0.0.0:11434
+	env := &apis.ContainerKeyValue{
+		Key:   api.LLM_OLLAMA_EXPORT_ENV_KEY,
+		Value: api.LLM_OLLAMA_EXPORT_ENV_VALUE,
+	}
+	ctr.Envs = append(ctr.Envs, env)
+
+	log.Infoln("In llm create: pod create input", jsonutils.Marshal(input).String())
+	return jsonutils.Marshal(input), nil
 }
 
 func (llm *SLLM) PerformStart(ctx context.Context, userCred mcclient.TokenCredential, query jsonutils.JSONObject, data jsonutils.JSONObject) (jsonutils.JSONObject, error) {
@@ -178,30 +232,6 @@ func (llm *SLLM) RunModel(ctx context.Context, userCred mcclient.TokenCredential
 
 func (llm *SLLM) RealDelete(ctx context.Context, userCred mcclient.TokenCredential) error {
 	return llm.SVirtualResourceBase.Delete(ctx, userCred)
-}
-
-func llmFetchContainerById(ctrMngr *SContainerManager, llm *SLLM) (*SContainer, error) {
-	ctr := &SContainer{}
-	if err := ctrMngr.Query().Equals("id", llm.ContainerId).First(ctr); err != nil {
-		return nil, errors.Wrap(err, "Query.First Container")
-	}
-	return ctr, nil
-}
-
-func llmGetOllamaContainer(ctrMngr *SContainerManager, llm *SLLM) (*SContainer, error) {
-	ctrs, err := ctrMngr.GetContainersByPod(llm.GuestId)
-	if err != nil {
-		return nil, err
-	}
-	// found ollama
-	for _, ctr := range ctrs {
-		if strings.Contains(ctr.Spec.Image, "ollama") {
-			// update ContainerId
-			llm.SetContainer(ctr.GetId())
-			return &ctr, nil
-		}
-	}
-	return nil, errors.Wrapf(errors.ErrNotFound, "ollama container for guest %s not found", llm.GuestId)
 }
 
 func (llm *SLLM) GetContainer() (*SContainer, error) {
@@ -230,52 +260,107 @@ func (llm *SLLM) SetContainer(ctrId string) error {
 // }
 
 func (llm *SLLM) PullModel(ctx context.Context, userCred mcclient.TokenCredential) error {
-	// get container
-	ctr, err := llm.GetContainer()
-	if nil != err {
-		return err
-	}
 	// pull model
-	pullInput := &api.ContainerExecSyncInput{
-		Command: []string{
-			"/bin/sh", "-c",
-			fmt.Sprintf("nohup %s %s %s > /dev/null 2>&1 &",
-				api.LLM_OLLAMA_EXEC_PATH,
-				api.LLM_OLLAMA_PULL_ACTION,
-				llm.Model),
-		},
-		Timeout: 5,
-	}
-
-	_, err = ctr.PerformExecSync(ctx, userCred, nil, pullInput)
-	if nil != err {
-		return err
-	}
-
-	// wait until model pull complete
-	// check model exists every 10 seconds until it returns true
-	ticker := time.NewTicker(10 * time.Second)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		case <-ticker.C:
-			exists, err := llm.CheckModelExists(ctx, userCred, ctr, llm.Model)
-			if err != nil {
-				return errors.Wrap(err, "CheckModelExists")
-			}
-			if exists {
-				log.Infoln("check model success", llm.Model)
-				return nil
-			}
-		}
-	}
+	_, err := llm.exec(userCred, api.LLM_OLLAMA_EXEC_PATH, api.LLM_OLLAMA_PULL_ACTION, llm.Model)
+	return err
 }
 
 func (llm *SLLM) PostCreate(ctx context.Context, userCred mcclient.TokenCredential, ownerId mcclient.IIdentityProvider, query jsonutils.JSONObject, data jsonutils.JSONObject) {
 	llm.SVirtualResourceBase.PostCreate(ctx, userCred, ownerId, query, data)
 	// set status to creating pod
 	llm.SetStatus(ctx, userCred, api.LLM_STATUS_CREATING_POD, "")
+}
+
+func cleanFinalANSI(input string) string {
+	ansiRegexp := regexp.MustCompile(`\x1b\[[0-9;?]*[ -/]*[@-~]`)
+	input = strings.ReplaceAll(input, "\r", "")
+	return ansiRegexp.ReplaceAllString(input, "")
+}
+
+func processTerminalStream(input string) string {
+	input = strings.ReplaceAll(input, "\x1b[1G", "\r")
+	lines := strings.Split(input, "\n")
+	var resultLines []string
+
+	for _, line := range lines {
+		parts := strings.Split(line, "\r")
+		finalPart := parts[len(parts)-1]
+		cleanedLine := cleanFinalANSI(finalPart)
+		if cleanedLine != "" {
+			resultLines = append(resultLines, cleanedLine)
+		}
+	}
+
+	return strings.Join(resultLines, "\n")
+}
+
+func execStream(url *url.URL, headers http.Header) (string, error) {
+	// define out stream and err stream
+	var outBuf, errBuf strings.Builder
+	outReader, outWriter := io.Pipe()
+	errReader, errWriter := io.Pipe()
+
+	// define goroutine to copy result
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		io.Copy(&outBuf, outReader)
+	}()
+
+	go func() {
+		defer wg.Done()
+		io.Copy(&errBuf, errReader)
+	}()
+
+	// exec stream
+	exec, err := remotecommand.NewSPDYExecutor("POST", url)
+	if err != nil {
+		return "", errors.Wrap(err, "NewSPDYExecutor")
+	}
+	err = exec.Stream(remotecommand.StreamOptions{
+		Stdin:             nil,
+		Stdout:            outWriter,
+		Stderr:            errWriter,
+		Tty:               false,
+		TerminalSizeQueue: nil,
+		Header:            headers,
+	})
+
+	// copy result
+	outWriter.Close()
+	errWriter.Close()
+	wg.Wait()
+
+	outResult := processTerminalStream(outBuf.String())
+	errResult := processTerminalStream(errBuf.String())
+	if err != nil {
+		return "", errors.Wrapf(err, "exec stream error (stdout [%s], stderr [%s])", outResult, errResult)
+	}
+	return outResult, nil
+}
+
+func llmFetchContainerById(ctrMngr *SContainerManager, llm *SLLM) (*SContainer, error) {
+	ctr, err := ctrMngr.FetchById(llm.ContainerId)
+	if nil != err {
+		return nil, errors.Wrap(err, "llmFetchContainerById")
+	}
+	return ctr.(*SContainer), nil
+}
+
+func llmGetOllamaContainer(ctrMngr *SContainerManager, llm *SLLM) (*SContainer, error) {
+	ctrs, err := ctrMngr.GetContainersByPod(llm.GuestId)
+	if err != nil {
+		return nil, err
+	}
+	// found ollama
+	for _, ctr := range ctrs {
+		if strings.Contains(ctr.Spec.Image, api.LLM_OLLAMA) {
+			// update ContainerId
+			llm.SetContainer(ctr.GetId())
+			return &ctr, nil
+		}
+	}
+	return nil, errors.Wrapf(errors.ErrNotFound, "ollama container for guest %s not found", llm.GuestId)
 }
