b6613ee2a473cad1d4c6da28acd5c098d1269478
PengJingzhao <1670916827@qq.com>
2025-09-02 15:33:08 +0800

    feat(mcp-server): 增加mcclient sdk适配器结构体以及对应的认证方法

---
+++ pkg/mcp-server/adapters/cloudpods_adapter.go
@@ -0,0 +1,82 @@
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package adapters
+
+import (
+	"context"
+	"github.com/sirupsen/logrus"
+	"yunion.io/x/onecloud/pkg/mcclient"
+	"yunion.io/x/onecloud/pkg/mcp-server/config"
+)
+
+// CloudpodsAdapter 是与 Cloudpods API 交互的适配器，负责认证和资源管理
+type CloudpodsAdapter struct {
+	config  *config.Config
+	logger  *logrus.Logger
+	client  *mcclient.Client
+	session *mcclient.ClientSession
+}
+
+type CloudRegion struct {
+	RegionId string `json:"region_id"`
+}
+
+// NewCloudpodsAdapter 创建一个新的 Cloudpods 适配器实例
+func NewCloudpodsAdapter(cfg *config.Config, logger *logrus.Logger) *CloudpodsAdapter {
+
+	client := mcclient.NewClient(
+		cfg.External.Cloudpods.BaseURL,
+		cfg.External.Cloudpods.Timeout,
+		false,
+		true,
+		"",
+		"",
+	)
+
+	return &CloudpodsAdapter{
+		config: cfg,
+		logger: logger,
+		client: client,
+	}
+}
+
+// authenticate 实现 Cloudpods 的认证逻辑，例如获取访问令牌
+func (a *CloudpodsAdapter) authenticate(ak string, sk string) error {
+	if a.session != nil {
+		return nil
+	}
+
+	token, err := a.client.AuthenticateByAccessKey(ak, sk, "")
+	if err != nil {
+		return err
+	}
+
+	a.session = a.client.NewSession(
+		context.Background(),
+		"",
+		"",
+		"apigateway",
+		token,
+	)
+
+	return nil
+}
+
+func (a *CloudpodsAdapter) getSession(ak string, sk string) (*mcclient.ClientSession, error) {
+	if err := a.authenticate(ak, sk); err != nil {
+		return nil, err
+	}
+	return a.session, nil
+}
