a5898fcd257158b4e800adc786d1c3cbb91e281f
happygame <happygame1024@gmail.com>
2025-09-02 13:08:55 +0800

    feat(host): provide Kickstart config via content
    Embed the provided config content as a file in a
    labeled ISO (mkisofs), then mount the ISO during
    the Kickstart installation to supply the config.

--- pkg/hostman/guestman/qemu/generate.go
+++ pkg/hostman/guestman/qemu/generate.go
@@ -738,6 +738,7 @@ type KickstartBootInfo struct {
 	InitrdPath     string
 	KernelArgs     string
 	SerialFilePath string
+	ConfigIsoPath  string
 }
 
 func (input *GenerateStartOptionsInput) HasBootIndex() bool {
