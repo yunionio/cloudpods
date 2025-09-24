c7627029a48b45c30d3da0848754f3d1bbc5d563
cwz <cwz_eikoh@163.com>
2025-09-24 23:08:11 +0800

    fix: resolve errors (task cant callback yet)

--- pkg/llm/models/difies.go
+++ pkg/llm/models/difies.go
@@ -10,6 +10,7 @@ import (
 	"yunion.io/x/onecloud/pkg/cloudcommon/db"
 	"yunion.io/x/onecloud/pkg/cloudcommon/db/taskman"
 	"yunion.io/x/onecloud/pkg/httperrors"
+	"yunion.io/x/onecloud/pkg/llm/drivers"
 	"yunion.io/x/onecloud/pkg/mcclient"
 	"yunion.io/x/pkg/errors"
 	"yunion.io/x/pkg/util/stringutils"
@@ -103,6 +104,10 @@ type SDify struct {
 	Registry   string               `width:"64" charset:"ascii" list:"user"`
 }
 
+func (dify *SDify) GetPodDriver() IPodDriver {
+	return &drivers.SPodDriver{}
+}
+
 func (dify *SDify) CustomizeCreate(ctx context.Context, userCred mcclient.TokenCredential, ownerId mcclient.IIdentityProvider, query jsonutils.JSONObject, data jsonutils.JSONObject) error {
 	// unmarshal input
 	input := &api.DifyCreateInput{}
@@ -119,8 +124,7 @@ func (dify *SDify) CustomizeCreate(ctx context.Context, userCred mcclient.TokenC
 	input.ParentTaskId = task.GetId()
 
 	// use data to create a pod
-	handler := db.NewModelHandler(GuestManager)
-	server, err := handler.Create(ctx, jsonutils.NewDict(), jsonutils.Marshal(input.ServerCreateInput), nil)
+	server, err := dify.GetPodDriver().RequestCreatePod(ctx, userCred, &input.ServerCreateInput)
 	if err != nil {
 		return errors.Wrap(err, "CreateServer")
 	}
@@ -153,9 +157,7 @@ func (dify *SDify) CreateContainer(ctx context.Context, userCred mcclient.TokenC
 	}
 
 	// do create task
-	params := jsonutils.NewDict()
-	params.Set("auto_start", jsonutils.JSONTrue)
-	if err := ctr.StartCreateTask(ctx, userCred, taskId, params); nil != err {
+	if err := dify.GetPodDriver().RequestDoCreateContainer(ctx, userCred, ctr, taskId); nil != err {
 		return err
 	}
 
@@ -163,36 +165,39 @@ func (dify *SDify) CreateContainer(ctx context.Context, userCred mcclient.TokenC
 }
 
 func (dify *SDify) CheckContainerHealth(ctx context.Context, userCred mcclient.TokenCredential, containerKey string, command ...string) error {
-	container, err := dify.getContainerByContainerKey(containerKey)
+	containerId, err := dify.getContainerIdByContainerKey(containerKey)
 	if nil != err {
 		return err
 	}
 
 	// exec command
-	result, err := container.PerformExecSync(ctx, userCred, nil, &computeapi.ContainerExecSyncInput{
+	result, err := dify.GetPodDriver().RequestExecSyncContainer(ctx, userCred, containerId, &computeapi.ContainerExecSyncInput{
 		Command: command,
 	})
 	if nil != err {
 		return err
 	}
-	log.Infof("dify check container %s health, result: %s", container.GetName(), result.String())
+	log.Infof("dify check container %s health, result: %s", containerId, result.String())
 
 	return nil
 }
 
 func (dify *SDify) CheckRedis(ctx context.Context, userCred mcclient.TokenCredential) error {
 	// get redis container
-	ctrs, err := GetContainerManager().GetContainersByPod(dify.GuestId)
+	ctrs, err := dify.GetPodDriver().RequestGetContainersByPodId(ctx, userCred, dify.GuestId)
 	if err != nil {
 		return err
 	}
 
 	// set redis container id
-	if len(ctrs) != 1 {
+	if len(ctrs.Data) != 1 {
 		return errors.Errorf("Strange redis container")
 	}
-	ctr := ctrs[0]
-	dify.setContainerId(api.DIFY_REDIS_KEY, ctr.GetId())
+	ctr, err := ctrs.Data[0].GetString("id")
+	if nil != err {
+		return err
+	}
+	dify.setContainerId(api.DIFY_REDIS_KEY, ctr)
 
 	// exec to check health
 	if err := dify.CheckContainerHealth(ctx, userCred, api.DIFY_REDIS_KEY, "redis-cli", "-c", "ping"); nil != err {
@@ -201,24 +206,21 @@ func (dify *SDify) CheckRedis(ctx context.Context, userCred mcclient.TokenCreden
 	return nil
 }
 
-func (dify *SDify) createContainerOnPod(ctx context.Context, userCred mcclient.TokenCredential, containerKey string, data *api.PodContainerCreateInput) (*SContainer, error) {
-	// get guest
-	guest, err := GuestManager.FetchById(dify.GuestId)
-	if nil != err {
-		return nil, err
-	}
-	pod := guest.(*SGuest)
-
+func (dify *SDify) createContainerOnPod(ctx context.Context, userCred mcclient.TokenCredential, containerKey string, data *computeapi.PodContainerCreateInput) (string, error) {
 	// create container on pod
-	ctr, err := GetContainerManager().CreateOnPod(ctx, userCred, pod.GetOwnerId(), pod, data)
+	container, err := dify.GetPodDriver().RequestCreateContainerOnPod(ctx, userCred, dify.GuestId, data)
 	if nil != err {
-		return nil, err
+		return "", err
 	}
 
 	// set container id
-	err = dify.setContainerId(containerKey, ctr.GetId())
+	ctrId, err := container.GetString("id")
+	if err != nil {
+		return "", errors.Wrap(err, "GetContainerId")
+	}
+	err = dify.setContainerId(containerKey, ctrId)
 
-	return ctr, err
+	return ctrId, err
 }
 
 func (dify *SDify) setContainerId(containerKey, ctrId string) error {
@@ -244,30 +246,24 @@ func (dify *SDify) setContainerId(containerKey, ctrId string) error {
 	return nil
 }
 
-func (dify *SDify) getContainerByContainerKey(containerKey string) (*SContainer, error) {
+func (dify *SDify) getContainerIdByContainerKey(containerKey string) (string, error) {
 	if dify.Containers == nil {
-		return nil, errors.Error("containers not initialized")
+		return "", errors.Error("containers not initialized")
 	}
 	containersDict, ok := dify.Containers.(*jsonutils.JSONDict)
 	if !ok {
-		return nil, errors.Error("containers is not a valid JSON dict")
+		return "", errors.Error("containers is not a valid JSON dict")
 	}
 
 	// get ctrId
 	ctrIdJson, err := containersDict.Get(containerKey)
 	if nil != err {
-		return nil, errors.Wrapf(err, "container key %s not found", containerKey)
+		return "", errors.Wrapf(err, "container key %s not found", containerKey)
 	}
 	ctrId, err := ctrIdJson.GetString()
 	if nil != err {
-		return nil, errors.Wrapf(err, "container id not string")
+		return "", errors.Wrapf(err, "container id not string")
 	}
 
-	// get ctr
-	imd, err := GetContainerManager().FetchById(ctrId)
-	if nil != err {
-		return nil, errors.Wrapf(err, "No container with id %s", ctrId)
-	}
-
-	return imd.(*SContainer), nil
+	return ctrId, nil
 }
