dd7602e6281f4dafeb9d4b71697f0507db52d8c6
wanyaoqi <18528551+wanyaoqi@users.noreply.github.com>
2025-10-09 13:27:44 +0800

    fix(host): disk resize error input params (#23493)

--- pkg/hostman/storageman/diskhandlers/diskhandler.go
+++ pkg/hostman/storageman/diskhandlers/diskhandler.go
@@ -351,7 +351,7 @@ func diskResize(ctx context.Context, userCred mcclient.TokenCredential, storage 
 			}
 			return disk.Resize(ctx, input)
 		}
-		hostutils.DelayTask(ctx, resizeFunc, diskInfo)
+		hostutils.DelayTask(ctx, resizeFunc, resizeDiskInfo)
 		return nil, nil
 	}
 }
