cb0729f67224db5248728ae67d08fc711aaeb84c
Jian Qiu <swordqiu@gmail.com>
2025-10-22 23:53:16 +0800

    fix: suport bulding multi-arch rpm deb (#23570)
    Co-authored-by: Qiu Jian <qiujian@yunionyun.com>

--- build/build_deb.sh
+++ build/build_deb.sh
@@ -39,10 +39,18 @@ if [ -z "$VERSION" ]; then
     TAG=$(git describe --abbrev=0 --tags || echo 000000)
     VERSION=${TAG/\//-}
     VERSION=${VERSION/v/}
+    VERSION=${VERSION/master-/}
 fi
 RELEASE=`date +"%y%m%d%H"`
 FULL_VERSION=$VERSION-$RELEASE
-BUILDROOT=$OUTPUT_DIR/yunion-$1-$FULL_VERSION
+BUILDROOT=$OUTPUT_DIR/yunion-$PKG-$FULL_VERSION
+function cleanup {
+  rm -rf "$BUILDROOT"
+  echo "Deleted temp working directory $BUILDROOT"
+}
+# register the cleanup function to be called on the EXIT signal
+trap cleanup EXIT
+
 rm -rf $BUILDROOT
 mkdir -p $BUILDROOT/DEBIAN
 mkdir -p $BUILDROOT/$BIN_PATH
@@ -64,15 +72,45 @@ case $(uname -m) in
         ;;
 esac
 
-echo "Package: yunion-$1
+if [[ -n "$GOARCH" ]]; then
+    case "$GOARCH" in
+		"arm64" | "arm" | "aarch64")
+	        CURRENT_ARCH="arm64"
+            ;;
+		"x86" | "x86_64" | "i686" | "i386" | "amd64")
+			CURRENT_ARCH="amd64"
+            ;;
+	esac
+fi
+
+echo "Package: yunion-$PKG
 Version: $FULL_VERSION
 Section: base
 Priority: optional
 Architecture: $CURRENT_ARCH
 Maintainer: wanyaoqi@yunionyun.com
-Description: Yunion $1
-" > $BUILDROOT/DEBIAN/control
+Description: $DESCRIPTION" > $BUILDROOT/DEBIAN/control
+if [ ${#REQUIRES[@]} -gt 0 ]; then
+    DEPS=$(IFS=, ; echo "${REQUIRES[*]}")
+    echo "Depends: $DEPS" >> $BUILDROOT/DEBIAN/control
+fi
 chmod 0755 $BUILDROOT/DEBIAN/control
 
+cat $BUILDROOT/DEBIAN/control
+
+if [ -n "$SERVICE" ]; then
+echo "#!/bin/bash
+
+/usr/bin/systemctl --no-reload disable yunion-${PKG}.service >/dev/null 2>&1 || :
+/usr/bin/systemctl stop yunion-${PKG}.service >/dev/null 2>&1 ||:
+" > $BUILDROOT/DEBIAN/preinst
+chmod 0755 $BUILDROOT/DEBIAN/preinst
+echo "#!/bin/bash
+
+/usr/bin/systemctl preset yunion-${PKG}.service >/dev/null 2>&1 ||:
+/usr/bin/systemctl daemon-reload >/dev/null 2>&1 ||:
+" > $BUILDROOT/DEBIAN/postinst
+chmod 0755 $BUILDROOT/DEBIAN/postinst
+fi
 
 dpkg-deb --build $BUILDROOT
