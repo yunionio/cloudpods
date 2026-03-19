f5beef50f5d8ed35bf72d0e1f498434ac55b83d4
屈轩 <qu_xuan@icloud.com>
2026-03-19 10:27:57 +0800

    fix(region): allow set disk auto-delete attribute (#24488)

--- pkg/compute/models/guests.go
+++ pkg/compute/models/guests.go
@@ -4808,6 +4808,9 @@ func (self *SGuest) CreateDiskOnStorage(ctx context.Context, userCred mcclient.T
 	if storage.IsLocal() || billingType == billing_api.BILLING_TYPE_PREPAID || isWithServerCreate {
 		autoDelete = true
 	}
+	if diskConfig.AutoDelete != nil {
+		autoDelete = *diskConfig.AutoDelete
+	}
 	disk, err := storage.createDisk(ctx, diskName, diskConfig, userCred, self.GetOwnerId(), autoDelete, self.IsSystem,
 		billingType, billingCycle, self.EncryptKeyId)
 
@@ -5140,6 +5143,7 @@ func (self *SGuest) GetLoadbalancerBackends() ([]SLoadbalancerBackend, error) {
 }
 
 func (self *SGuest) RealDelete(ctx context.Context, userCred mcclient.TokenCredential) error {
+	SnapshotPolicyResourceManager.RemoveByResource(self.Id, api.SNAPSHOT_POLICY_TYPE_SERVER)
 	return self.purge(ctx, userCred)
 }
 
