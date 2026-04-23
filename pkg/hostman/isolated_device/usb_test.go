94a861e0b9f51d0c279755f8a44bb78392b02913
wanyaoqi <18528551+wanyaoqi@users.noreply.github.com>
2026-04-23 13:52:37 +0800

    fix(host): usb device passthrough use hostport (#24709)

--- pkg/hostman/isolated_device/usb_test.go
+++ pkg/hostman/isolated_device/usb_test.go
@@ -178,7 +178,7 @@ func Test_getUSBDevQemuOptions(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got, _ := getUSBDevQemuOptions(tt.args.vendorId, tt.args.deviceId, tt.args.bus, tt.args.addr); !reflect.DeepEqual(got, tt.want) {
+			if got, _ := getUSBDevQemuOptions(tt.args.vendorId, tt.args.deviceId, tt.args.bus, tt.args.addr, ""); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("getUSBDevQemuOptions() = %v, want %v", got, tt.want)
 			}
 		})
