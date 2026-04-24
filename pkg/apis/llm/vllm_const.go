67cceae9e228eb07ab1d8f3bc38390dbb9fa4244
Zexi Li <zexi.li@icloud.com>
2026-04-24 10:28:34 +0800

    Automated cherry pick of #24717: Feature/hf-search (#24718)
    * llm: add instant-model huggingface query commands
    
    * fix(vllm): use ping instead of health
    
    ---------
    
    Co-authored-by: cwz <cwz_eikoh@163.com>

--- pkg/apis/llm/vllm_const.go
+++ pkg/apis/llm/vllm_const.go
@@ -5,13 +5,13 @@ import "time"
 const (
 	LLM_VLLM              = "vllm"
 	LLM_VLLM_DEFAULT_PORT = 8000
-	LLM_VLLM_EXEC_PATH    = "python3 -m vllm.entrypoints.openai.api_server"
+	LLM_VLLM_EXEC_PATH    = "vllm serve"
 
 	LLM_VLLM_HF_ENDPOINT = "https://hf-mirror.com"
 	LLM_VLLM_CACHE_DIR   = "/root/.cache/huggingface"
 	LLM_VLLM_BASE_PATH   = "/data/models"
 	LLM_VLLM_MODELS_PATH = "/data/models/huggingface"
 
-	LLM_VLLM_HEALTH_CHECK_TIMEOUT  = 120 * time.Second
+	LLM_VLLM_HEALTH_CHECK_TIMEOUT  = 180 * time.Second
 	LLM_VLLM_HEALTH_CHECK_INTERVAL = 10 * time.Second
 )
