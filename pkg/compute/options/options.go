63bfef657cf868c25191150ae9d74dbfbf6eb507
Qu Xuan <qu_xuan@icloud.com>
2026-06-02 10:37:09 +0800

    fix(region): support skip server sync by tags

--- pkg/compute/options/options.go
+++ pkg/compute/options/options.go
@@ -208,9 +208,10 @@ type ComputeOptions struct {
 	SkipServerBySysTagKeys         string   `help:"skip server,disk sync and create with system tags" default:""`
 	SkipServerByUserTagKeys        string   `help:"skip server,disk sync and create with user tags" default:""`
 	SkipServerByUserTagValues      []string `help:"skip server,disk sync and create with user tag values"`
+	SkipServerByUserTags           []string `help:"skip server,disk sync and create with tags, format: key=value"`
 	RetentionServerByUserTagKeys   []string `help:"retain server,disk with user tags" default:""`
 	RetentionServerByUserTagValues []string `help:"retain server,disk with user tag values" default:""`
-	RetentionServerByUserTags      []string `help:"retain server,disk with user tags" default:""`
+	RetentionServerByUserTags      []string `help:"retain server,disk with tags, format: key=value"`
 	// 修改标签时不再同步至云上, 云账号同步资源时不会冲掉本地打的标签(key相同的会覆盖), 云账号开启只读同步和此参数效果相同,且仅影响开启只读同步的账号
 	KeepTagLocalization bool `help:"keep tag localization, not synchronized to the cloud" default:"false"`
 
