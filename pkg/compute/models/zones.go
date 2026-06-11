ded845a3dc3aec0cb1442c512746a295898d3cef
屈轩 <qu_xuan@icloud.com>
2026-06-11 14:31:29 +0800

    fix(region): optimized sku query (#25009)

--- pkg/compute/models/zones.go
+++ pkg/compute/models/zones.go
@@ -17,6 +17,7 @@ package models
 import (
 	"context"
 	"database/sql"
+	"time"
 
 	"yunion.io/x/cloudmux/pkg/cloudprovider"
 	"yunion.io/x/jsonutils"
@@ -33,6 +34,7 @@ import (
 	"yunion.io/x/onecloud/pkg/compute/options"
 	"yunion.io/x/onecloud/pkg/httperrors"
 	"yunion.io/x/onecloud/pkg/mcclient"
+	"yunion.io/x/onecloud/pkg/util/hashcache"
 	"yunion.io/x/onecloud/pkg/util/stringutils2"
 )
 
@@ -44,6 +46,8 @@ type SZoneManager struct {
 }
 
 var ZoneManager *SZoneManager
+
+var networkUsableZoneIdsCache = hashcache.NewCache(256, 10*time.Minute)
 
 func init() {
 	ZoneManager = &SZoneManager{
@@ -682,7 +686,39 @@ func filterResult(q *sqlchemy.SQuery) ([]string, error) {
 	return results, nil
 }
 
+func networkUsableZoneIdsCacheKey(usableNet, usableVpc bool, query *api.ZoneListInput) string {
+	type cacheKey struct {
+		UsableNet       bool     `json:"usable_net"`
+		UsableVpc       bool     `json:"usable_vpc"`
+		CloudregionId   []string `json:"cloudregion_id,omitempty"`
+		CloudproviderId []string `json:"cloudprovider_id,omitempty"`
+	}
+	key := cacheKey{
+		UsableNet: usableNet,
+		UsableVpc: usableVpc,
+	}
+	if query != nil {
+		key.CloudregionId = query.CloudregionId
+		key.CloudproviderId = query.CloudproviderId
+	}
+	return jsonutils.Marshal(key).String()
+}
+
 func NetworkUsableZoneIds(usableNet, usableVpc bool, query *api.ZoneListInput) ([]string, error) {
+	cacheKey := networkUsableZoneIdsCacheKey(usableNet, usableVpc, query)
+	if cached := networkUsableZoneIdsCache.AtomicGet(cacheKey); cached != nil {
+		return cached.([]string), nil
+	}
+
+	ret, err := queryNetworkUsableZoneIds(usableNet, usableVpc, query)
+	if err != nil {
+		return nil, err
+	}
+	networkUsableZoneIdsCache.AtomicSet(cacheKey, ret)
+	return ret, nil
+}
+
+func queryNetworkUsableZoneIds(usableNet, usableVpc bool, query *api.ZoneListInput) ([]string, error) {
 	vpcs, err := zoneUsableVpc(usableVpc, query)
 	if err != nil {
 		return nil, errors.Wrap(err, "zoneUsableVpc")
