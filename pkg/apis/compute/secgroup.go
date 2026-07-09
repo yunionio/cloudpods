23975a3ab612647bf7cad9c13520273367d2fe4d
Qiu Jian <qiujian@yunionyun.com>
2026-07-09 14:59:25 +0800

    fix: allow security group cidr have multiple prefixes

--- pkg/apis/compute/secgroup.go
+++ pkg/apis/compute/secgroup.go
@@ -16,6 +16,7 @@ package compute
 
 import (
 	"fmt"
+	"strings"
 
 	"yunion.io/x/pkg/errors"
 	"yunion.io/x/pkg/util/regutils"
@@ -136,6 +137,24 @@ type SSecgroupRuleUpdateInput struct {
 	Description string `json:"description"`
 }
 
+func IsValidSecgroupRuleCIDR(cidr string) bool {
+	isInvalidCidr := func(cidr string) bool {
+		return !regutils.MatchCIDR(cidr) && !regutils.MatchIP4Addr(cidr) && !regutils.MatchCIDR6(cidr) && !regutils.MatchIP6Addr(cidr)
+	}
+
+	if strings.Contains(cidr, ",") {
+		cidrs := strings.Split(cidr, ",")
+		for i := range cidrs {
+			if isInvalidCidr(cidrs[i]) {
+				return false
+			}
+		}
+	} else if isInvalidCidr(cidr) {
+		return false
+	}
+	return true
+}
+
 func (input *SSecgroupRuleResource) Check() error {
 	priority := 1
 	if input.Priority != nil {
@@ -159,8 +178,8 @@ func (input *SSecgroupRuleResource) Check() error {
 	}
 
 	if len(input.CIDR) > 0 {
-		if !regutils.MatchCIDR(input.CIDR) && !regutils.MatchIP4Addr(input.CIDR) && !regutils.MatchCIDR6(input.CIDR) && !regutils.MatchIP6Addr(input.CIDR) {
-			return fmt.Errorf("invalid ip address: %s", input.CIDR)
+		if !IsValidSecgroupRuleCIDR(input.CIDR) {
+			return fmt.Errorf("invalid cidr: %s", input.CIDR)
 		}
 	} else {
 		// empty CIDR means both IPv4 and IPv6
