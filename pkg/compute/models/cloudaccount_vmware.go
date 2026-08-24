1ec83ab46a794cd48fed5927f2233fa52372949f
屈轩 <qu_xuan@icloud.com>
2026-08-24 10:21:37 +0800

    fix(region): show more error info for vmware net detect (#25403)

--- pkg/compute/models/cloudaccount_vmware.go
+++ pkg/compute/models/cloudaccount_vmware.go
@@ -79,7 +79,7 @@ func (account *SCloudaccount) PrepareEsxiHostNetwork(ctx context.Context, userCr
 		accessIp := iHosts[i].GetAccessIp()
 		hostNics, err := iHosts[i].GetIHostNics()
 		if err != nil {
-			return errors.Wrapf(err, "iHosts[%d].GetIHostNics()", i)
+			return errors.Wrapf(err, "iHosts(%s,%s).GetIHostNics()", iHosts[i].GetName(), iHosts[i].GetId())
 		}
 		findAccessIp := false
 		for _, hn := range hostNics {
@@ -150,7 +150,7 @@ func fetchOnpremiseZoneIds(onPremiseNets []SNetwork) ([]string, error) {
 	for i := range onPremiseNets {
 		zone, err := onPremiseNets[i].GetZone()
 		if err != nil {
-			return nil, errors.Wrapf(err, "onPremiseNets[%d].GetZone", i)
+			return nil, errors.Wrapf(err, "onPremiseNets(%s,%s).GetZone()", onPremiseNets[i].GetName(), onPremiseNets[i].GetId())
 		}
 		if !utils.IsInArray(zone.Id, zoneIds) {
 			zoneIds = append(zoneIds, zone.Id)
