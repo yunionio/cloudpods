e81b80e8feb63d203ca3132f66b28c5455106319
Zexi Li <zexi.li@icloud.com>
2026-09-04 18:25:44 +0800

    feat(llm): allow disabling pod/container cgroup CPU and memory limits
    Inference SKUs default to off; SKU and CLI expose enable flags so hosts can skip CFS quota and memory hard limits when needed.

--- pkg/apis/container.go
+++ pkg/apis/container.go
@@ -136,17 +136,21 @@ type ContainerSpec struct {
 	// List of environment variable to set in the container.
 	Envs []*ContainerKeyValue `json:"envs"`
 	// Enable lxcfs
-	EnableLxcfs        bool                      `json:"enable_lxcfs"`
-	Capabilities       *ContainerCapability      `json:"capabilities"`
-	Privileged         bool                      `json:"privileged"`
-	DisableNoNewPrivs  bool                      `json:"disable_no_new_privs"`
-	Lifecyle           *ContainerLifecyle        `json:"lifecyle"`
-	CgroupDevicesAllow []string                  `json:"cgroup_devices_allow"`
-	CgroupPidsMax      int                       `json:"cgroup_pids_max"`
-	ResourcesLimit     *ContainerResources       `json:"resources_limit"`
-	SimulateCpu        bool                      `json:"simulate_cpu"`
-	ShmSizeMB          int                       `json:"shm_size_mb"`
-	SecurityContext    *ContainerSecurityContext `json:"security_context,omitempty"`
+	EnableLxcfs        bool                 `json:"enable_lxcfs"`
+	Capabilities       *ContainerCapability `json:"capabilities"`
+	Privileged         bool                 `json:"privileged"`
+	DisableNoNewPrivs  bool                 `json:"disable_no_new_privs"`
+	Lifecyle           *ContainerLifecyle   `json:"lifecyle"`
+	CgroupDevicesAllow []string             `json:"cgroup_devices_allow"`
+	CgroupPidsMax      int                  `json:"cgroup_pids_max"`
+	ResourcesLimit     *ContainerResources  `json:"resources_limit"`
+	// DisableCgroupCpuLimit skips setting CPU CFS quota for this container.
+	DisableCgroupCpuLimit bool `json:"disable_cgroup_cpu_limit"`
+	// DisableCgroupMemoryLimit skips setting memory hard limit for this container.
+	DisableCgroupMemoryLimit bool                      `json:"disable_cgroup_memory_limit"`
+	SimulateCpu              bool                      `json:"simulate_cpu"`
+	ShmSizeMB                int                       `json:"shm_size_mb"`
+	SecurityContext          *ContainerSecurityContext `json:"security_context,omitempty"`
 	// Periodic probe of container liveness.
 	// Container will be restarted if the probe fails.
 	// Cannot be updated.
