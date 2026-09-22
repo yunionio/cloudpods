91f25cac46a83f4475132615bb63fe974a7700c4
Zexi Li <zexi.li@icloud.com>
2026-09-22 19:57:06 +0800

    fix(climc): default reset_password to true for server-rebuild-root

--- pkg/mcclient/options/compute/servers.go
+++ pkg/mcclient/options/compute/servers.go
@@ -1218,11 +1218,9 @@ func (o *ServerRebuildRootOptions) Params() (jsonutils.JSONObject, error) {
 	if err != nil {
 		return nil, err
 	}
+	params.Set("reset_password", jsonutils.JSONTrue)
 	if o.NoAccountInit != nil && *o.NoAccountInit {
 		params.Add(jsonutils.JSONFalse, "reset_password")
-	}
-	if o.Password != "" {
-		params.Set("reset_password", jsonutils.JSONTrue)
 	}
 	return params, nil
 }
