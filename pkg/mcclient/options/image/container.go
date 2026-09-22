d67cfe81d89c1745a9656a9024442d3f5792d83f
Zexi Li <zexi.li@icloud.com>
2026-09-22 18:02:28 +0800

    feat(glance,region): add command and envs to container_image (#25720)

--- pkg/mcclient/options/image/container.go
+++ pkg/mcclient/options/image/container.go
@@ -15,11 +15,27 @@
 package image
 
 import (
+	"strings"
+
 	"yunion.io/x/jsonutils"
 
 	"yunion.io/x/onecloud/pkg/apis"
 	"yunion.io/x/onecloud/pkg/mcclient/options"
 )
+
+// fetchContainerImageEnvs parses env strings in the format of key=value
+func fetchContainerImageEnvs(envStrs []string) []*apis.ContainerKeyValue {
+	envs := make([]*apis.ContainerKeyValue, 0, len(envStrs))
+	for _, env := range envStrs {
+		pos := strings.Index(env, "=")
+		if pos > 0 {
+			key := strings.TrimSpace(env[:pos])
+			val := strings.TrimSpace(env[pos+1:])
+			envs = append(envs, &apis.ContainerKeyValue{Key: key, Value: val})
+		}
+	}
+	return envs
+}
 
 type ContainerRegistryListOptions struct {
 	options.BaseListOptions
@@ -132,10 +148,24 @@ type ContainerImageCreateOptions struct {
 	IMAGE_LABEL  string `json:"image_label"`
 	CredentialId string `json:"credential_id"`
 	RegistryId   string `json:"registry_id"`
+
+	Command []string `help:"Default startup command used when a container does not specify one, repeatable" json:"-"`
+	Args    []string `help:"Default startup args used when a container does not specify one, repeatable" json:"-"`
+	Env     []string `help:"Default env in the format of key=value, e.g. FOO=bar, repeatable" json:"-"`
 }
 
 func (o *ContainerImageCreateOptions) Params() (jsonutils.JSONObject, error) {
-	return jsonutils.Marshal(o), nil
+	params := jsonutils.Marshal(o).(*jsonutils.JSONDict)
+	if len(o.Command) > 0 {
+		params.Set("command", jsonutils.Marshal(o.Command))
+	}
+	if len(o.Args) > 0 {
+		params.Set("args", jsonutils.Marshal(o.Args))
+	}
+	if len(o.Env) > 0 {
+		params.Set("envs", jsonutils.Marshal(fetchContainerImageEnvs(o.Env)))
+	}
+	return params, nil
 }
 
 type ContainerImageUpdateOptions struct {
@@ -145,6 +175,10 @@ type ContainerImageUpdateOptions struct {
 	ImageLabel   string `json:"image_label"`
 	CredentialId string `json:"credential_id"`
 	RegistryId   string `json:"registry_id"`
+
+	Command []string `help:"Replace the default startup command, repeatable" json:"-"`
+	Args    []string `help:"Replace the default startup args, repeatable" json:"-"`
+	Env     []string `help:"Replace the default env in the format of key=value, repeatable" json:"-"`
 }
 
 func (o *ContainerImageUpdateOptions) GetId() string {
@@ -152,5 +186,15 @@ func (o *ContainerImageUpdateOptions) GetId() string {
 }
 
 func (o *ContainerImageUpdateOptions) Params() (jsonutils.JSONObject, error) {
-	return jsonutils.Marshal(o), nil
+	params := jsonutils.Marshal(o).(*jsonutils.JSONDict)
+	if len(o.Command) > 0 {
+		params.Set("command", jsonutils.Marshal(o.Command))
+	}
+	if len(o.Args) > 0 {
+		params.Set("args", jsonutils.Marshal(o.Args))
+	}
+	if len(o.Env) > 0 {
+		params.Set("envs", jsonutils.Marshal(fetchContainerImageEnvs(o.Env)))
+	}
+	return params, nil
 }
