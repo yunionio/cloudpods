e080ebd8ef6c4d75b5a8b62e9da15796dbaacf95
wanyaoqi <18528551+wanyaoqi@users.noreply.github.com>
2026-09-22 23:12:27 +0800

    fix(region,host): server create add qemu version params (#25714)
    - windows detect is virtio_net support, default use e1000 nic driver
    - add server-set-iso interface

--- pkg/apis/compute/guests.go
+++ pkg/apis/compute/guests.go
@@ -1337,6 +1337,12 @@ type ServerSetPasswordInput struct {
 	AutoStart     bool `json:"auto_start"`
 }
 
+type ServerSetIsoInput struct {
+	CdromOrdinal int64  `json:"cdrom_ordinal"`
+	ImageId      string `json:"image_id"`
+	BootIndex    *int8  `json:"boot_index"`
+}
+
 type ServerInsertVfdInput struct {
 	FloppyOrdinal int64  `json:"floppy_ordinal"`
 	ImageId       string `json:"image_id"`
